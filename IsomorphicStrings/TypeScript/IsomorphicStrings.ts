41b9f58f1b9a3e750b8af44a0ec3d49efc9b9a91
Jason Mauss <jason.mauss@gmail.com>
2021-09-15 00:28:52 -0700

    Adding Solutions for TypeScript and JavaScript

--- IsomorphicStrings/TypeScript/IsomorphicStrings.ts
+++ IsomorphicStrings/TypeScript/IsomorphicStrings.ts
@@ -2,20 +2,27 @@
 const isIsomorphic = (s: string, t: string): boolean => {
 
     const mapSet: Set<string> = new Set<string>();
-    const occurenceSet: Set<string> = new Set<string>();
+    const replacingSet: Set<string> = new Set<string>();
+    const replacedSet: Set<string> = new Set<string>();
 
-    for(let i: number = 0; i < s.length; i++) {
+    const sArray: string[] = s.split('');
+    const tArray: string[] = t.split('');
 
-        let key:string = `${s.substring(i, i + 1)} : ${t.substring(i, i+ 1)}`;
-        
-        if(occurenceSet.has(s[i])) {
-            if(!mapSet.has(key)) {
+    for(let i: number = 0; i < sArray.length; i++) {
+
+        let key:string = `${sArray[i]} : ${tArray[i]}`;
+        //let key2:string = `${tArray[i]} : ${sArray[i]}`;
+
+        if(replacingSet.has(sArray[i])) {
+            if(!mapSet.has(key)) { // || !mapSet.has(key2)) {
                 return false;
-            } else {
-                mapSet.add(key);
             }
         } else {
-            occurenceSet.add(s[i]);
+            if(replacedSet.has(tArray[i])) return false;
+            replacingSet.add(sArray[i]);
+            replacedSet.add(tArray[i]);
+            mapSet.add(key);
+            //mapSet.add(key2);
         }
     }
 
@@ -28,3 +35,4 @@ console.log(isIsomorphic('egg','add')); // true
 console.log(isIsomorphic('foo','bar')); // false
 console.log(isIsomorphic('paper','title')); // true
 console.log(isIsomorphic('paper','tille')); // false
+console.log(isIsomorphic('badc','baba')); // false
