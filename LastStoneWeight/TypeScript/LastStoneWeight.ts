e8c4b3bcb2f62a7dacd0ff10aa8f09b3a724783d
Jason Mauss <jason.mauss@gmail.com>
2021-10-09 02:13:11 -0700

    refactored to something a bit more simple

--- LastStoneWeight/TypeScript/LastStoneWeight.ts
+++ LastStoneWeight/TypeScript/LastStoneWeight.ts
@@ -1,31 +1,26 @@
 // Solution for: https://leetcode.com/problems/last-stone-weight/
 const lastStoneWeight = (stones: number[]): number => {
 
-    let canStillPlay = stones.length > 1;
-    stones.sort((a,b) => b-a); //O(n)
-    console.log(stones);
+    let needToSmash:boolean = stones.length > 1;
+
+    // sort heaviest to lightest
+    stones.sort((a,b) => b - a);
     
-    while (canStillPlay){ //O(logn)
-        //get stones
-        let stone1 = stones.shift(); //heaviest
-        let stone2 = stones.shift();
-        console.log(`stone 1 is ${stone1}`);
-        console.log(`stone 2 is ${stone2}`);
+    while (needToSmash) {
+
+        let firstStone = stones.shift(); // heaviest
+        let secondStone = stones.shift(); // next heaviest
         
-        //smash them
-        if (stone2 <= stone1){
-             if (stone1 === stone2){
-                console.log('INFO: both stones are destroyed!');
-            } else{
-                stone1 = stone1 - stone2;
-                stones.push(stone1);
-                stones.sort((a,b) => b-a); //O(n)
-                console.log('INFO: pushing back into stones array');
-            }
+        // smash the stones
+        if(firstStone !== secondStone) {
+            firstStone = firstStone - secondStone;
+            stones.push(firstStone);
+            stones.sort((a,b) => b - a);
         }
        
-        canStillPlay = stones.length > 1     
+        needToSmash = stones.length > 1     
     }
+
     return stones.length > 0 ? Math.min(...stones) : 0;
 };
 
