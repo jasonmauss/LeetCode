8278cc4c2f4b8cc9d8d88f945cd485889986b212
Jason Mauss <jason.mauss@gmail.com>
2021-10-12 21:22:46 -1000

    Adding Solutions for TypeScript and JavaScript

---
+++ TwoSumII/JavaScript/TwoSumII.js
@@ -0,0 +1,35 @@
+// Solution for: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// const twoSum = (numbers: number[], target: number): number[] => {
+//     const indexValues:number[] = [];
+//     // for each item we encounter, check to see if
+//     // the matching value we need exists, and if so
+//     // what index it exists at. If it exists, push the 
+//     // current index + 1 and the matching value index + 1
+//     // to the returned array
+//     for(let i:number = 0; i < numbers.length; i++) {
+//         let result:number = target - numbers[i];
+//         let matchingIndex:number = numbers.indexOf(result, i + 1);
+//         if(matchingIndex !== -1) {
+//             indexValues.push(i + 1);
+//             indexValues.push(matchingIndex + 1);
+//             return indexValues;
+//         }
+//     }
+// };
+var twoSum = function (numbers, target) {
+    var length = numbers.length;
+    var beginningPosition = 0;
+    var endingPosition = length - 1;
+    var sum = numbers[beginningPosition] + numbers[endingPosition];
+    while (sum !== target) {
+        sum < target ? beginningPosition++ : endingPosition--;
+        sum = numbers[beginningPosition] + numbers[endingPosition];
+    }
+    return [beginningPosition + 1, endingPosition + 1];
+};
+// some test cases
+console.log(twoSum([2, 7, 11, 15], 9)); // [1,2];
+console.log(twoSum([2, 3, 4], 6)); // [1,3];
+console.log(twoSum([-1, 0], -1)); // [1,2];
+console.log(twoSum([5, 7, 8, 9], 14)); // [1,4];
+console.log(twoSum([0, 0, 3, 4], 0)); // [1,2];
