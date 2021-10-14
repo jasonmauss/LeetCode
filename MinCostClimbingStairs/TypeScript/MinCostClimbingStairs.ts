a47ffcd3febc1bcfc0531f9fc9c95d6516aabf35
Jason Mauss <jason.mauss@gmail.com>
2021-10-13 22:26:25 -0700

    Adding Solutions for TypeScript and JavaScript

---
+++ MinCostClimbingStairs/TypeScript/MinCostClimbingStairs.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/min-cost-climbing-stairs/
+const minCostClimbingStairs = (cost: number[]): number => {
+
+    for (let i:number = 2; i < cost.length; i++) {
+        cost[i] += Math.min(cost[i - 2], cost[i - 1]);
+    }
+    
+    return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+
+};
+
+
+// some test cases
+console.log(minCostClimbingStairs([10,15,20])); // 15
+console.log(minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1])); // 6
