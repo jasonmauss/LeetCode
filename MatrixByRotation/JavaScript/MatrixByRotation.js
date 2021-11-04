f17278248ba03bf40124b3cffbca71ff1eec3941
Jason Mauss <jason.mauss@gmail.com>
2021-11-03 22:25:42 -0700

    updated variable names/added comments

--- MatrixByRotation/JavaScript/MatrixByRotation.js
+++ MatrixByRotation/JavaScript/MatrixByRotation.js
@@ -1,11 +1,12 @@
 // Solution for: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
 var findRotation = function (mat, target) {
+    // perform 4 rotations, if rotation result ever matches JSON string of target, return true
     for (var i = 0; i < 4; i++)
         if (JSON.stringify(mat = rotate(mat)) === JSON.stringify(target))
             return true;
     return false;
 };
-var rotate = function (mat) { return mat.map(function (row, i) { return row.map(function (_, j) { return mat[mat.length - 1 - j][i]; }); }); };
+var rotate = function (mat) { return mat.map(function (row, index) { return row.map(function (_, j) { return mat[mat.length - 1 - j][index]; }); }); };
 // some test cases
 console.log(findRotation([[0, 1], [1, 0]], [[1, 0], [0, 1]])); // true
 console.log(findRotation([[0, 1], [1, 1]], [[1, 0], [0, 1]])); // false
--- MatrixByRotation/TypeScript/MatrixByRotation.ts
+++ MatrixByRotation/TypeScript/MatrixByRotation.ts
@@ -1,14 +1,15 @@
 // Solution for: https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
 const findRotation = (mat: number[][], target: number[][]): boolean => {
 
+    // perform 4 rotations, if rotation result ever matches JSON string of target, return true
     for (let i = 0; i < 4; i++)
         if (JSON.stringify(mat = rotate(mat)) === JSON.stringify(target)) return true;
-        
+
     return false;
 
 };
 
-const rotate = (mat: number[][]): number[][] => mat.map((row, i) => row.map((_, j) => mat[mat.length - 1 - j][i]));
+const rotate = (mat: number[][]): number[][] => mat.map((row, index) => row.map((_, j) => mat[mat.length - 1 - j][index]));
 
 // some test cases
 console.log(findRotation([[0,1],[1,0]], [[1,0],[0,1]])); // true
