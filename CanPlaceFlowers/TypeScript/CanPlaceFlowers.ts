94884ce7e31dc1f5286e88d54f6c9d9a0b673542
Jason Mauss <jason.mauss@gmail.com>
2021-12-20 00:31:42 -0700

    Adding Solutions for TypeScript and JavaScript

--- CanPlaceFlowers/TypeScript/CanPlaceFlowers.ts
+++ CanPlaceFlowers/TypeScript/CanPlaceFlowers.ts
@@ -1,23 +1,39 @@
 // Solution for: https://leetcode.com/problems/can-place-flowers/
 const canPlaceFlowers = (flowerbed: number[], n: number): boolean => {
     
-    // turn the numeric array into a string of 1's and 0's
-    const bed = flowerbed.join('').toString();
-    // bedIndex will track where in the string we find
-    // a string of 3 consecutive zeros
-    let bedIndex:number = -1;
     // openPlots will track how many open plots we
     // find by following the no-adjacent-flowers rule
     let openPlots = 0;
+    let beforeIsZero:boolean, afterIsZero:boolean;
 
-    // search for the first occurence of 3 consecutive zeros.
-    bedIndex = bed.indexOf('0') || bed.indexOf('100');
+    // for any given position in the 1's and 0's, determine
+    // if the previous number is a zero and if the next number
+    // is a zero. If both of those are true, add a plot and then
+    // advance the iterator by 1 to skip the "after" zero, and
+    // keep checking.
     
-    while(bedIndex > -1) {
-        openPlots++;
-        bedIndex = bed.indexOf('000', bedIndex + 2) || bed.indexOf('100', bedIndex + 2);
+    // Keep in mind the position in the string where
+    // you're checking because "beforeisZero" should always be true
+    // for the first character, and "afterIsZero" should always be true
+    // for the last character in the string.
+    for(let i:number = 0; i < flowerbed.length && openPlots < n; i++) {
+        if(flowerbed[i] === 0) {
+            beforeIsZero = i === 0 || flowerbed[i - 1] === 0;
+            afterIsZero = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+        } else {
+            continue;
+        }
+
+        if(beforeIsZero && afterIsZero) {
+            openPlots++;
+            i = i + 1;
+        }
     }
 
+    // the return value should just be whether you found as many
+    // (or more, just to be safe) plots as the input argument n. Though, with the boolean
+    // condition in the for loop initializer checking the openPlots value,
+    // openPlots should never be greater than, only equal to.
     return openPlots >= n;
 };
 
@@ -26,4 +42,4 @@ console.log(canPlaceFlowers([1,0,0,0,1], 1)); // true
 console.log(canPlaceFlowers([1,0,0,0,1], 2)); // false
 console.log(canPlaceFlowers([1,0,0,0,0,0,1], 2)); // true
 console.log(canPlaceFlowers([1,0,0,0,0,1], 2)); // false
-console.log(canPlaceFlowers([1,0,0,0,0,0,0,1,0,0], 3)); // true
+console.log(canPlaceFlowers([1,0,0,0,0,0,0,1,0,0], 2)); // true
