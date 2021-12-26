5c4b5d9d6ae2ba81340e949c28193bfc6c95f845
Jason Mauss <jason.mauss@gmail.com>
2021-12-25 23:21:08 -0700

    Adding Solution for TypeScript and JavaScript

--- PrisonCellsAfterNDays/JavaScript/PrisonCellsAfterNDays.js
+++ PrisonCellsAfterNDays/JavaScript/PrisonCellsAfterNDays.js
@@ -1,6 +1,28 @@
 // Solution for: https://leetcode.com/problems/prison-cells-after-n-days/
 var prisonAfterNDays = function (cells, n) {
-    return [0];
+    // Since n (number of days/cycles) can get rather large for this problem,
+    // we will store a history of patterns so that we can use it later on
+    // if/when the pattern repeats.
+    var result = [], resultHistory = [];
+    while (n--) {
+        for (var i = 0; i < cells.length; i++) {
+            if (cells[i - 1] === cells[i + 1]) {
+                result[i] = 1;
+            }
+            else {
+                result[i] = 0;
+            }
+        }
+        cells = [];
+        cells.push.apply(cells, result);
+        var resultIndex = resultHistory.indexOf(cells.join(''));
+        if (resultIndex != -1) {
+            var indexMod = n % (resultHistory.length - resultIndex);
+            return resultHistory[resultIndex + indexMod].split('');
+        }
+        resultHistory.push(cells.join(''));
+    }
+    return result;
 };
 // some test cases
 console.log(prisonAfterNDays([0, 1, 0, 1, 1, 0, 0, 1], 7)); // [0,0,1,1,0,0,0,0]
--- PrisonCellsAfterNDays/TypeScript/PrisonCellsAfterNDays.ts
+++ PrisonCellsAfterNDays/TypeScript/PrisonCellsAfterNDays.ts
@@ -1,6 +1,33 @@
 // Solution for: https://leetcode.com/problems/prison-cells-after-n-days/
 const prisonAfterNDays = (cells: number[], n: number): number[] => {
-    return [0];
+    
+    // Since n (number of days/cycles) can get rather large for this problem,
+    // we will store a history of patterns so that we can use it later on
+    // if/when the pattern repeats.
+    let result = [],
+        resultHistory = [];
+
+    while (n--) {
+        for(let i:number = 0; i < cells.length; i++) {
+            if(cells[i - 1] === cells[i + 1]) {
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+        }
+        cells = [];
+        cells.push(...result);
+        const resultIndex = resultHistory.indexOf(cells.join(''));
+        
+        if(resultIndex != -1) {
+            const indexMod = n % (resultHistory.length - resultIndex);
+            return resultHistory[resultIndex + indexMod].split('');
+        }
+
+        resultHistory.push(cells.join(''));
+    }
+
+    return result;
 };
 
 // some test cases
