87db1996add79d35318ca5c384804fb81248e061
Jason Mauss <jason.mauss@gmail.com>
2022-01-01 23:27:16 -0700

    Initial commit

---
+++ NonOverlappingIntervals/TypeScript/NonOverlappingIntervals.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/non-overlapping-intervals/
+const eraseOverlapIntervals = (intervals: number[][]): number => {
+
+    return 0;
+
+};
+
+// some test cases
+console.log(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]])); // 1
+console.log(eraseOverlapIntervals([[1,2],[1,2],[1,2]])); // 2
+console.log(eraseOverlapIntervals([[1,2],[2,3]])); // 0
