117ee2eb3356fe745cde5fdd645b534db850374f
Jason Mauss <jason.mauss@gmail.com>
2022-01-04 23:58:14 -0700

    implementation using String.prototype.repeat

--- RepeatedStringMatch/TypeScript/RepeatedStringMatch.ts
+++ RepeatedStringMatch/TypeScript/RepeatedStringMatch.ts
@@ -1,10 +1,21 @@
 // Solution for: https://leetcode.com/problems/repeated-string-match/
 const repeatedStringMatch = (a: string, b: string): number => {
     
-    return 0;
+    let repeatCount:number = 1;
+    let repeatedString:string = '';
+
+    while(repeatedString.length <= b.length) {
+        repeatedString = a.repeat(repeatCount);
+        if(repeatedString.indexOf(b) > -1) return repeatCount;
+        repeatCount++;    
+    }
+    
+
+    return -1;
 
 };
 
 // some test cases
 console.log(repeatedStringMatch("abcd","cdabcdab")); // 3
 console.log(repeatedStringMatch("a","aa")); // 2
+console.log(repeatedStringMatch("abc","cabcabca")); // 4
