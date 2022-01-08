29693d09c6d786068a52143c63b5cfc4d9811f43
Jason Mauss <jason.mauss@gmail.com>
2022-01-07 23:48:38 -0700

    initial commit

---
+++ CyclicallyRotatingAGrid/TypeScript/CyclicallyRotatingAGrid.ts
@@ -0,0 +1,10 @@
+// Solution for: https://leetcode.com/problems/cyclically-rotating-a-grid/
+const rotateGrid = (grid: number[][], k: number): number[][] => {
+
+    return [];
+    
+};
+
+// some test cases
+console.log(rotateGrid([[40,10],[30,20]], 1)); //  [[10,20],[40,30]]
+console.log(rotateGrid([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 2)); //  [[3,4,8,12],[2,11,10,16],[1,7,6,15],[5,9,13,14]]
