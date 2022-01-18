004877a893f316a5e95ed5bec9d4c06e734878ec
Jason Mauss <jason.mauss@gmail.com>
2022-01-17 23:58:23 -0700

    Initial commit

---
+++ PartitionArrayDisjointIntervals/TypeScript/PartitionArrayDisjointIntervals.ts
@@ -0,0 +1,10 @@
+// Solution for: https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+const partitionDisjoint = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+// some test cases
+console.log(partitionDisjoint([5,0,3,8,6])); // 3
+console.log(partitionDisjoint([1,1,1,0,6,12])); // 4
