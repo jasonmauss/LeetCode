db4ae4189d9932f2d1d5695f7678374fd73fb761
Jason Mauss <jason.mauss@gmail.com>
2022-01-24 23:46:39 -0700

    initial commit

---
+++ MinCostToMergeStones/JavaScript/MinCostToMergeStones.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/minimum-cost-to-merge-stones/
+var mergeStones = function (stones, k) {
+    return 0;
+};
+// some test cases
+console.log(mergeStones([3, 2, 4, 1], 2)); // 20
+console.log(mergeStones([3, 2, 4, 1], 3)); // -1
+console.log(mergeStones([3, 5, 1, 2, 6], 3)); // 25
---
+++ MinCostToMergeStones/TypeScript/MinCostToMergeStones.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/minimum-cost-to-merge-stones/
+const mergeStones = (stones: number[], k: number): number => {
+
+    return 0;
+
+};
+
+// some test cases
+console.log(mergeStones([3,2,4,1], 2)); // 20
+console.log(mergeStones([3,2,4,1], 3)); // -1
+console.log(mergeStones([3,5,1,2,6], 3)); // 25
