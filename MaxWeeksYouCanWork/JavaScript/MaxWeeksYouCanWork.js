a46af5588c77398748d0c008616f678dc40dcd24
Jason Mauss <jason.mauss@gmail.com>
2022-02-01 23:55:13 -0700

    initial commit

---
+++ MaxWeeksYouCanWork/JavaScript/MaxWeeksYouCanWork.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/maximum-number-of-weeks-for-which-you-can-work/
+var numberOfWeeks = function (milestones) {
+    return 0;
+};
+// some test cases
+console.log(numberOfWeeks([1, 2, 3])); // 6
+console.log(numberOfWeeks([5, 2, 1])); // 7
---
+++ MaxWeeksYouCanWork/TypeScript/MaxWeeksYouCanWork.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/maximum-number-of-weeks-for-which-you-can-work/
+const numberOfWeeks = (milestones: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(numberOfWeeks([1,2,3])); // 6
+console.log(numberOfWeeks([5,2,1])); // 7
