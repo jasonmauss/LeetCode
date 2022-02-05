6392a4e83e04128bb0ec3da21005365f695bf066
Jason Mauss <jason.mauss@gmail.com>
2022-02-04 22:32:05 -0700

    Adding Solutions for TypeScript and JavaScript

--- SubsetsII/TypeScript/SubsetsII.ts
+++ SubsetsII/TypeScript/SubsetsII.ts
@@ -1,7 +1,22 @@
 // Solution for: https://leetcode.com/problems/subsets-ii/
 const subsetsWithDup = (nums: number[]): number[][] => {
 
-    return [[0],[0]];
+    nums = nums.sort((a,b) => a - b);
+
+    let starts: number[] = new Array(nums.length).fill(0);
+    let result: number[][] = [[]];
+
+    for(let i:number = 0; i < nums.length; i++) {
+        let size:number = result.length;
+        starts[i] = size;
+        let j = (i > 0 && nums[i] == nums[i-1]) ? starts[i-1] : 0;
+        while(j < size) {
+            result.push([...result[j], nums[i]]);
+            j++;
+        }
+    }
+    
+    return result;
 
 };
 
