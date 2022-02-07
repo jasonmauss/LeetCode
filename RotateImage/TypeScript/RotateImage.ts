3fe931195ef1fdb7fc19adb8d9196995c0f899c5
Jason Mauss <jason.mauss@gmail.com>
2022-02-06 23:11:56 -0700

    Initial commit

---
+++ RotateImage/TypeScript/RotateImage.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/rotate-image/
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+ const rotate = (matrix: number[][]): void => {
+
+
+};
+
+
+// some test cases
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]])); // [[7,4,1],[8,5,2],[9,6,3]]
+console.log(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])); // [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
