d1f0ad4cd77755dd8bb0f6dabe298e2b2db578d9
Jason Mauss <jason.mauss@gmail.com>
2022-02-09 21:22:20 -0600

    initial commit

---
+++ CheckStringContainsBinaryCodes/TypeScript/CheckStringContainsBinaryCodes.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+function hasAllCodes(s: string, k: number): boolean {
+
+    return false;
+    
+};
+
+
+// some test cases
+console.log(hasAllCodes("00110110", 2)); // true
+console.log(hasAllCodes("0110", 1)); // true
+console.log(hasAllCodes("0110", 2)); // false
