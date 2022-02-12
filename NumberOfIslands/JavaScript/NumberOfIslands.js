936163720c768b8afcb0707406297c1c9f3c2f5f
Jason Mauss <jason.mauss@gmail.com>
2022-02-11 22:53:34 -0700

    Adding Solutions for TypeScript and JavaScript

--- NumberOfIslands/JavaScript/NumberOfIslands.js
+++ NumberOfIslands/JavaScript/NumberOfIslands.js
@@ -1,6 +1,30 @@
 // Solution for: https://leetcode.com/problems/number-of-islands/
 var numIslands = function (grid) {
-    return 0;
+    var islandCount = 0;
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === '1') {
+                islandCount++;
+                helper(i, j, grid);
+            }
+        }
+    }
+    return islandCount;
+};
+var helper = function (i, j, grid) {
+    grid[i][j] = '0';
+    if (i > 0 && grid[i - 1][j] === '1') {
+        helper(i - 1, j, grid);
+    }
+    if (i < grid.length - 1 && grid[i + 1][j] === '1') {
+        helper(i + 1, j, grid);
+    }
+    if (j > 0 && grid[i][j - 1] === '1') {
+        helper(i, j - 1, grid);
+    }
+    if (j < grid[i].length - 1 && grid[i][j + 1] === '1') {
+        helper(i, j + 1, grid);
+    }
 };
 // some test cases
 console.log(numIslands([
--- NumberOfIslands/TypeScript/NumberOfIslands.ts
+++ NumberOfIslands/TypeScript/NumberOfIslands.ts
@@ -1,10 +1,42 @@
 // Solution for: https://leetcode.com/problems/number-of-islands/
 const numIslands = (grid: string[][]): number => {
     
-    return 0;
+  let islandCount:number = 0;
+
+  for(let i:number = 0; i < grid.length; i++) {
+    for(let j:number = 0; j < grid[i].length; j++) {
+      if(grid[i][j] === '1') {
+        islandCount++;
+        helper(i, j, grid);
+      }
+    }
+  }
+
+  return islandCount;
 
 };
 
+const helper = (i: number, j: number, grid: string[][]): void => {
+  
+  grid[i][j] = '0';
+
+  if(i > 0 && grid[i-1][j] === '1') {
+      helper(i - 1, j, grid);
+  }
+
+  if(i < grid.length-1 && grid[i+1][j] === '1') {
+      helper(i + 1, j, grid);
+  }
+
+  if(j > 0 && grid[i][j-1] === '1') {
+      helper(i, j - 1, grid);
+  }
+
+  if(j < grid[i].length-1 && grid[i][j+1] === '1') {
+      helper(i, j + 1, grid);
+  }
+
+}
 
 // some test cases
 console.log(numIslands([
