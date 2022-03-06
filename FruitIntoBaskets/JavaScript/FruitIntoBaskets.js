72d66842dddecd01d4a9457b424b2b1a55a52019
Jason Mauss <jason.mauss@gmail.com>
2022-03-05 20:27:55 -0700

    Initial commit

---
+++ FruitIntoBaskets/JavaScript/FruitIntoBaskets.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/fruit-into-baskets/
+const totalFruit = (fruits) => {
+    return 0;
+};
+// some test cases
+console.log(totalFruit([1, 2, 1])); // 3
+console.log(totalFruit([0, 1, 2, 2])); // 3
+console.log(totalFruit([1, 2, 3, 2, 2])); // 4
---
+++ FruitIntoBaskets/TypeScript/FruitIntoBaskets.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/fruit-into-baskets/
+const totalFruit = (fruits: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(totalFruit([1,2,1])); // 3
+console.log(totalFruit([0,1,2,2])); // 3
+console.log(totalFruit([1,2,3,2,2])); // 4
