098ef793321a4732f3aab69d246ff1803629155e
Jason Mauss <jason.mauss@gmail.com>
2022-03-07 22:31:02 -0700

    Initial Commit

---
+++ FurthestBuildingYouCanReach/TypeScript/FurthestBuildingYouCanReach.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/furthest-building-you-can-reach/
+const furthestBuilding = (heights: number[], bricks: number, ladders: number): number => {
+
+    return 0;
+
+};
+
+
+// some test cases
+console.log(furthestBuilding([4,12,2,7,3,18,20,3,19], 10, 2)); // 7
+console.log(furthestBuilding([14,3,19,3], 17, 0)); // 3
