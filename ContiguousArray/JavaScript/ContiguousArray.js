0e3845f6a178cace63ceab04f6f4fcfbb9085c8b
Jason Mauss <jason.mauss@gmail.com>
2022-03-14 23:29:30 -0700

    Adding Solutions for TypeScript and JavaScript

---
+++ ContiguousArray/JavaScript/ContiguousArray.js
@@ -0,0 +1,24 @@
+// Solution for: https://leetcode.com/problems/contiguous-array/
+function findMaxLength(nums) {
+    let max = 0;
+    let count = {};
+    let discrepency = 0;
+    let ind = 0;
+    if (!Array.isArray(nums))
+        return;
+    while (ind < nums.length) {
+        discrepency += (nums[ind] == 1 ? 1 : -1);
+        if (discrepency === 0)
+            max = Math.max(max, ind + 1);
+        else if (discrepency in count)
+            max = Math.max(max, ind - count[discrepency]);
+        else
+            count[discrepency] = ind;
+        ind++;
+    }
+    return max;
+}
+;
+// some test cases
+console.log(findMaxLength([0, 1])); // 2
+console.log(findMaxLength([0, 1, 0])); // 2
