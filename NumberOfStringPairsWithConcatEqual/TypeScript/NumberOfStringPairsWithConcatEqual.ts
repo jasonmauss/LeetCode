7b437a43fad18fc5effb53ab7b8efca5dd008880
Jason Mauss <jason.mauss@gmail.com>
2022-03-19 23:43:44 -0700

    Adding Solutions for TypeScript and JavaScript

--- NumberOfStringPairsWithConcatEqual/TypeScript/NumberOfStringPairsWithConcatEqual.ts
+++ NumberOfStringPairsWithConcatEqual/TypeScript/NumberOfStringPairsWithConcatEqual.ts
@@ -1,12 +1,27 @@
 // Solution for: https://leetcode.com/problems/number-of-pairs-of-strings-with-concatenation-equal-to-target/
 const numOfPairs = (nums: string[], target: string): number => {
 
-    return 0;
+    let count:number = 0;
+    let i:number = 0;
+
+    while (i < nums.length) {
+        for (let j:number = 0; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                count++;
+                if (i === j) {
+                    count -= 1;
+                }
+            }
+        }
+        i++;
+    }
+
+    return count;
 
 };
 
 
 // some test cases
 console.log(numOfPairs(["777","7","77","77"], '7777')); // 4
-console.log(numOfPairs(["123","4","12","34"], '12')); // 2
+console.log(numOfPairs(["123","4","12","34"], '1234')); // 2
 console.log(numOfPairs(["1","1","1"], '11')); // 6
