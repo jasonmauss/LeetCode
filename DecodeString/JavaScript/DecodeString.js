db4c5514aac27cfbd63614155de209e44bf0957b
Jason Mauss <jason.mauss@gmail.com>
2022-04-09 23:53:50 -0700

    Adding Solutions for TypeScript and JavaScript

---
+++ DecodeString/JavaScript/DecodeString.js
@@ -0,0 +1,29 @@
+// Solution for: https://leetcode.com/problems/decode-string/
+const decodeString = (s) => {
+    const stack = [];
+    for (const char of s) {
+        if (char !== "]") {
+            stack.push(char);
+            continue;
+        }
+        let cur = stack.pop();
+        let str = '';
+        while (cur !== '[') {
+            str = cur + str;
+            cur = stack.pop();
+        }
+        let num = '';
+        cur = stack.pop();
+        while (!Number.isNaN(Number(cur))) {
+            num = cur + num;
+            cur = stack.pop();
+        }
+        stack.push(cur);
+        stack.push(str.repeat(Number(num)));
+    }
+    return stack.join('');
+};
+// some test cases
+console.log(decodeString("3[a]2[bc]")); // "aaabcbc"
+console.log(decodeString("3[a2[c]]")); // "accaccacc"
+console.log(decodeString("2[abc]3[cd]ef")); // "abcabccdcdcdef"
