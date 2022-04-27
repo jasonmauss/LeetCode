57575e5612aedf061e67e00728020b9fa82232c5
Jason Mauss <jason.mauss@gmail.com>
2022-04-26 23:58:00 -0700

    initial commit

---
+++ FindAndReplacePattern/JavaScript/FindAndReplacePattern.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/find-and-replace-pattern/
+const findAndReplacePattern = (words, pattern) => {
+    return [''];
+};
+// some test cases
+console.log(findAndReplacePattern(["abc", "deq", "mee", "aqq", "dkd", "ccc"], "abb")); // ["mee","aqq"]
+console.log(findAndReplacePattern(["a", "b", "c"], "a")); //["a","b","c"]
---
+++ FindAndReplacePattern/TypeScript/FindAndReplacePattern.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/find-and-replace-pattern/
+const findAndReplacePattern = (words: string[], pattern: string): string[] => {
+
+    return [''];
+
+};
+
+
+
+// some test cases
+console.log(findAndReplacePattern(["abc","deq","mee","aqq","dkd","ccc"], "abb")); // ["mee","aqq"]
+console.log(findAndReplacePattern(["a","b","c"], "a")); //["a","b","c"]
