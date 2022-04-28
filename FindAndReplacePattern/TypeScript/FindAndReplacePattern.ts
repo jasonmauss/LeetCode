a0a256d4ec3d67dab90c9d5738f4234bbf290826
Jason Mauss <jason.mauss@gmail.com>
2022-04-27 22:45:53 -0700

    Adding Solutions for TypeScript and JavaScript

--- FindAndReplacePattern/TypeScript/FindAndReplacePattern.ts
+++ FindAndReplacePattern/TypeScript/FindAndReplacePattern.ts
@@ -1,11 +1,31 @@
 // Solution for: https://leetcode.com/problems/find-and-replace-pattern/
 const findAndReplacePattern = (words: string[], pattern: string): string[] => {
 
-    return [''];
+    return words.filter((word) => findAndReplacePatternHelper(word, pattern));
 
 };
 
-
+const findAndReplacePatternHelper = (word: string, pattern: string) => {
+    const wHash: { [key: string]: string } = {};
+    const pHash: { [key: string]: string } = {};
+  
+    for (let i = 0; i < pattern.length; i++) {
+      const cW = word[i];
+      const cP = pattern[i];
+  
+      if (
+        (cW in wHash && wHash[cW] !== cP) ||
+        (cP in pHash && pHash[cP] !== cW)
+      ) {
+        return false;
+      }
+  
+      wHash[cW] = cP;
+      pHash[cP] = cW;
+    }
+  
+    return true;
+};
 
 // some test cases
 console.log(findAndReplacePattern(["abc","deq","mee","aqq","dkd","ccc"], "abb")); // ["mee","aqq"]
