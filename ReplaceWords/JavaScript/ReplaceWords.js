483a3905772ae9f84b0d58c4ea73a2b0c4a223cd
Jason Mauss <jason.mauss@gmail.com>
2022-05-02 21:58:01 -0700

    Adding Solutions for TypeScript and JavaScript

---
+++ ReplaceWords/JavaScript/ReplaceWords.js
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/replace-words/
+const replaceWords = (dictionary, sentence) => {
+    const dict = new Set(dictionary);
+    const words = sentence.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 1; j < words[i].length; j++) {
+            if (dict.has(words[i].substring(0, j)))
+                words[i] = words[i].substring(0, j);
+        }
+    }
+    return words.join(' ');
+};
+// some test cases
+console.log(replaceWords(["cat", "bat", "rat"], "the cattle was rattled by the battery")); // "the cat was rat by the bat"
+console.log(replaceWords(["a", "b", "c"], "aadsfasf absbs bbab cadsfafs")); // "a a b c"
