562d54424c5af8eb7fae91c21ebcd466059fa3aa
Jason Mauss <jason.mauss@gmail.com>
2022-05-06 00:00:13 -0700

    initial commit

---
+++ MinimumOperationsToHalveArraySum/JavaScript/MinimumOperationsToHalveArraySum.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+const halveArray = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(halveArray([5, 19, 8, 1])); // 3
+console.log(halveArray([3, 8, 20])); // 3
---
+++ MinimumOperationsToHalveArraySum/TypeScript/MinimumOperationsToHalveArraySum.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+const halveArray = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(halveArray([5,19,8,1])); // 3
+console.log(halveArray([3,8,20])); // 3
