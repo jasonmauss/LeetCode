0bcdd87e825b268c102cae4ef0e42aba3e9ec239
Jason Mauss <jason.mauss@gmail.com>
2022-05-16 22:09:03 -0700

    initial commit

---
+++ SubstringConcatAllWords/TypeScript/SubstringConcatAllWords.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+const findSubstring = (s: string, words: string[]): number[] => {
+
+    return [0];
+
+};
+
+
+
+// some test cases
+console.log(findSubstring("barfoothefoobarman", ["foo","bar"])); // [0,9]
+console.log(findSubstring("wordgoodgoodgoodbestword", ["word","good","best","word"])); // []
+console.log(findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"])); // [6,9,12]
