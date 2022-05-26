49edba7d4725f7d6913b65d40a13f4b05aa15654
Jason Mauss <jason.mauss@gmail.com>
2022-05-25 21:12:36 -0700

    Initial commit

---
+++ EditDistance/JavaScript/EditDistance.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/edit-distance/
+const minDistance = (word1, word2) => {
+    return 0;
+};
+// some test cases
+console.log(minDistance('horse', 'ros')); // 3
+console.log(minDistance('intention', 'execution')); // 5
