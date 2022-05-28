8577350c97facd648a25a2fbf41d3cbb4439bc72
Jason Mauss <jason.mauss@gmail.com>
2022-05-27 23:49:48 -0700

    initial commit

---
+++ ReducingDishes/JavaScript/ReducingDishes.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/reducing-dishes/
+const maxSatisfaction = (satisfaction) => {
+    return 0;
+};
+// some test cases
+console.log(maxSatisfaction([-1, -8, 0, 5, -9])); // 14
+console.log(maxSatisfaction([4, 3, 2])); // 20
+console.log(maxSatisfaction([-1, -4, -5])); // 0
---
+++ ReducingDishes/TypeScript/ReducingDishes.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/reducing-dishes/
+const maxSatisfaction = (satisfaction: number[]): number => {
+
+    return 0;
+
+};
+
+
+// some test cases
+console.log(maxSatisfaction([-1,-8,0,5,-9])); // 14
+console.log(maxSatisfaction([4,3,2])); // 20
+console.log(maxSatisfaction([-1,-4,-5])); // 0
