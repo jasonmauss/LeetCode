4ccbfd3bafc80d90ebbe03d0ddbb64660eb44a9d
Jason Mauss <jason.mauss@gmail.com>
2022-06-06 23:53:14 -0800

    Initial commit

---
+++ EarliestPossibleDayOfFullBloom/TypeScript/EarliestPossibleDayOfFullBloom.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
+const earliestFullBloom = (plantTime: number[], growTime: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(earliestFullBloom([1,4,3],[2,3,1])); // 9
+console.log(earliestFullBloom([1,2,3, 2],[2,1,2,1])); // 9
+console.log(earliestFullBloom([1],[1])); // 2
