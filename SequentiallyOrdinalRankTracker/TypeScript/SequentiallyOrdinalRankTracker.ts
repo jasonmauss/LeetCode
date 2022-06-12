9fcf5936c75c2cc7434f1bc3d10e7df8e20126ce
Jason Mauss <jason.mauss@gmail.com>
2022-06-11 23:57:49 -0700

    initial commit

---
+++ SequentiallyOrdinalRankTracker/TypeScript/SequentiallyOrdinalRankTracker.ts
@@ -0,0 +1,25 @@
+// Solution for: https://leetcode.com/problems/sequentially-ordinal-rank-tracker/
+class SORTracker {
+    constructor() {
+
+    }
+
+    add(name: string, score: number): void {
+
+    }
+
+    get(): string {
+        return '';
+    }
+}
+
+/**
+ * Your SORTracker object will be instantiated and called as such:
+ * var obj = new SORTracker()
+ * obj.add(name,score)
+ * var param_2 = obj.get()
+ */
+
+
+
+// some test cases
