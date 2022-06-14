dcb8da136338c95fbffd3a624c2d7a25c8094911
Jason Mauss <jason.mauss@gmail.com>
2022-06-13 23:58:06 -0800

    additional commit, trying out datastructures-js

--- SequentiallyOrdinalRankTracker/TypeScript/SequentiallyOrdinalRankTracker.ts
+++ SequentiallyOrdinalRankTracker/TypeScript/SequentiallyOrdinalRankTracker.ts
@@ -1,16 +1,43 @@
 // Solution for: https://leetcode.com/problems/sequentially-ordinal-rank-tracker/
-class SORTracker {
-    constructor() {
+import {
+    MinPriorityQueue,
+    MaxPriorityQueue,
+    ICompare,
+    IGetCompareValue
+  } from '@datastructures-js/priority-queue';
 
+interface Item{
+    name:string
+    score:number
+}
+
+class SORTracker {
+
+    count:number;
+    min:MinPriorityQueue<Item>;
+    max:MaxPriorityQueue<Item>;
+
+    constructor() {
+        this.count = 0;
+        const minCompareItems: IGetCompareValue<Item> = (a:Item, b:Item) => {
+            return a.score - b.score || b.name.localeCompare(a.name);
+        };
+
+        this.min = new MinPriorityQueue<Item>( getCompareValue: minCompareItems);
+        this.max = new MaxPriorityQueue({ compare: (a, b) => b[0] - a[0] || a[1].localeCompare(b[1]) })
     }
 
     add(name: string, score: number): void {
+        this.min.enqueue([score, name]);
+        this.max.enqueue(this.min.dequeue());
+    };
 
-    }
+    get():string {
+        let maxElement = this.max.dequeue();
+        this.min.enqueue(maxElement);
 
-    get(): string {
-        return '';
-    }
+        return maxElement[1];
+    };
 }
 
 /**
@@ -19,7 +46,3 @@ class SORTracker {
  * obj.add(name,score)
  * var param_2 = obj.get()
  */
-
-
-
-// some test cases
