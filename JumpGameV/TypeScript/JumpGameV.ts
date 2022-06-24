8f0fc0e6c3465f29401a334bb05cfefdc5a8286f
Jason Mauss <jason.mauss@gmail.com>
2022-06-23 23:16:40 -0700

    initial commit

---
+++ JumpGameV/TypeScript/JumpGameV.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/jump-game-v/
+const maxJumps = (arr: number[], d: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(maxJumps([6,4,14,6,8,13,9,7,10,6,12], 2)); // 3
+console.log(maxJumps([3,3,3,3,3], 3)); // 1
+console.log(maxJumps([7,6,5,4,3,2,1], 1)); // 7
