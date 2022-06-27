2e0e89e65ba066b8abeb7b1f4845d3a0923724b3
Jason Mauss <jason.mauss@gmail.com>
2022-06-26 23:28:42 -0700

    Adding Solutions for TypeScript and JavaScript

--- JumpGameV/TypeScript/JumpGameV.ts
+++ JumpGameV/TypeScript/JumpGameV.ts
@@ -1,7 +1,27 @@
 // Solution for: https://leetcode.com/problems/jump-game-v/
 const maxJumps = (arr: number[], d: number): number => {
 
-    return 0;
+    const helper = (index:number): number => {
+        if (cache[index] === 0) {
+        
+            let max = 0;
+            
+            for (let i = index + 1; i <= index + d && i < arr.length && arr[i] < arr[index]; ++i) {
+                max = Math.max(helper(i), max);
+            }
+
+            for (let i = index - 1; i >= index - d && i >= 0 && arr[i] < arr[index]; --i) {
+                max = Math.max(helper(i), max);
+            }
+
+            cache[index] = max + 1;
+        }
+
+        return cache[index];
+    }
+
+    const cache = new Uint16Array(1001);
+    return Math.max(...(arr.map((v, i) => helper(i))));
 
 };
 
@@ -9,6 +29,6 @@ const maxJumps = (arr: number[], d: number): number => {
 
 
 // some test cases
-console.log(maxJumps([6,4,14,6,8,13,9,7,10,6,12], 2)); // 3
+console.log(maxJumps([6,4,14,6,8,13,9,7,10,6,12], 2)); // 4
 console.log(maxJumps([3,3,3,3,3], 3)); // 1
 console.log(maxJumps([7,6,5,4,3,2,1], 1)); // 7
