101f35ab173b13042b6d3f24286fe08fdd02dfdf
Jason Mauss <jason.mauss@gmail.com>
2022-06-28 23:09:41 -0700

    initial commit

---
+++ NumberOfWaysToPaintGrid/TypeScript/NumberOfWaysToPaintGrid.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/
+const numOfWays = (n: number): number => {
+    
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(numOfWays(1)); // 12
+console.log(numOfWays(5000)); // 30228214
