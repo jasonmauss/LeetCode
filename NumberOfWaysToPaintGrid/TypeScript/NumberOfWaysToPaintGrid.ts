a4123165ec9f6138ae6425f19483848d209eb441
Jason Mauss <jason.mauss@gmail.com>
2022-06-30 00:02:20 -0700

    Adding Solutions for TypeScript and JavaScript

--- NumberOfWaysToPaintGrid/TypeScript/NumberOfWaysToPaintGrid.ts
+++ NumberOfWaysToPaintGrid/TypeScript/NumberOfWaysToPaintGrid.ts
@@ -1,7 +1,20 @@
 // Solution for: https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/
 const numOfWays = (n: number): number => {
     
-    return 0;
+    let colors3 = 6
+    let colors2 = 6
+    let next2, next3
+    
+    for (let i = 2; i <= n; i++) {
+        next2 = 3 * colors2 + 2 * colors3
+
+        next3 = 2 * colors2 + 2 * colors3
+
+        colors2 = next2 % 1000000007
+        colors3 = next3 % 1000000007
+    }  
+
+    return (colors3 + colors2) % 1000000007
 
 };
 
