3d62630dce771c1ad92ca9a8cd3303f50db04713
Jason Mauss <jason.mauss@gmail.com>
2022-06-30 23:59:27 -0700

    Initial Commit

---
+++ ShortestPathVisitingAllNodes/TypeScript/ShortestPathVisitingAllNodes.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/number-of-ways-to-paint-n-3-grid/
+const shortestPathLength = (graph: number[][]): number => {
+
+    return 0;
+
+};
+
+
+
+console.log(shortestPathLength([[1,2,3],[0],[0],[0]])); // 4
+console.log(shortestPathLength([[1],[0,2,4],[1,3,4],[2],[1,2]])); // 4
