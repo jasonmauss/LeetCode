8fbc247132ec67f12b2721574af553ed98d8f644
Jason Mauss <jason.mauss@gmail.com>
2022-07-05 22:40:23 -0700

    Initial commit

---
+++ CherryPickupII/TypeScript/CherryPickupII.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/cherry-pickup-ii/
+const cherryPickup = (grid: number[][]): number => {
+
+    return 0;
+
+};
+
+
+// some test cases
+console.log(cherryPickup([[3,1,1],[2,5,1],[1,5,5],[2,1,1]])); // 24
+console.log(cherryPickup([[1,0,0,0,0,0,1],[2,0,0,0,0,3,0],[2,0,9,0,0,0,0],[0,3,0,5,4,0,0],[1,0,2,3,0,0,6]])); // 28
