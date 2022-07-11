7ed7891f3a36faeb2901d7cf6409aa03802a06c7
Jason Mauss <jason.mauss@gmail.com>
2022-07-10 23:12:44 -0700

    initial commit

---
+++ MyCalendarIII/JavaScript/MyCalendarIII.js
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/my-calendar-iii/
+var MyCalendarThree = /** @class */ (function () {
+    function MyCalendarThree() {
+    }
+    MyCalendarThree.prototype.book = function (start, end) {
+        return 0;
+    };
+    return MyCalendarThree;
+}());
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * var obj = new MyCalendarThree()
+ * var param_1 = obj.book(start,end)
+ */
+// some test cases
+console.log(); // 
