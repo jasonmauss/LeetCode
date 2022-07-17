1eae9d6a98710cea874946799c10b6eca3d974c3
Jason Mauss <jason.mauss@gmail.com>
2022-07-16 21:02:48 -0700

    Adding Solutions for TypeScript and JavaScript

--- PaintHouseIII/JavaScript/PaintHouseIII.js
+++ PaintHouseIII/JavaScript/PaintHouseIII.js
@@ -1,6 +1,59 @@
 // Solution for: https://leetcode.com/problems/paint-house-iii/
+var dp = null;
+var nColors, nHouses;
+var nTarget;
+var minHelp = function (houses, cost, index, previous, hoods) {
+    if (index >= houses.length) {
+        return (hoods == nTarget) ? 0 : Infinity;
+    }
+    if (dp[index][previous][hoods] != -1)
+        return dp[index][previous][hoods];
+    if (nTarget == hoods) {
+        if (houses[index] != previous && houses[index] != 0)
+            return dp[index][previous][hoods] = Infinity;
+        if (houses[index] == previous)
+            return dp[index][previous][hoods] = minHelp(houses, cost, index + 1, previous, hoods);
+        var v_1 = minHelp(houses, cost, index + 1, previous, hoods);
+        if (v_1 != Infinity)
+            v_1 += cost[index][previous - 1];
+        return dp[index][previous][hoods] = v_1;
+    }
+    var answer = Infinity;
+    if (houses[index] == previous) {
+        return dp[index][previous][hoods] = minHelp(houses, cost, index + 1, previous, hoods);
+    }
+    else if (houses[index] != 0) {
+        return dp[index][previous][hoods] = minHelp(houses, cost, index + 1, houses[index], hoods + 1);
+    }
+    var i, v;
+    for (i = 1; i <= nColors; i++) {
+        if (previous == i) {
+            v = minHelp(houses, cost, index + 1, previous, hoods);
+        }
+        else {
+            v = minHelp(houses, cost, index + 1, i, hoods + 1);
+        }
+        if (v == Infinity)
+            continue;
+        v += cost[index][i - 1];
+        answer = Math.min(answer, v);
+    }
+    return dp[index][previous][hoods] = answer;
+};
 var minCost = function (houses, cost, m, n, target) {
-    return 0;
+    nColors = n;
+    nHouses = m;
+    nTarget = target;
+    dp = new Array(nHouses);
+    var i, j;
+    for (i = 0; i < m; i++) {
+        dp[i] = new Array(nColors + 2);
+        for (j = 0; j <= nColors + 1; j++) {
+            dp[i][j] = new Array(target + 1).fill(-1);
+        }
+    }
+    var v = minHelp(houses, cost, 0, nColors + 1, 0);
+    return (v == Infinity) ? -1 : v;
 };
 // some test cases
 console.log(minCost([0, 0, 0, 0, 0], [[1, 10], [10, 1], [10, 1], [1, 10], [5, 1]], 5, 2, 3)); // 9
