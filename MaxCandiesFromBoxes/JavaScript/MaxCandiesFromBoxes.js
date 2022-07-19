f7c61506059c921c36c69fd1ff6c93830328ac99
Jason Mauss <jason.mauss@gmail.com>
2022-07-18 23:03:19 -0700

    Adding Solutions for TypeScript and JavaScript

--- MaxCandiesFromBoxes/JavaScript/MaxCandiesFromBoxes.js
+++ MaxCandiesFromBoxes/JavaScript/MaxCandiesFromBoxes.js
@@ -1,6 +1,28 @@
 // Solution for: https://leetcode.com/problems/maximum-candies-you-can-get-from-boxes/
-var maxCandies = function (status, candies, keys, containedBoxes, initialBoxes) {
-    return 0;
+const maxCandies = (status, candies, keys, containedBoxes, initialBoxes) => {
+    let boxesHeld = new Set(initialBoxes);
+    let candyCount = 0;
+    let eventOccured = true;
+    while (eventOccured) {
+        eventOccured = false;
+        for (let box of boxesHeld) {
+            if (status[box] === 1) {
+                status[box] = 2;
+                boxesHeld.delete(box);
+                eventOccured = true;
+                candyCount += candies[box];
+                for (let key of keys[box]) {
+                    if (status[key] === 0)
+                        status[key] = 1;
+                }
+                for (let innerBox of containedBoxes[box]) {
+                    if (status[innerBox] < 2)
+                        boxesHeld.add(innerBox);
+                }
+            }
+        }
+    }
+    return candyCount;
 };
 // some test cases
 console.log(maxCandies([1, 0, 1, 0], [7, 5, 4, 100], [[], [], [1], []], [[1, 2], [3], [], []], [0])); // 16
