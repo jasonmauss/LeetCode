7e6eea068e606384871a84ce47421fca49b1832a
Jason Mauss <jason.mauss@gmail.com>
2022-07-19 22:11:19 -0700

    Initial commit

---
+++ PalindromePartitioningIII/JavaScript/PalindromePartitioningIII.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/palindrome-partitioning-iii/
+var palindromePartition = function (s, k) {
+    return 0;
+};
+// some test cases
+console.log(palindromePartition("abc", 2)); // 1
+console.log(palindromePartition("aabbc", 3)); // 0
+console.log(palindromePartition("leetcode", 8)); // 0
---
+++ PalindromePartitioningIII/TypeScript/PalindromePartitioningIII.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/palindrome-partitioning-iii/
+const palindromePartition = (s: string, k: number): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(palindromePartition("abc", 2)); // 1
+console.log(palindromePartition("aabbc", 3)); // 0
+console.log(palindromePartition("leetcode", 8)); // 0
