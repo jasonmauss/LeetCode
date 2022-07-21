b2375e74a384cb75af9685a0ff05e32e8d2a584e
Jason Mauss <jason.mauss@gmail.com>
2022-07-20 21:26:36 -0700

    Adding Solutions for TypeScript and JavaScript

--- PalindromePartitioningIII/TypeScript/PalindromePartitioningIII.ts
+++ PalindromePartitioningIII/TypeScript/PalindromePartitioningIII.ts
@@ -1,7 +1,27 @@
 // Solution for: https://leetcode.com/problems/palindrome-partitioning-iii/
 const palindromePartition = (s: string, k: number): number => {
 
-    return 0;
+    const n = s.length;
+    const dp = Array.from({ length: n }, () => Array(k + 1).fill(100)); // k can't be more than 100 in size
+    const cost = Array.from({ length: n }, () => Array(n).fill(0));
+    
+    for (let len = 1; len < n; len++) {
+        for (let i = 0; i < n - len; i++) {
+            const j = i + len;
+            cost[i][j] = cost[i + 1][j - 1] + (s[i] == s[j] ? 0 : 1);
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        dp[i][1] = cost[0][i];
+        for (let kk = 2; kk <= k; kk++) {
+            for (let j = 0; j < i; j++) {
+                dp[i][kk] = Math.min(dp[i][kk], dp[j][kk - 1] + cost[j + 1][i]);
+            }
+        }
+    }
+   
+    return dp[n - 1][k];
 
 };
 
