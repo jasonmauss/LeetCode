787ccac872bcc872f0924547f421b7250d8234f3
Jason Mauss <jason.mauss@gmail.com>
2022-07-22 18:30:10 -0700

    initial commit

---
+++ StoneGameIV/JavaScript/StoneGameIV.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/stone-game-iv/
+var winnerSquareGame = function (n) {
+    return false;
+};
+// some test cases
+console.log(winnerSquareGame(1)); // true
+console.log(winnerSquareGame(2)); // false
+console.log(winnerSquareGame(4)); // true
---
+++ StoneGameIV/TypeScript/StoneGameIV.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/stone-game-iv/
+const winnerSquareGame = (n: number): boolean => {
+
+    return false;
+
+};
+
+
+
+// some test cases
+console.log(winnerSquareGame(1)); // true
+console.log(winnerSquareGame(2)); // false
+console.log(winnerSquareGame(4)); // true
