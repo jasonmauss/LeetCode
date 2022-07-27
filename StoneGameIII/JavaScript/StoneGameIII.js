2bebf3cb5b5db04d2ae74d84ad6278edd55c7d40
Jason Mauss <jason.mauss@gmail.com>
2022-07-26 19:31:28 -0700

    initial commit

---
+++ StoneGameIII/JavaScript/StoneGameIII.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/stone-game-iii/
+var stoneGameIII = function (stoneValue) {
+    return '';
+};
+// some test cases
+console.log(stoneGameIII([1, 2, 3, 7])); // Bob
+console.log(stoneGameIII([1, 2, 3, -9])); // Alice
+console.log(stoneGameIII([1, 2, 3, 6])); // Tie
---
+++ StoneGameIII/TypeScript/StoneGameIII.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/stone-game-iii/
+const stoneGameIII = (stoneValue: number[]): string => {
+
+    return '';
+
+};
+
+
+
+// some test cases
+console.log(stoneGameIII([1,2,3,7])); // Bob
+console.log(stoneGameIII([1,2,3,-9])); // Alice
+console.log(stoneGameIII([1,2,3,6])); // Tie
