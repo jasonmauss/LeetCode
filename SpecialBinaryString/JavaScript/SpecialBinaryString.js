74e04c0899ae7c91fdb0125d12ed82c830e70ba3
Jason Mauss <jason.mauss@gmail.com>
2022-07-28 20:00:35 -0700

    initial commit

---
+++ SpecialBinaryString/JavaScript/SpecialBinaryString.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/special-binary-string/
+var makeLargestSpecial = function (s) {
+    return '';
+};
+// some test cases
+console.log(makeLargestSpecial("11011000")); // "11100100"
+console.log(makeLargestSpecial("10")); // "10"
---
+++ SpecialBinaryString/TypeScript/SpecialBinaryString.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/special-binary-string/
+const makeLargestSpecial = (s: string): string => {
+
+    return '';
+
+};
+
+
+
+
+// some test cases
+console.log(makeLargestSpecial("11011000")); // "11100100"
+console.log(makeLargestSpecial("10")); // "10"
