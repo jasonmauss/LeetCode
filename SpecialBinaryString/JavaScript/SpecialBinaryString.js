106e1228a7e2d5370ad1aa26874c288aefc074bc
Jason Mauss <jason.mauss@gmail.com>
2022-07-29 23:28:20 -0700

    Adding Solutions for TypeScript and JavaScript

--- SpecialBinaryString/JavaScript/SpecialBinaryString.js
+++ SpecialBinaryString/JavaScript/SpecialBinaryString.js
@@ -1,6 +1,23 @@
 // Solution for: https://leetcode.com/problems/special-binary-string/
 var makeLargestSpecial = function (s) {
-    return '';
+    var recurse = function (s) {
+        var i = 0;
+        var result = [];
+        var bal = 0;
+        for (var j = 0; j < s.length; j++) {
+            if (s[j] === '1')
+                bal++;
+            else
+                bal--;
+            if (bal === 0) {
+                result.push('1' + recurse(s.slice(i + 1, j)) + '0');
+                i = j + 1;
+            }
+        }
+        result.sort(function (a, b) { return b.localeCompare(a); });
+        return result.join('');
+    };
+    return recurse(s);
 };
 // some test cases
 console.log(makeLargestSpecial("11011000")); // "11100100"
