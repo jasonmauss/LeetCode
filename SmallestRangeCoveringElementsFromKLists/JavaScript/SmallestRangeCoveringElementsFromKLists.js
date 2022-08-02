d238d4dcf44d097a3fde95e4ab493fe6d07bc9e0
Jason Mauss <jason.mauss@gmail.com>
2022-08-01 21:50:20 -0700

    initial commit

---
+++ SmallestRangeCoveringElementsFromKLists/JavaScript/SmallestRangeCoveringElementsFromKLists.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+const smallestRange = (nums) => {
+    return [0];
+};
+// some test cases
+console.log(smallestRange([[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]])); // [20,24]
+console.log(smallestRange([[1, 2, 3], [1, 2, 3], [1, 2, 3]])); // [1,1]
---
+++ SmallestRangeCoveringElementsFromKLists/TypeScript/SmallestRangeCoveringElementsFromKLists.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+const smallestRange = (nums: number[][]): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+// some test cases
+console.log(smallestRange([[4,10,15,24,26],[0,9,12,20],[5,18,22,30]])); // [20,24]
+console.log(smallestRange([[1,2,3],[1,2,3],[1,2,3]])); // [1,1]
