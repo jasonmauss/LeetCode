db777bd842d456737eaffd4ef4c4299d6a5560d8
Jason Mauss <jason.mauss@gmail.com>
2022-08-09 22:57:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- OrderlyQueue/JavaScript/OrderlyQueue.js
+++ OrderlyQueue/JavaScript/OrderlyQueue.js
@@ -1,4 +1,20 @@
-// Solution for: https://leetcode.com/problems/swim-in-rising-water/
+// Solution for: https://leetcode.com/problems/orderly-queue/
+const orderlyQueue = (s, k) => {
+    if (1 < k) {
+        return [...s].sort().join('');
+    }
+    else {
+        const mapFunction = (a, x) => {
+            if (s.slice(x) + s.slice(0, x) < a) {
+                return s.slice(x) + s.slice(0, x);
+            }
+            else {
+                return a;
+            }
+        };
+        return Array.from({ length: s.length }, (_, i) => i).reduce(mapFunction, s);
+    }
+};
 // some test cases
-console.log(); // 3
-console.log(); // 16
+console.log(orderlyQueue('cba', 1)); // "acb"
+console.log(orderlyQueue('baaca', 3)); // "aaabc"
