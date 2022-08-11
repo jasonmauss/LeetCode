4e2920232176083d68913d98b25c987ffb93f9eb
Jason Mauss <jason.mauss@gmail.com>
2022-08-11 00:14:47 -0700

    initial commit

---
+++ ParsingBooleanExpression/TypeScript/ParsingBooleanExpression.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/parsing-a-boolean-expression/
+const parseBoolExpr = (expression: string): boolean => {
+
+    return false;
+
+};
+
+
+
+
+// some test cases
+console.log(parseBoolExpr("!(f)")); // true
+console.log(parseBoolExpr("|(f,t)")); // true
+console.log(parseBoolExpr("&(t,f)")); // false
