52d1a6bb78205a8cdea051ec80c89a7005e6918e
Jason Mauss <jason.mauss@gmail.com>
2022-08-13 22:34:26 -0700

    initial commit

---
+++ PoorPigs/TypeScript/PoorPigs.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/poor-pigs/
+const poorPigs = (buckets: number, minutesToDie: number, minutesToTest: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(poorPigs(1000, 15, 60)); //  5
+console.log(poorPigs(4, 15, 15)); //  2
+console.log(poorPigs(4, 15, 30)); //  2
