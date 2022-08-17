6cd3e2fb2d3b702495203460cd949f018a27cd56
Jason Mauss <jason.mauss@gmail.com>
2022-08-16 20:56:18 -0700

    Adding Solutions for TypeScript and JavaScript

--- DeleteDuplicateFoldersInSystem/TypeScript/DeleteDuplicateFoldersInSystem.ts
+++ DeleteDuplicateFoldersInSystem/TypeScript/DeleteDuplicateFoldersInSystem.ts
@@ -1,9 +1,118 @@
 // Solution for: https://leetcode.com/problems/delete-duplicate-folders-in-system/
-const deleteDuplicateFolder = (paths: string[][]): string[][] => {
+class PathTrieNode {
 
-    return [''][''];
+    val = '';
+    children = new Map<string, PathTrieNode>();
+    hash = '';
 
-};
+    // Parent pointer is important when we are trying to delete nodes.
+    parent: PathTrieNode | undefined;
+}
+  
+class PathTrie {
+
+    private root = new PathTrieNode();
+    private hashMemMap = new Map<string, PathTrieNode[]>();
+    
+    /**
+    * Builds Trie tree for each path passed in Test Case
+    */
+    build(path: string[]): void {
+        let node = this.root;
+        for (let pathVal of path) {
+            if (!node.children.has(pathVal)) {
+                const childNode = new PathTrieNode();
+                childNode.parent = node;
+                childNode.val = pathVal;
+                node.children.set(childNode.val, childNode);
+                node = childNode;
+            } else {
+                node = node.children.get(pathVal);
+            }
+        }
+    }
+  
+    /**
+    * Creates hash for a node based on it's sub folders
+    *  Note that sorting for subfolder is important.
+    */
+    createHash(node = this.root): string {
+        
+        if (node.children.size === 0) {
+            // leaf node
+            return node.val;
+        }
+
+        let hash = [];
+
+        for (let child of node.children.values()) {
+            hash.push(this.createHash(child));
+        }
+
+        hash = hash.sort();
+        node.hash = hash.join('~');
+
+        if (!this.hashMemMap.has(node.hash)) {
+            this.hashMemMap.set(node.hash, [])
+        }
+
+        this.hashMemMap.get(node.hash).push(node);
+        return node.val + '[' + node.hash + ']';
+    }
+    
+    /**
+    * Deletes similar folders as map would contain more than one entries for those.
+    * For deleting a node, we go its parent, and delete parent's connection to child.
+    */
+    removeDuplicates(node = this.root): void {
+
+        if (this.hashMemMap.has(node.hash) && this.hashMemMap.get(node.hash).length > 1) {
+            for (let deleteNode of this.hashMemMap.get(node.hash)) {
+                deleteNode.parent.children.delete(deleteNode.val);
+            }
+        }
+
+        for (let child of node.children.values()) {
+            this.removeDuplicates(child);
+        }
+    }
+    
+    /**
+    * Stack is used to track folder structure (dfs kind of thing)
+    * Res contains actual items.
+    */
+    toArray(node = this.root, stack = [], result = []): string[][] {
+
+        if (node.val !== '') {
+            stack.push(node.val);
+            result.push([...stack]);
+        }
+
+        for (let child of node.children.values()) {
+            this.toArray(child, stack, result);
+        }
+
+        stack.pop();
+        return result;
+
+    }
+    
+}
+  
+  const deleteDuplicateFolder = (paths: string[][]): string[][] => {
+
+    const pathTrie = new PathTrie();
+
+    for (let path of paths) {
+        pathTrie.build(path);
+    }
+
+    pathTrie.createHash();
+    pathTrie.removeDuplicates();
+
+    return pathTrie.toArray();
+
+  };
 
 
 
