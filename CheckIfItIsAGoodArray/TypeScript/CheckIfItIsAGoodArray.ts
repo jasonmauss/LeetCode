80e1252a725b0283c46e5bb2110e653d92b5114a
Jason Mauss <jason.mauss@gmail.com>
2022-08-17 18:05:10 -0700

    initial commit

---
+++ CheckIfItIsAGoodArray/TypeScript/CheckIfItIsAGoodArray.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/check-if-it-is-a-good-array/
+const isGoodArray = (nums: number[]): boolean => {
+
+    return false;
+
+};
+
+
+
+// some test cases
+console.log(isGoodArray([12,5,7,23])); // true
+console.log(isGoodArray([29,6,10])); // true
+console.log(isGoodArray([3,6])); // false
