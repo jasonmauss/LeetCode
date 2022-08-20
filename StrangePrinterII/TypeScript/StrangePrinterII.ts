9df97571f1b5455650dd6fe1e62062dd8a00e448
Jason Mauss <jason.mauss@gmail.com>
2022-08-19 19:03:33 -0700

    initial commit

---
+++ StrangePrinterII/TypeScript/StrangePrinterII.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/strange-printer-ii/
+const isPrintable = (targetGrid: number[][]): boolean => {
+
+    return false;
+
+};
+
+
+
+
+// some test cases
+console.log(isPrintable([[1,1,1,1],[1,2,2,1],[1,2,2,1],[1,1,1,1]])); // true
+console.log(isPrintable([[1,1,1,1],[1,1,3,3],[1,1,3,4],[5,5,1,4]])); // true
+console.log(isPrintable([[1,2,1],[2,1,2],[1,2,1]])); // false
