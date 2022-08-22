1bacb306b48701b1eed9e04a49321ad7211dc197
Jason Mauss <jason.mauss@gmail.com>
2022-08-21 23:06:05 -0800

    initial commit

---
+++ MaxPathQualityOfGraph/JavaScript/MaxPathQualityOfGraph.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/maximum-path-quality-of-a-graph/
+const maximalPathQuality = (values, edges, maxTime) => {
+    return 0;
+};
+// some test cases
+console.log(maximalPathQuality([0, 32, 10, 43], [[0, 1, 10], [1, 2, 15], [0, 3, 10]], 49)); // 75
+console.log(maximalPathQuality([5, 10, 15, 20], [[0, 1, 10], [1, 2, 10], [0, 3, 10]], 30)); // 25
+console.log(maximalPathQuality([1, 2, 3, 4], [[0, 1, 10], [1, 2, 11], [2, 3, 12], [1, 3, 13]], 50)); // 7
