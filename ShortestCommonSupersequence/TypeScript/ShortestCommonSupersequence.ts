108d0ae2a639b264fd223341a1fad5b7148e21c9
Jason Mauss <jason.mauss@gmail.com>
2022-08-23 23:32:48 -0700

    Initial commit

---
+++ ShortestCommonSupersequence/TypeScript/ShortestCommonSupersequence.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/shortest-common-supersequence/
+const shortestCommonSupersequence = (str1: string, str2: string): string => {
+
+    return '';
+
+};
+
+
+
+
+// some test cases
+console.log(shortestCommonSupersequence('abac', 'cab')); // "cabac"
+console.log(shortestCommonSupersequence('aaaaaaaa', 'aaaaaaaa')); // "aaaaaaaa"
