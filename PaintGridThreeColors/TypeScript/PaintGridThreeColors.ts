633f76e02fb9abeafa8abc1101868d9662236acb
Jason Mauss <jason.mauss@gmail.com>
2022-08-25 21:03:14 -0700

    initial commit

---
+++ PaintGridThreeColors/TypeScript/PaintGridThreeColors.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/painting-a-grid-with-three-different-colors
+const colorTheGrid = (m: number, n: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(colorTheGrid(1, 1)); // 3
+console.log(colorTheGrid(1, 2)); // 6
+console.log(colorTheGrid(5, 5)); // 580986
