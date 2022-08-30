c63ba275b8fe19ab96ed4622c552bb2d9d32f478
Jason Mauss <jason.mauss@gmail.com>
2022-08-29 18:57:00 -0700

    initial commit

---
+++ BurstBalloons/JavaScript/BurstBalloons.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/burst-balloons/
+const maxCoins = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(maxCoins([3, 1, 5, 8])); // 167
+console.log(maxCoins([1, 5])); // 10
