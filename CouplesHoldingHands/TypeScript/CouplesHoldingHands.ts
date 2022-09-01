4045ebf3a0777af07f805476eccbeafcb3bdc988
Jason Mauss <jason.mauss@gmail.com>
2022-08-31 21:44:29 -0700

    initial commit

---
+++ CouplesHoldingHands/TypeScript/CouplesHoldingHands.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/couples-holding-hands/
+function minSwapsCouples(row: number[]): number {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(minSwapsCouples([0,2,1,3])); // 1
+console.log(minSwapsCouples([3,2,0,1])); // 0
