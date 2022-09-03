c3e2a06c9cd57ccae3d17b113cf189579cdab72d
Jason Mauss <jason.mauss@gmail.com>
2022-09-02 22:24:45 -0700

    initial commit

---
+++ DistinctRollSequences/TypeScript/DistinctRollSequences.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/number-of-distinct-roll-sequences/
+const distinctSequences = (n: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(distinctSequences(4)); // 184
+console.log(distinctSequences(2)); // 22
