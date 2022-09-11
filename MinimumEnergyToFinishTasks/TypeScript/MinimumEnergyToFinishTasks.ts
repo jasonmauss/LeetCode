8c949b01eb06dde0e2f7ba90872fb880198f8ac6
Jason Mauss <jason.mauss@gmail.com>
2022-09-10 22:41:27 -0700

    initial commit

---
+++ MinimumEnergyToFinishTasks/TypeScript/MinimumEnergyToFinishTasks.ts
@@ -0,0 +1,15 @@
+// Solution for:https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/
+const minimumEffort = (tasks: number[][]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(minimumEffort([[1,2],[2,4],[4,8]])); // 8
+console.log(minimumEffort([[1,3],[2,4],[10,11],[10,12],[8,9]])); // 32
+console.log(minimumEffort([[1,7],[2,8],[3,9],[4,10],[5,11],[6,12]])); // 27
