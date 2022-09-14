1ab9a3321f8242a015a7b841240fb11743623e8a
Jason Mauss <jason.mauss@gmail.com>
2022-09-13 17:37:33 -0700

    initial commit

---
+++ CrackingTheSafe/JavaScript/CrackingTheSafe.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/cracking-the-safe/
+function crackSafe(n, k) {
+    return '';
+}
+;
+// some test cases
+console.log(crackSafe(1, 2)); //  10
+console.log(crackSafe(2, 2)); //  01100
---
+++ CrackingTheSafe/TypeScript/CrackingTheSafe.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/cracking-the-safe/
+function crackSafe(n: number, k: number): string {
+
+    return '';
+
+};
+
+
+
+
+// some test cases
+console.log(crackSafe(1, 2)); //  10
+console.log(crackSafe(2, 2)); //  01100
