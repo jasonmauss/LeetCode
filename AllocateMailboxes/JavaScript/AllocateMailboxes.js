15fe04d4b23eea7e7ef75bf5c74f179665112d1a
Jason Mauss <jason.mauss@gmail.com>
2022-09-15 23:45:35 -0700

    initial commit

---
+++ AllocateMailboxes/JavaScript/AllocateMailboxes.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/allocate-mailboxes/
+const minDistance = (houses, k) => {
+    return 0;
+};
+// some test cases
+console.log(minDistance([1, 4, 8, 10, 20], 3)); // 5
+console.log(minDistance([2, 3, 5, 12, 18], 2)); // 9
---
+++ AllocateMailboxes/TypeScript/AllocateMailboxes.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/allocate-mailboxes/
+const minDistance = (houses: number[], k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(minDistance([1,4,8,10,20], 3)); // 5
+console.log(minDistance([2,3,5,12,18], 2)); // 9
