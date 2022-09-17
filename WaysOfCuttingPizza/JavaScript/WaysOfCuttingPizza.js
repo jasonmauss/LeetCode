152d42b2b40baa8eee1200bcce50afd9c8fe01d7
Jason Mauss <jason.mauss@gmail.com>
2022-09-17 13:46:29 -0700

    initial commit

---
+++ WaysOfCuttingPizza/JavaScript/WaysOfCuttingPizza.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/number-of-ways-of-cutting-a-pizza/
+const ways = (pizza, k) => {
+    return 0;
+};
+// some test cases
+console.log(ways(["A..", "AAA", "..."], 3)); // 3
+console.log(ways(["A..", "AA.", "..."], 3)); // 1
+console.log(ways(["A..", "A..", "..."], 3)); // 1
---
+++ WaysOfCuttingPizza/TypeScript/WaysOfCuttingPizza.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/number-of-ways-of-cutting-a-pizza/
+const ways = (pizza: string[], k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(ways(["A..","AAA","..."], 3)); // 3
+console.log(ways(["A..","AA.","..."], 3)); // 1
+console.log(ways(["A..","A..","..."], 3)); // 1
