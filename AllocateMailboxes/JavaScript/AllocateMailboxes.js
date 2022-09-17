0289ddd25ab173bead5dff7ca1979c16c4a29da6
Jason Mauss <jason.mauss@gmail.com>
2022-09-16 22:32:29 -0700

    Adding Solutions for TypeScript and JavaScript

--- AllocateMailboxes/JavaScript/AllocateMailboxes.js
+++ AllocateMailboxes/JavaScript/AllocateMailboxes.js
@@ -1,6 +1,37 @@
 // Solution for: https://leetcode.com/problems/allocate-mailboxes/
 const minDistance = (houses, k) => {
-    return 0;
+    houses.sort((b, a) => b - a);
+    const numHouses = houses.length;
+    let dp = Array.from({ length: numHouses }, _ => Array(k + 1)), prefixSum = [0];
+    for (let i = 0; i < numHouses; i++) {
+        prefixSum[i + 1] = prefixSum[i] + houses[i];
+    }
+    const minDistanceForHouses = (start, end) => {
+        const mid = Math.trunc((start + end) / 2);
+        return (prefixSum[end] -
+            prefixSum[mid] -
+            prefixSum[mid - ((end - start + 1) % 2)] +
+            prefixSum[start - 1]);
+    };
+    const partition = (index, mailboxes) => {
+        if (dp[index][mailboxes] === undefined) {
+            if (numHouses - index === mailboxes) {
+                dp[index][mailboxes] = 0;
+            }
+            else if (mailboxes === 1) {
+                dp[index][mailboxes] = minDistanceForHouses(index + 1, numHouses);
+            }
+            else {
+                dp[index][mailboxes] = Number.MAX_SAFE_INTEGER;
+                for (let i = index; i <= numHouses - mailboxes; i++) {
+                    dp[index][mailboxes] = Math.min(dp[index][mailboxes], minDistanceForHouses(index + 1, i + 1) +
+                        partition(i + 1, mailboxes - 1));
+                }
+            }
+        }
+        return dp[index][mailboxes];
+    };
+    return partition(0, k);
 };
 // some test cases
 console.log(minDistance([1, 4, 8, 10, 20], 3)); // 5
