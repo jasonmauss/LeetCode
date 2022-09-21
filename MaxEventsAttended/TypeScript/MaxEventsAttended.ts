dbfe517765d1cf0bce925927b4d647bef371ceac
Jason Mauss <jason.mauss@gmail.com>
2022-09-21 01:30:21 -0700

    initial commit

---
+++ MaxEventsAttended/TypeScript/MaxEventsAttended.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended-ii/
+const maxValue = (events: number[][], k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(maxValue([[1,2,4],[3,4,3],[2,3,1]], 2)); // 7
+console.log(maxValue([[1,2,4],[3,4,3],[2,3,10]], 2)); // 10
+console.log(maxValue([[1,1,1],[2,2,2],[3,3,3],[4,4,4]], 3)); // 9
