0e5323646bb831de7d130a4e86a4478f8972d03a
Jason Mauss <jason.mauss@gmail.com>
2022-09-23 23:21:07 -0700

    initial commit

---
+++ CarFleetII/TypeScript/CarFleetII.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/car-fleet-ii/
+const getCollisionTimes = (cars: number[][]): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+// some test cases
+console.log(getCollisionTimes([[1,2],[2,1],[4,3],[7,2]])); // [1.00000,-1.00000,3.00000,-1.00000]
+console.log(getCollisionTimes([[3,4],[5,4],[6,3],[9,1]])); // [2.00000,1.00000,1.50000,-1.00000]
