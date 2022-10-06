b479dbb524d9781758ff979532e1a43aa5df7847
Jason Mauss <jason.mauss@gmail.com>
2022-10-06 10:55:57 -0500

    initial commit

---
+++ BuildingBoxes/TypeScript/BuildingBoxes.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/building-boxes/
+const minimumBoxes = (n: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(minimumBoxes(3)); // 3
+console.log(minimumBoxes(4)); // 3
+console.log(minimumBoxes(10)); // 6
