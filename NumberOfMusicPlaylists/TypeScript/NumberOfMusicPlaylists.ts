5e27c305588ea36fbd751aa13030eaade28d2ecf
Jason Mauss <jason.mauss@gmail.com>
2022-10-15 23:55:10 -0500

    initial commit

---
+++ NumberOfMusicPlaylists/TypeScript/NumberOfMusicPlaylists.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/number-of-music-playlists/
+const numMusicPlaylists = (n: number, goal: number, k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(numMusicPlaylists(3,3,1)); // 6
+console.log(numMusicPlaylists(2,3,0)); // 6
+console.log(numMusicPlaylists(2,3,1)); // 2
