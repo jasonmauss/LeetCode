c13a2b26dba418dc7a4f3521184c6aacbdf89423
Jason Mauss <jason.mauss@gmail.com>
2022-10-21 08:49:06 -0500

    initial commit

---
+++ 24Game/JavaScript/24Game.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/24-game/
+const judgePoint24 = (cards) => {
+    return false;
+};
+// some test cases
+console.log(judgePoint24([4, 1, 8, 7])); // true
+console.log(judgePoint24([1, 2, 1, 2])); // false
