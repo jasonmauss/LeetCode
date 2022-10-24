eb086c4deb531453d2b889cef3f43e2df2877bf3
Jason Mauss <jason.mauss@gmail.com>
2022-10-23 23:02:47 -0500

    initial commit

---
+++ DiceRollSimulation/TypeScript/DiceRollSimulation.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/dice-roll-simulation/
+const dieSimulator = (n: number, rollMax: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(dieSimulator(2, [1,1,2,2,2,3])); // 34
+console.log(dieSimulator(2, [1,1,1,1,1,1])); // 30
+console.log(dieSimulator(3, [1,1,1,2,2,3])); // 181
