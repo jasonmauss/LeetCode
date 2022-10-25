bfc799eff34d1809aae5b0d7dfdfb135e1c45323
Jason Mauss <jason.mauss@gmail.com>
2022-10-25 10:06:25 -0500

    initial commit

---
+++ MaxStudentsTakingExam/JavaScript/MaxStudentsTakingExam.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/maximum-students-taking-exam/
+const maxStudents = (seats) => {
+    return 0;
+};
+// some test cases
+console.log(maxStudents([["#", ".", "#", "#", ".", "#"], [".", "#", "#", "#", "#", "."], ["#", ".", "#", "#", ".", "#"]])); // 4
+console.log(maxStudents([[".", "#"], ["#", "#"], ["#", "."], ["#", "#"], [".", "#"]])); // 3
+console.log(maxStudents([["#", ".", ".", ".", "#"], [".", "#", ".", "#", "."], [".", ".", "#", ".", "."], [".", "#", ".", "#", "."], ["#", ".", ".", ".", "#"]])); // 10
---
+++ MaxStudentsTakingExam/TypeScript/MaxStudentsTakingExam.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/maximum-students-taking-exam/
+const maxStudents = (seats: string[][]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(maxStudents([["#",".","#","#",".","#"],[".","#","#","#","#","."],["#",".","#","#",".","#"]])); // 4
+console.log(maxStudents([[".","#"],["#","#"],["#","."],["#","#"],[".","#"]])); // 3
+console.log(maxStudents([["#",".",".",".","#"],[".","#",".","#","."],[".",".","#",".","."],[".","#",".","#","."],["#",".",".",".","#"]])); // 10
