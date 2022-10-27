eebb97749fe6b79a8721091ea792bbeefb554a57
Jason Mauss <jason.mauss@gmail.com>
2022-10-27 12:22:13 -0500

    initial commit

---
+++ RemoveBoxes/JavaScript/RemoveBoxes.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/remove-boxes/
+const removeBoxes = (boxes) => {
+    return 0;
+};
+// some test cases
+console.log(removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1])); // 23
+console.log(removeBoxes([1, 1, 1])); // 9
+console.log(removeBoxes([1])); // 1
