64e611f50ac4f5fb7e7c89984112f3c0f0b241a8
Jason Mauss <jason.mauss@gmail.com>
2022-10-28 12:07:17 -0500

    Adding Solutions for TypeScript and JavaScript

--- RemoveBoxes/JavaScript/RemoveBoxes.js
+++ RemoveBoxes/JavaScript/RemoveBoxes.js
@@ -1,6 +1,37 @@
 // Solution for: https://leetcode.com/problems/remove-boxes/
 const removeBoxes = (boxes) => {
-    return 0;
+    const merged = [];
+    const points = [];
+    let count = 1;
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i] !== boxes[i + 1]) {
+            merged.push(boxes[i]);
+            points.push(count);
+            count = 1;
+            continue;
+        }
+        count++;
+    }
+    const size = merged.length;
+    const dp = Array.from({ length: size }, () => Array.from({ length: size }, () => new Uint16Array(boxes.length)));
+    const calculate = (left, right, p) => {
+        if (left > right) {
+            return 0;
+        }
+        if (dp[left][right][p]) {
+            return dp[left][right][p];
+        }
+        let point = points[left] + p;
+        let max = point * point + calculate(left + 1, right, 0);
+        for (let i = left + 1; i <= right; i++) {
+            if (merged[i] === merged[left]) {
+                max = Math.max(max, calculate(left + 1, i - 1, 0) + calculate(i, right, point));
+            }
+        }
+        dp[left][right][p] = max;
+        return max;
+    };
+    return calculate(0, size - 1, 0);
 };
 // some test cases
 console.log(removeBoxes([1, 3, 2, 2, 2, 3, 4, 3, 1])); // 23
