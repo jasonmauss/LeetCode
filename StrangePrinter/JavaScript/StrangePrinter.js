bd1b2cc5991e800c5db52950ce541c8a94e09cbf
Jason Mauss <jason.mauss@gmail.com>
2022-10-31 15:41:54 -0500

    Adding Solutions for TypeScript and JavaScript

---
+++ StrangePrinter/JavaScript/StrangePrinter.js
@@ -0,0 +1,33 @@
+// Solution for: https://leetcode.com/problems/strange-printer/
+const strangePrinter = (s) => {
+    let dp = Array(s.length + 1).fill(null).map(a => Array(s.length + 1).fill(-1));
+    const check = (left, right) => {
+        if (dp[left][right] > -1) {
+            return dp[left][right];
+        }
+        if (left == right) {
+            return 1;
+        }
+        else if (left > right) {
+            return 0;
+        }
+        const target = s[left];
+        let out = s.length;
+        for (let i = left; i <= right; i++) {
+            if (s[i] === target) {
+                if (i == left) {
+                    out = 1 + check(left + 1, right);
+                }
+                else {
+                    out = Math.min(out, check(left, i - 1) + check(i + 1, right));
+                }
+            }
+        }
+        dp[left][right] = out;
+        return out;
+    };
+    return check(0, s.length - 1);
+};
+// some test cases
+console.log(strangePrinter("aaabbb")); // 2
+console.log(strangePrinter("aba")); // 2
