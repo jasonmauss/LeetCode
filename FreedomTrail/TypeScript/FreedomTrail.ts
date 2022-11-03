4229f02a19e6c0878a1c9a570147212ecdd4b159
Jason Mauss <jason.mauss@gmail.com>
2022-11-03 16:42:00 -0700

    Adding Solutions for TypeScript and JavaScript

--- FreedomTrail/TypeScript/FreedomTrail.ts
+++ FreedomTrail/TypeScript/FreedomTrail.ts
@@ -1,7 +1,38 @@
 // Solution for: https://leetcode.com/problems/freedom-trail/
-const findRotateSteps = (ring: string, key: string): number => {
+const findRotateSteps = (ring: string, key: string, keyIndex:number = 0, currentRingIndex:number = 0, memo:object = {}): number => {
 
-    return 0;
+    let ckey = `${keyIndex},${currentRingIndex}`;
+    if(ckey in memo) {
+        return memo[ckey];
+    }
+
+    let letter = key[keyIndex];
+    let clockwiseIndex = ring.indexOf(letter, currentRingIndex);
+    let clockwiseDistance;
+    if(clockwiseIndex < 0 ){ 
+        clockwiseIndex = ring.indexOf(letter);
+        clockwiseDistance = ring.length - currentRingIndex + clockwiseIndex;
+    }else{
+        clockwiseDistance = clockwiseIndex - currentRingIndex;
+    }
+    
+    let antiClockIndex = ring.lastIndexOf(letter, currentRingIndex);
+    let antiClockDistance;
+    if(antiClockIndex < 0) {
+        antiClockIndex = ring.lastIndexOf(letter);
+        antiClockDistance = currentRingIndex + ring.length - antiClockIndex;
+    } else {
+        antiClockDistance = currentRingIndex - antiClockIndex;
+    }
+    
+    if(keyIndex === key.length-1) {
+        memo[ckey] = 1+ Math.min(clockwiseDistance, antiClockDistance);
+        return memo[ckey];
+    }
+    
+    memo[ckey] = 1 + Math.min(clockwiseDistance + findRotateSteps(ring, key, keyIndex+1, clockwiseIndex, memo), 
+                                antiClockDistance + findRotateSteps(ring, key, keyIndex+1, antiClockIndex, memo));
+    return memo[ckey];
 
 };
 
