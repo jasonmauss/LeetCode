fa877ade925db4bb45a5f860241566cbcbff841f
Jason Mauss <jason.mauss@gmail.com>
2022-11-02 23:25:03 -0700

    initial commit

---
+++ FreedomTrail/TypeScript/FreedomTrail.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/freedom-trail/
+const findRotateSteps = (ring: string, key: string): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(findRotateSteps('godding','gd')); // 4
+console.log(findRotateSteps('godding','godding')); // 13
