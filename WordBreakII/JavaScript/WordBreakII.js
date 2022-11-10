9fe087cccdacf3c0735f83f157f268ff99179ce6
Jason Mauss <jason.mauss@gmail.com>
2022-11-09 19:33:56 -0700

    Adding Solutions for TypeScript and JavaScript

--- WordBreakII/JavaScript/WordBreakII.js
+++ WordBreakII/JavaScript/WordBreakII.js
@@ -1,6 +1,29 @@
 // Solution for: https://leetcode.com/problems/word-break-ii/
+const dfs = (s, set, memo) => {
+    if (memo.has(s)) {
+        return memo.get(s);
+    }
+    let i = 1;
+    const result = [];
+    while (i < s.length + 1) {
+        const thisWord = s.substring(0, i);
+        if (set.has(thisWord)) {
+            if (i === s.length) {
+                result.push(thisWord);
+                break;
+            }
+            const nextCombos = dfs(s.substring(i), set, memo);
+            nextCombos.forEach(combo => {
+                result.push(`${thisWord} ${combo}`);
+            });
+        }
+        i++;
+    }
+    memo.set(s, result);
+    return result;
+};
 const wordBreak = (s, wordDict) => {
-    return [''];
+    return dfs(s, new Set(wordDict), new Map);
 };
 // some test cases
 console.log(wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"])); // ["cats and dog","cat sand dog"]
--- WordBreakII/TypeScript/WordBreakII.ts
+++ WordBreakII/TypeScript/WordBreakII.ts
@@ -1,7 +1,40 @@
 // Solution for: https://leetcode.com/problems/word-break-ii/
+const dfs = (s:string, set:Set<string>, memo:Map<string, string[]>):string[] => {
+    
+    if (memo.has(s)) {
+        return memo.get(s);
+    }
+    
+    let i = 1;
+    const result = [];
+    
+    while (i < s.length + 1) {
+
+        const thisWord = s.substring(0, i);
+        
+        if (set.has(thisWord)) {
+            if (i === s.length) {
+                result.push(thisWord);
+                break;
+            }
+            
+            const nextCombos = dfs(s.substring(i), set, memo);
+            nextCombos.forEach(combo => {
+                result.push(`${thisWord} ${combo}`);    
+            });
+        }
+        
+        i++;
+    }
+    
+    memo.set(s, result);
+    
+    return result;
+}
+
 const wordBreak = (s: string, wordDict: string[]): string[] => {
 
-    return [''];
+    return dfs(s, new Set(wordDict), new Map);
 
 };
 
