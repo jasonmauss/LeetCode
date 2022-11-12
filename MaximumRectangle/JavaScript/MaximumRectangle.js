bb63e88e8a29af04a8cfd16b2d71265149660f65
Jason Mauss <jason.mauss@gmail.com>
2022-11-11 19:26:44 -0700

    initial commit

---
+++ MaximumRectangle/JavaScript/MaximumRectangle.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/maximal-rectangle/
+const maximalRectangle = (matrix) => {
+    return 0;
+};
+// some test cases
+console.log(maximalRectangle([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]])); // 6
+console.log(maximalRectangle([["0"]])); // 0
+console.log(maximalRectangle([["1"]])); // 1
