1cf9254922ed4c587794af376efbdbedd26de5cf
Jason Mauss <jason.mauss@gmail.com>
2022-11-13 18:29:17 -0700

    initial commit

---
+++ JumpGameIV/TypeScript/JumpGameIV.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/jump-game-iv/
+const minJumps = (arr: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(minJumps([100,-23,-23,404,100,23,23,23,3,404])); // 3
+console.log(minJumps([7])); // 0
+console.log(minJumps([7,6,9,6,9,6,9,7])); // 1
