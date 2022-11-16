adfc48cfa302348c08f62775c61f7ed4e3fa93ec
Jason Mauss <jason.mauss@gmail.com>
2022-11-15 20:38:43 -0700

    initial commit

---
+++ DistinctSubsequences/JavaScript/DistinctSubsequences.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/distinct-subsequences/
+const numDistinct = (s, t) => {
+    return 0;
+};
+// some test cases
+console.log(numDistinct("rabbbit", "rabbit")); // 3
+console.log(numDistinct("babgbag", "bag")); // 5
