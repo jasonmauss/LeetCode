bcb1fbdfca1f67d84af800a694f0e3195ba869fa
Jason Mauss <jason.mauss@gmail.com>
2022-11-24 13:21:29 -0700

    initial commit

---
+++ FindAllGoodStrings/TypeScript/FindAllGoodStrings.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/find-all-good-strings/
+const findGoodStrings = (n: number, s1: string, s2: string, evil: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(findGoodStrings(2,'aa','da','b')); // 51
+console.log(findGoodStrings(8,'leetcode','leetgoes','leet')); // 0
+console.log(findGoodStrings(2,'gx','gz','x')); // 2
