1ea0db9c574a3d47e5d001521b9171ac8aff4722
Jason Mauss <jason.mauss@gmail.com>
2022-11-26 13:46:31 -0700

    initial commit

---
+++ GuessTheWord/TypeScript/GuessTheWord.ts
@@ -0,0 +1,20 @@
+// Solution for: https://leetcode.com/problems/guess-the-word/
+
+// This is the Master's API interface.
+// You should not implement it, or speculate about its implementation
+//
+class Master {
+    guess(word: string): number {
+        return 0;
+    }
+}
+
+function findSecretWord(words: string[], master: Master) {
+
+};
+
+
+
+
+// some test cases  
+console.log(findSecretWord([], new Master())); // 
