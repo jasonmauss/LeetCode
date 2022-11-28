b0736c454fb97e07c5df1cc8b20a7cbbc41aeab5
Jason Mauss <jason.mauss@gmail.com>
2022-11-27 18:09:17 -0700

    Adding Solutions for TypeScript and JavaScript

--- GuessTheWord/JavaScript/GuessTheWord.js
+++ GuessTheWord/JavaScript/GuessTheWord.js
@@ -7,8 +7,47 @@ class Master {
         return 0;
     }
 }
-function findSecretWord(words, master) {
-}
-;
+const findSecretWord = (words, master) => {
+    const getMostCommonWord = (words) => {
+        const count = new Array(6).fill(new Array(26).fill(0));
+        for (let word of words) {
+            for (let [idx, char] of Object.entries(word)) {
+                count[idx][char.toString().charCodeAt(0) - 97]++;
+            }
+        }
+        let mostCommonWord = "";
+        let maxScore = 0;
+        for (let word of words) {
+            let score = 0;
+            for (let [idx, char] of Object.entries(word)) {
+                score += count[idx][char.toString().charCodeAt(0) - 97];
+            }
+            if (score > maxScore) {
+                maxScore = score;
+                mostCommonWord = word;
+            }
+        }
+        return mostCommonWord;
+    };
+    const getNewWordlist = (wordlist, target, cnt) => {
+        return wordlist.filter(word => {
+            let c = 0;
+            for (let [idx, char] of Object.entries(word)) {
+                if (char === target[idx])
+                    c++;
+            }
+            if (c === cnt)
+                return true;
+            return false;
+        });
+    };
+    while (words.length > 0) {
+        const mostCommonWord = getMostCommonWord(words);
+        const cnt = master.guess(mostCommonWord);
+        if (cnt === 6)
+            return;
+        words = getNewWordlist(words, mostCommonWord, cnt);
+    }
+};
 // some test cases  
 console.log(findSecretWord([], new Master())); // 
--- GuessTheWord/TypeScript/GuessTheWord.ts
+++ GuessTheWord/TypeScript/GuessTheWord.ts
@@ -9,8 +9,49 @@ class Master {
     }
 }
 
-function findSecretWord(words: string[], master: Master) {
-
+const findSecretWord = (words: string[], master: Master):any => {
+    
+    const getMostCommonWord = (words) => {
+        const count = new Array(6).fill(new Array(26).fill(0));
+        for (let word of words) {
+            for (let [idx, char] of Object.entries(word)) {
+                count[idx][char.toString().charCodeAt(0)-97]++;
+            }
+        }
+        
+        let mostCommonWord = "";
+        let maxScore = 0;
+        for (let word of words) {
+            let score = 0;
+            for (let [idx, char] of Object.entries(word)) {
+                score += count[idx][char.toString().charCodeAt(0)-97];
+            }
+            if (score > maxScore) {
+                maxScore = score;
+                mostCommonWord = word;
+            }
+        }
+        
+        return mostCommonWord;    
+    };
+    
+    const getNewWordlist = (wordlist, target, cnt) => {
+        return wordlist.filter(word => {
+            let c = 0;
+            for (let [idx, char] of Object.entries(word)) {
+                if (char === target[idx]) c++;
+            }
+            if (c === cnt) return true;
+            return false;
+        });
+    };
+    
+    while (words.length > 0) {
+        const mostCommonWord = getMostCommonWord(words);
+        const cnt = master.guess(mostCommonWord);
+        if (cnt === 6) return;
+        words = getNewWordlist(words, mostCommonWord, cnt);
+    }
 };
 
 
