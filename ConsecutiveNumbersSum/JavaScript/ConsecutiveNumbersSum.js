1058099aca6bc02d8f63fdd72a71310e95a87ef2
Jason Mauss <jason.mauss@gmail.com>
2022-11-30 18:47:00 -0700

    initial commit

---
+++ ConsecutiveNumbersSum/JavaScript/ConsecutiveNumbersSum.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/consecutive-numbers-sum/
+const consecutiveNumbersSum = (n) => {
+    return 0;
+};
+// some test cases
+console.log(consecutiveNumbersSum(5)); // 2
+console.log(consecutiveNumbersSum(9)); // 3
+console.log(consecutiveNumbersSum(15)); // 4
