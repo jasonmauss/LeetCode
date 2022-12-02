828cf5da7a56b1e90ce549d969419dd6f6d79473
Jason Mauss <jason.mauss@gmail.com>
2022-12-01 23:08:46 -0700

    Adding Solutions for TypeScript and JavaScript

--- ConsecutiveNumbersSum/JavaScript/ConsecutiveNumbersSum.js
+++ ConsecutiveNumbersSum/JavaScript/ConsecutiveNumbersSum.js
@@ -1,6 +1,12 @@
 // Solution for: https://leetcode.com/problems/consecutive-numbers-sum/
 const consecutiveNumbersSum = (n) => {
-    return 0;
+    let count = 1;
+    for (let item = 2; item < Math.sqrt(2 * n); item++) {
+        if ((n - (item + 1) * item / 2) % item === 0) {
+            count++;
+        }
+    }
+    return count;
 };
 // some test cases
 console.log(consecutiveNumbersSum(5)); // 2
