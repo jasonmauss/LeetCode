c0745161e3b9672c7b3b9a953e4dc753fa1f69c1
Jason Mauss <jason.mauss@gmail.com>
2022-12-02 22:48:57 -0700

    initial commit

---
+++ SlidingWindowMedian/TypeScript/SlidingWindowMedian.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/sliding-window-median/
+const medianSlidingWindow = (nums: number[], k: number): number[] => {
+
+    return [0];
+
+};
+
+
+// some test cases
+console.log(medianSlidingWindow([1,3,-1,-3,5,3,6,7], 3)); // [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
+console.log(medianSlidingWindow([1,2,3,4,2,3,1,4,2], 3)); // [2.00000,3.00000,3.00000,3.00000,2.00000,3.00000,2.00000]
