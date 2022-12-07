05ceb5bd10c83971aa13e8f8d19998bbac4f4878
Jason Mauss <jason.mauss@gmail.com>
2022-12-06 23:12:53 -0700

    initial commit

---
+++ candy/JavaScript/candy.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/candy/
+const candy = (ratings) => {
+    return 0;
+};
+// some test cases
+console.log(candy([1, 0, 2])); // 5
+console.log(candy([1, 2, 2])); // 4
