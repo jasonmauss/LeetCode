fcc3ba8ec46d6eaf77c407b9d9139a6bd0fbd172
Jason Mauss <jason.mauss@gmail.com>
2022-12-08 18:52:42 -0700

    initial commit

---
+++ RepeatedDigits/JavaScript/RepeatedDigits.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/numbers-with-repeated-digits/
+const numDupDigitsAtMostN = (n) => {
+    return 0;
+};
+// some test cases
+console.log(numDupDigitsAtMostN(20)); // 1
+console.log(numDupDigitsAtMostN(20)); // 10
+console.log(numDupDigitsAtMostN(20)); // 262
