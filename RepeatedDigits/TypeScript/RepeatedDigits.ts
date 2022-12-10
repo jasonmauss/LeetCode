d1917d0274279596bd72720cc343c5cd9a409882
Jason Mauss <jason.mauss@gmail.com>
2022-12-09 22:55:22 -0700

    Adding Solutions for TypeScript and JavaScript

--- RepeatedDigits/TypeScript/RepeatedDigits.ts
+++ RepeatedDigits/TypeScript/RepeatedDigits.ts
@@ -1,7 +1,47 @@
 // Solution for: https://leetcode.com/problems/numbers-with-repeated-digits/
+const permutation = (n, r) => {
+
+    let nonRepeatingNumbers = 1;
+
+    for(let i = 0; i < r; i++){
+        nonRepeatingNumbers *= n;
+        n--;
+    }
+
+    return nonRepeatingNumbers;
+}
+
 const numDupDigitsAtMostN = (n: number): number => {
 
-    return 0;
+    let count = 0;
+    const numDigits = [];
+    
+    let temp = n + 1;
+
+    while(temp !== 0){
+        const num = temp % 10;
+        numDigits.unshift(num);
+        temp = parseInt((temp / 10).toString());
+    }
+
+    for(let i = 0; i < numDigits.length - 1; i++){
+        count += 9 * permutation(9, i);
+    }
+
+    const mySet = new Set();
+
+    for(let i = 0; i < numDigits.length; i++){
+        for(let j = i === 0? 1: 0; j < numDigits[i]; j++){
+            if(mySet.has(j)) continue;
+            count += permutation(10 - (i + 1), numDigits.length - (i + 1)); 
+        }
+
+        if(mySet.has(numDigits[i])) break;
+        mySet.add(numDigits[i]);
+
+    }
+    
+    return n - count;
 
 };
 
@@ -9,5 +49,5 @@ const numDupDigitsAtMostN = (n: number): number => {
 
 // some test cases
 console.log(numDupDigitsAtMostN(20)); // 1
-console.log(numDupDigitsAtMostN(20)); // 10
-console.log(numDupDigitsAtMostN(20)); // 262
+console.log(numDupDigitsAtMostN(100)); // 10
+console.log(numDupDigitsAtMostN(1000)); // 262
