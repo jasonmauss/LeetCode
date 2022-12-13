16ade7136e5315379451217cee28d0f2ee24c7ca
Jason Mauss <jason.mauss@gmail.com>
2022-12-12 22:45:45 -0700

    initial commit

---
+++ ProfitableSchemes/JavaScript/ProfitableSchemes.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/profitable-schemes/
+const profitableSchemes = (n, minProfit, group, profit) => {
+    return 0;
+};
+// some test cases
+console.log(profitableSchemes(5, 3, [2, 2], [2, 3])); // 2
+console.log(profitableSchemes(10, 5, [2, 3, 5], [6, 7, 8])); // 7
---
+++ ProfitableSchemes/TypeScript/ProfitableSchemes.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/profitable-schemes/
+const profitableSchemes = (n: number, minProfit: number, group: number[], profit: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(profitableSchemes(5, 3, [2,2], [2,3])); // 2
+console.log(profitableSchemes(10, 5, [2,3,5], [6,7,8])); // 7
