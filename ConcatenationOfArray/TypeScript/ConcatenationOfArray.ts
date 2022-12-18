ba0702ac74edcbba9b34fb0ec48411aa840aeaf9
Jason Mauss <jason.mauss@gmail.com>
2022-12-17 23:59:16 -0700

    initial commit

---
+++ ConcatenationOfArray/TypeScript/ConcatenationOfArray.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/concatenation-of-array/
+const getConcatenation = (nums: number[]): number[] => {
+
+    return [0];
+};
+
+
+
+// some test cases
+console.log(getConcatenation([1,2,1])); // [1,2,1,1,2,1]
+console.log(getConcatenation([1,3,2,1])); // [1,3,2,1,1,3,2,1]
