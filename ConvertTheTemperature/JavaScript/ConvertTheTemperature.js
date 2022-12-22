e80cc9d32e1cfcc2700e1c029bed04cdee5f5439
Jason Mauss <jason.mauss@gmail.com>
2022-12-21 22:45:58 -0700

    initial commit

---
+++ ConvertTheTemperature/JavaScript/ConvertTheTemperature.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/convert-the-temperature/
+const convertTemperature = (celsius) => {
+    return [0];
+};
+// some test cases
+console.log(convertTemperature(36.50)); // [309.65000,97.70000]
+console.log(convertTemperature(122.11)); // [395.26000,251.79800]
