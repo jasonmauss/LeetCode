617592e8e677c1eb9e89a7620e3c854acc652506
Jason Mauss <jason.mauss@gmail.com>
2022-12-29 23:35:36 -0800

    initial commit

---
+++ AddTwoIntegers/JavaScript/AddTwoIntegers.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/add-two-integers/
+const sum = (num1, num2) => {
+    return 0;
+};
+// some test cases
+console.log(sum(12, 5)); // 17
+console.log(sum(-10, 4)); // -6
