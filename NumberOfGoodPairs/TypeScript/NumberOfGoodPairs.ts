610b6d541cf915285045e0fd07236a7fa558c16e
Jason Mauss <jason.mauss@gmail.com>
2023-01-04 23:33:02 -0700

    initial commit

---
+++ NumberOfGoodPairs/TypeScript/NumberOfGoodPairs.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/number-of-good-pairs/
+const numIdenticalPairs = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(numIdenticalPairs([1,2,3,1,1,3])); // 4
+console.log(numIdenticalPairs([1,1,1,1])); // 6
+console.log(numIdenticalPairs([1,2,3])); // 0
