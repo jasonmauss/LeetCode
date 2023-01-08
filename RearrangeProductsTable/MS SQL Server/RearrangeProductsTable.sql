8e6cbd39706fd3c2236c79c37c376aefd191fb50
Jason Mauss <jason.mauss@gmail.com>
2023-01-07 19:41:37 -0700

    Adding Solutions for MySQL and MS SQL Server

--- RearrangeProductsTable/MS SQL Server/RearrangeProductsTable.sql
+++ RearrangeProductsTable/MS SQL Server/RearrangeProductsTable.sql
@@ -12,6 +12,30 @@ GO
 TRUNCATE TABLE Products;
 
 INSERT INTO Products (product_id, store1, store2, store3) VALUES (0, 95, 100, 105);
-INSERT INTO Products (product_id, store1, store2, store3) VALUES (1, 70, 0, 80);
+INSERT INTO Products (product_id, store1, store2, store3) VALUES (1, 70, NULL, 80);
 
+-- This could be done with a CTE and also use an UNPIVOT
+-- but a simple UNION works just as well for a limited number
+-- of columns that we're dealing with.
 
+SELECT 	product_id,
+		'store1' AS store,
+		store1 AS price
+  FROM	Products
+ WHERE	(store1 IS NOT NULL)
+ 
+ UNION 	ALL
+ 
+SELECT 	product_id,
+		'store2' AS store,
+		store2 AS price
+  FROM	Products
+ WHERE	(store2 IS NOT NULL)
+ 
+ UNION	ALL
+ 
+ SELECT  product_id,
+		'store3' AS store,
+		store3 AS price
+  FROM	Products
+ WHERE	(store3 IS NOT NULL)
--- RearrangeProductsTable/MySQL/RearrangeProductsTable.sql
+++ RearrangeProductsTable/MySQL/RearrangeProductsTable.sql
@@ -6,6 +6,29 @@ CREATE TABLE IF NOT EXISTS Products (product_id INT, store1 INT, store2 INT, sto
 TRUNCATE TABLE Products;
 
 INSERT INTO Products (product_id, store1, store2, store3) VALUES ('0', '95', '100', '105');
-INSERT INTO Products (product_id, store1, store2, store3) VALUES ('1', '70', 'None', '80');
+INSERT INTO Products (product_id, store1, store2, store3) VALUES ('1', '70', NULL, '80');
 
-
+-- This could be done with a CTE and also use an UNPIVOT
+-- but a simple UNION works just as well for a limited number
+-- of columns that we're dealing with.
+SELECT 	product_id,
+		'store1' AS store,
+		store1 AS price
+  FROM	Products
+ WHERE	(store1 IS NOT NULL)
+ 
+ UNION 	ALL
+ 
+SELECT 	product_id,
+		'store2' AS store,
+		store2 AS price
+  FROM	Products
+ WHERE	(store2 IS NOT NULL)
+ 
+ UNION	ALL
+ 
+ SELECT  product_id,
+		'store3' AS store,
+		store3 AS price
+  FROM	Products
+ WHERE	(store3 IS NOT NULL)
