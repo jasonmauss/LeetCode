96a8ee0ad179a97c8f840118737b7a19922cafd9
Jason Mauss <jason.mauss@gmail.com>
2023-01-08 22:33:26 -0700

    initial commit

---
+++ JewelsAndStones/JavaScript/JewelsAndStones.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/jewels-and-stones/
+const numJewelsInStones = (jewels, stones) => {
+    return 0;
+};
+// some test cases
+console.log(numJewelsInStones('aA', 'aAAbbbb')); // 3
+console.log(numJewelsInStones('z', 'ZZ')); // 0
---
+++ JewelsAndStones/TypeScript/JewelsAndStones.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/jewels-and-stones/
+const numJewelsInStones = (jewels: string, stones: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(numJewelsInStones('aA', 'aAAbbbb')); // 3
+console.log(numJewelsInStones('z', 'ZZ')); // 0
