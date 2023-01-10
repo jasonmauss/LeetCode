a740105a6c07a07ff0896109b8d8c39edf7e1fb7
Jason Mauss <jason.mauss@gmail.com>
2023-01-09 22:22:17 -0700

    Adding Solutions for TypeScript and JavaScript

--- JewelsAndStones/JavaScript/JewelsAndStones.js
+++ JewelsAndStones/JavaScript/JewelsAndStones.js
@@ -1,7 +1,18 @@
 // Solution for: https://leetcode.com/problems/jewels-and-stones/
 const numJewelsInStones = (jewels, stones) => {
-    return 0;
+    let numberOfJewels = 0;
+    let uniqueJewels = new Set();
+    for (let jewel of jewels) {
+        if (!uniqueJewels.has(jewel.charCodeAt(0)))
+            uniqueJewels.add(jewel.charCodeAt(0));
+    }
+    for (let stone of stones) {
+        if (uniqueJewels.has(stone.charCodeAt(0)))
+            numberOfJewels++;
+    }
+    return numberOfJewels;
 };
 // some test cases
 console.log(numJewelsInStones('aA', 'aAAbbbb')); // 3
 console.log(numJewelsInStones('z', 'ZZ')); // 0
+console.log(numJewelsInStones('aAbBcCdD', 'aAAbbbbCCCCDDDeeE')); // 14
--- JewelsAndStones/TypeScript/JewelsAndStones.ts
+++ JewelsAndStones/TypeScript/JewelsAndStones.ts
@@ -1,8 +1,21 @@
 // Solution for: https://leetcode.com/problems/jewels-and-stones/
 const numJewelsInStones = (jewels: string, stones: string): number => {
 
-    return 0;
+    let numberOfJewels = 0;
 
+    let uniqueJewels:Set<number> = new Set<number>();
+
+    for(let jewel of jewels) {
+        if(!uniqueJewels.has(jewel.charCodeAt(0)))
+            uniqueJewels.add(jewel.charCodeAt(0));
+    }
+
+    for(let stone of stones) {
+        if(uniqueJewels.has(stone.charCodeAt(0)))
+            numberOfJewels++
+    }
+
+    return numberOfJewels;
 };
 
 
@@ -11,3 +24,4 @@ const numJewelsInStones = (jewels: string, stones: string): number => {
 // some test cases
 console.log(numJewelsInStones('aA', 'aAAbbbb')); // 3
 console.log(numJewelsInStones('z', 'ZZ')); // 0
+console.log(numJewelsInStones('aAbBcCdD', 'aAAbbbbCCCCDDDeeE')); // 14
