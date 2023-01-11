ac377cacc5ca31fe75b391056a7f478462223c0d
Jason Mauss <jason.mauss@gmail.com>
2023-01-10 23:28:57 -0700

    initial commit

---
+++ RichestCustomerWealth/JavaScript/RichestCustomerWealth.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/richest-customer-wealth/
+const maximumWealth = (accounts) => {
+    return 0;
+};
+// some test cases
+console.log(maximumWealth([[1, 2, 3], [3, 2, 1]])); // 6
+console.log(maximumWealth([[1, 5], [7, 3], [3, 5]])); // 10
+console.log(maximumWealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]])); // 17
