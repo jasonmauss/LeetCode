717a4de8c343ac958ceb17b0b32c3d449fd8cb48
Jason Mauss <jason.mauss@gmail.com>
2023-01-11 23:47:09 -0700

    initial commit

---
+++ MaxWordsInSentences/TypeScript/MaxWordsInSentences.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+const mostWordsFound = (sentences: string[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"])); // 6
+console.log(mostWordsFound(["please wait", "continue to fight", "continue to win"])); // 3
