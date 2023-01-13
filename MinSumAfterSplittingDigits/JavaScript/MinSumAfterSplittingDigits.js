f824417302bd3856e02b60c7f54870c395108c07
Jason Mauss <jason.mauss@gmail.com>
2023-01-12 20:51:37 -0700

    initial commit

---
+++ MinSumAfterSplittingDigits/JavaScript/MinSumAfterSplittingDigits.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+const minimumSum = (num) => {
+    return 0;
+};
+// some test cases
+console.log(minimumSum(2932)); // 52
+console.log(minimumSum(4009)); // 13
