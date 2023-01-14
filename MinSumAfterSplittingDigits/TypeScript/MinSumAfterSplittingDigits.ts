4166dac30b51761141de3f51caa64a10d216821b
Jason Mauss <jason.mauss@gmail.com>
2023-01-13 23:15:50 -0800

    Adding Solutions for TypeScript and JavaScript

--- MinSumAfterSplittingDigits/TypeScript/MinSumAfterSplittingDigits.ts
+++ MinSumAfterSplittingDigits/TypeScript/MinSumAfterSplittingDigits.ts
@@ -1,7 +1,17 @@
 // Solution for: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
 const minimumSum = (num: number): number => {
 
-    return 0;
+    const digits = num.toString().split('');
+    const n = digits.length;
+    let minSum = 0;
+    digits.sort();
+    
+    for (let i = 0; i < n / 2; i++) {
+        const pair = digits[i] + digits[n - 1 - i];
+        minSum += +pair;
+    }
+  
+    return minSum;
 
 };
 
