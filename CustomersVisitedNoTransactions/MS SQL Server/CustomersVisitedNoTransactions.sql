211e36244f050cfd60b7c7f7e4f3158121ddfe3f
Jason Mauss <jason.mauss@gmail.com>
2023-01-17 00:16:15 -0700

    initial commit

---
+++ CustomersVisitedNoTransactions/MS SQL Server/CustomersVisitedNoTransactions.sql
@@ -0,0 +1,36 @@
+-- Solution for: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+USE leetcodedb;
+GO
+
+IF OBJECT_ID(N'dbo.Visits', N'U') IS NULL BEGIN
+	CREATE TABLE Visits (visit_id INT, customer_id INT);
+END
+GO
+
+IF OBJECT_ID(N'dbo.Transactions', N'U') IS NULL BEGIN
+	CREATE TABLE Transactions (transaction_id INT, visit_id INT, amount INT);
+END
+GO
+
+
+TRUNCATE TABLE Visits;
+
+INSERT INTO Visits (visit_id, customer_id) values (1, 23)
+INSERT INTO Visits (visit_id, customer_id) values (2, 9)
+INSERT INTO Visits (visit_id, customer_id) values (4, 30)
+INSERT INTO Visits (visit_id, customer_id) values (5, 54)
+INSERT INTO Visits (visit_id, customer_id) values (6, 96)
+INSERT INTO Visits (visit_id, customer_id) values (7, 54)
+INSERT INTO Visits (visit_id, customer_id) values (8, 54)
+
+TRUNCATE TABLE Transactions;
+
+INSERT INTO Transactions (transaction_id, visit_id, amount) values (2, 5, 310)
+INSERT INTO Transactions (transaction_id, visit_id, amount) values (3, 5, 300)
+INSERT INTO Transactions (transaction_id, visit_id, amount) values (9, 5, 200)
+INSERT INTO Transactions (transaction_id, visit_id, amount) values (12, 1, 910)
+INSERT INTO Transactions (transaction_id, visit_id, amount) values (13, 2, 970)
+
+-- SQL Solution:
+
---
+++ CustomersVisitedNoTransactions/MySQL/CustomersVisitedNoTransactions.sql
@@ -0,0 +1,26 @@
+-- Solution for: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS Visits(visit_id INT, customer_id INT);
+CREATE TABLE IF NOT EXISTS Transactions(transaction_id INT, visit_id INT, amount INT);
+
+TRUNCATE TABLE Visits;
+
+INSERT INTO Visits (visit_id, customer_id) VALUES ('1', '23');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('2', '9');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('4', '30');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('5', '54');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('6', '96');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('7', '54');
+INSERT INTO Visits (visit_id, customer_id) VALUES ('8', '54');
+
+TRUNCATE TABLE Transactions;
+
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES ('2', '5', '310');
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES ('3', '5', '300');
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES ('9', '5', '200');
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES ('12', '1', '910');
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES ('13', '2', '970');
+
+-- SQL Solution:
+
