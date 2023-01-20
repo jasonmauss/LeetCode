a5b8e97097ba0afd734c2641411e3e6c42bbf347
Jason Mauss <jason.mauss@gmail.com>
2023-01-19 21:31:06 -0700

    initial commit

---
+++ SubtractProductAndSum/JavaScript/SubtractProductAndSum.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+const subtractProductAndSum = (n) => {
+    return 0;
+};
+// some test cases
+console.log(subtractProductAndSum(234)); // 15
+console.log(subtractProductAndSum(4421)); // 21
+console.log(subtractProductAndSum(1492)); // 56
