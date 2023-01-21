478e7fe13474c17be25be9d8dca8416b8954ed01
Jason Mauss <jason.mauss@gmail.com>
2023-01-21 15:58:35 -0700

    initial commit

---
+++ SmallerNumbersThanCurrent/JavaScript/SmallerNumbersThanCurrent.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+const smallerNumbersThanCurrent = (nums) => {
+    return [0];
+};
+// some test cases
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3])); // [4,0,1,1,3]
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8])); // [2,1,0,3]
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7])); // [0,0,0,0]
