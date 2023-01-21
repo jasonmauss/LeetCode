630edcfe1b5a288ec2162b91407d1fbb0e86501e
Jason Mauss <jason.mauss@gmail.com>
2023-01-20 23:40:40 -0700

    Adding Solutions for TypeScript and JavaScript

--- SubtractProductAndSum/JavaScript/SubtractProductAndSum.js
+++ SubtractProductAndSum/JavaScript/SubtractProductAndSum.js
@@ -1,6 +1,13 @@
 // Solution for: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 const subtractProductAndSum = (n) => {
-    return 0;
+    const digitValues = n.toString().split('');
+    let sumOfDigits = 0;
+    let productOfDigits = 1; // initialize to 1 so first multiplication results in that number being the product.
+    for (let digit of digitValues) {
+        productOfDigits *= Number(digit);
+        sumOfDigits += Number(digit);
+    }
+    return productOfDigits - sumOfDigits;
 };
 // some test cases
 console.log(subtractProductAndSum(234)); // 15
--- SubtractProductAndSum/TypeScript/SubtractProductAndSum.ts
+++ SubtractProductAndSum/TypeScript/SubtractProductAndSum.ts
@@ -1,8 +1,16 @@
 // Solution for: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 const subtractProductAndSum = (n: number): number => {
 
-    return 0;
+    const digitValues = n.toString().split('');
+    let sumOfDigits = 0;
+    let productOfDigits = 1; // initialize to 1 so first multiplication results in that number being the product.
 
+    for(let digit of digitValues) {
+        productOfDigits *= Number(digit);
+        sumOfDigits += Number(digit);
+    }
+
+    return productOfDigits - sumOfDigits;
 };
 
 
