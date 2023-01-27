eecb59c2e00afb5d960fea91ad6d7c90c24e643a
Jason Mauss <jason.mauss@gmail.com>
2023-01-26 22:16:49 -0700

    Adding Solutions for TypeScript and JavaScript

--- GoalParserInterpretation/JavaScript/GoalParserInterpretation.js
+++ GoalParserInterpretation/JavaScript/GoalParserInterpretation.js
@@ -1,6 +1,12 @@
 // Solution for: https://leetcode.com/problems/goal-parser-interpretation/
 const interpret = (command) => {
-    return '';
+    while (command.indexOf('(al)') > -1) {
+        command = command.replace('(al)', 'al');
+    }
+    while (command.indexOf('()') > -1) {
+        command = command.replace('()', 'o');
+    }
+    return command;
 };
 // some test cases
 console.log(interpret("G()(al)")); // "Goal"
