6d87a5c01e0efb55f1655faff72cba6f6a7e8419
Jason Mauss <jason.mauss@gmail.com>
2023-01-28 17:14:29 -0700

    initial commit

---
+++ DecodeXORedArray/TypeScript/DecodeXORedArray.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/decode-xored-array/
+const decode = (encoded: number[], first: number): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+// some test cases
+console.log(decode([1,2,3], 1)); // [1,0,2,1]
+console.log(decode([6,2,7,3], 4)); // [4,2,0,7,4]
