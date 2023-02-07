8ea312db39397e58f4d127d9797f05df0264b1b2
Jason Mauss <jason.mauss@gmail.com>
2023-02-06 22:41:05 -0700

    Adding Solutions for TypeScript and JavaScript

--- CellsInRange/JavaScript/CellsInRange.js
+++ CellsInRange/JavaScript/CellsInRange.js
@@ -1,6 +1,17 @@
 // Solution for: https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/
 const cellsInRange = (s) => {
-    return [''];
+    const [startCell, endCell] = s.split(':');
+    let startCellRowNum = startCell.charCodeAt(0);
+    let startCellColumnNum = Number(startCell.substring(1));
+    let endCellRowNum = endCell.charCodeAt(0);
+    let endCellColumnNum = Number(endCell.substring(1));
+    let CellsInRangeArray = [];
+    for (let rowPosition = startCellRowNum; rowPosition <= endCellRowNum; rowPosition++) {
+        for (let cellPosition = startCellColumnNum; cellPosition <= endCellColumnNum; cellPosition++) {
+            CellsInRangeArray.push(String.fromCharCode(rowPosition) + cellPosition);
+        }
+    }
+    return CellsInRangeArray;
 };
 // some test cases  
 console.log(cellsInRange("K1:L2")); // ["K1","K2","L1","L2"]
