78277205f08d8d4d05e8904fb5b881a2c4e4ca42
Jason Mauss <jason.mauss@gmail.com>
2023-02-13 22:21:27 -0700

    Adding Solutions for TypeScript and JavaScript

--- NumberOfSteps/JavaScript/NumberOfSteps.js
+++ NumberOfSteps/JavaScript/NumberOfSteps.js
@@ -1,6 +1,11 @@
 // Solution for: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 const numberOfSteps = (num) => {
-    return 0;
+    let steps = 0;
+    while (num > 0) {
+        num = num % 2 == 0 ? num / 2 : num - 1;
+        steps++;
+    }
+    return steps;
 };
 // some test cases
 console.log(numberOfSteps(14)); // 6
