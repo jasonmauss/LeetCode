740d62d4eaf23f5862ad8260aabe06d503b7619e
Jason Mauss <jason.mauss@gmail.com>
2023-02-14 21:13:44 -0700

    initial commit

---
+++ BalancedStringSplit/JavaScript/BalancedStringSplit.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/split-a-string-in-balanced-strings/
+const balancedStringSplit = (s) => {
+    return 0;
+};
+// some test cases
+console.log(balancedStringSplit('RLRRLLRLRL')); // 4
+console.log(balancedStringSplit('RLRRRLLRLL')); // 2
+console.log(balancedStringSplit('LLLLRRRR')); // 1
---
+++ BalancedStringSplit/TypeScript/BalancedStringSplit.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/split-a-string-in-balanced-strings/
+const balancedStringSplit = (s: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(balancedStringSplit('RLRRLLRLRL')); // 4
+console.log(balancedStringSplit('RLRRRLLRLL')); // 2
+console.log(balancedStringSplit('LLLLRRRR')); // 1
