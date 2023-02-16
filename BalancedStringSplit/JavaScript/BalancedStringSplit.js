396d860d315cffb9a9165e6cb0241a7f8d4ffdea
Jason Mauss <jason.mauss@gmail.com>
2023-02-15 20:03:38 -0700

    Adding Solutions for TypeScript and JavaScript

--- BalancedStringSplit/JavaScript/BalancedStringSplit.js
+++ BalancedStringSplit/JavaScript/BalancedStringSplit.js
@@ -1,8 +1,23 @@
 // Solution for: https://leetcode.com/problems/split-a-string-in-balanced-strings/
 const balancedStringSplit = (s) => {
-    return 0;
+    let balancedStringPairs = 0;
+    let letterLCount = 0;
+    let letterRCount = 0;
+    for (let char of s) {
+        if (char === 'L')
+            letterLCount++;
+        else
+            letterRCount++;
+        if (letterLCount === letterRCount) {
+            balancedStringPairs++;
+            letterLCount = 0;
+            letterRCount = 0;
+        }
+    }
+    return balancedStringPairs;
 };
 // some test cases
 console.log(balancedStringSplit('RLRRLLRLRL')); // 4
 console.log(balancedStringSplit('RLRRRLLRLL')); // 2
 console.log(balancedStringSplit('LLLLRRRR')); // 1
+console.log(balancedStringSplit('LLRLRRLRLRLLLLRRRRLLLRLRLRLRRR')); // 
--- BalancedStringSplit/TypeScript/BalancedStringSplit.ts
+++ BalancedStringSplit/TypeScript/BalancedStringSplit.ts
@@ -1,7 +1,27 @@
 // Solution for: https://leetcode.com/problems/split-a-string-in-balanced-strings/
 const balancedStringSplit = (s: string): number => {
 
-    return 0;
+    let balancedStringPairs = 0;
+
+    let letterLCount = 0;
+    let letterRCount = 0;
+
+    for(let char of s) {
+        if(char === 'L')
+            letterLCount++;
+        else
+            letterRCount++;
+
+        if(letterLCount ===  letterRCount) {
+            balancedStringPairs++;
+            letterLCount = 0;
+            letterRCount = 0;
+        }
+
+        
+    }
+
+    return balancedStringPairs;
 
 };
 
@@ -12,3 +32,4 @@ const balancedStringSplit = (s: string): number => {
 console.log(balancedStringSplit('RLRRLLRLRL')); // 4
 console.log(balancedStringSplit('RLRRRLLRLL')); // 2
 console.log(balancedStringSplit('LLLLRRRR')); // 1
+console.log(balancedStringSplit('LLRLRRLRLRLLLLRRRRLLLRLRLRLRRR')); // 5
