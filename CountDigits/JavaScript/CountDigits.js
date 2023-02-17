3eeefbf03b9f15789c11c3be2e3f4c4b614c5cd5
Jason Mauss <jason.mauss@gmail.com>
2023-02-16 23:51:57 -0700

    initial commit

---
+++ CountDigits/JavaScript/CountDigits.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+const countDigits = (num) => {
+    return 0;
+};
+// some test cases
+console.log(countDigits(7)); // 1
+console.log(countDigits(121)); // 2
+console.log(countDigits(1248)); // 4
---
+++ CountDigits/TypeScript/CountDigits.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+const countDigits = (num: number): number => {
+
+    return 0 ;
+
+};
+
+
+
+
+// some test cases
+console.log(countDigits(7)); // 1
+console.log(countDigits(121)); // 2
+console.log(countDigits(1248)); // 4
