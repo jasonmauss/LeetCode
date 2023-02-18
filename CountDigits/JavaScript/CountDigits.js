f7b1d29d984bbab8f6ddd3d85d24e8049005b83a
Jason Mauss <jason.mauss@gmail.com>
2023-02-17 21:32:36 -0700

    Adding Solutions for TypeScript and JavaScript

--- CountDigits/JavaScript/CountDigits.js
+++ CountDigits/JavaScript/CountDigits.js
@@ -1,6 +1,12 @@
 // Solution for: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
 const countDigits = (num) => {
-    return 0;
+    let digits = num.toString().split('');
+    let divisibleDigitCount = 0;
+    for (let digit of digits) {
+        if (num % Number(digit) === 0)
+            divisibleDigitCount++;
+    }
+    return divisibleDigitCount;
 };
 // some test cases
 console.log(countDigits(7)); // 1
