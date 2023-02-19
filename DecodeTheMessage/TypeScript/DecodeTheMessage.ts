b3c0f386db4e8f5db31bf68dc55e80cbb520cb99
Jason Mauss <jason.mauss@gmail.com>
2023-02-18 23:00:22 -0700

    initial commit

---
+++ DecodeTheMessage/TypeScript/DecodeTheMessage.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/decode-the-message/
+const decodeMessage = (key: string, message: string): string => {
+
+    return '';
+    
+};
+
+
+
+
+
+// some test cases
+console.log(decodeMessage('the quick brown fox jumps over the lazy dog', 'vkbs bs t suepuv')); // this is a secret
+console.log(decodeMessage('eljuxhpwnyrdgtqkviszcfmabo', 'zwx hnfx lqantp mnoeius ycgk vcnjrdb')); // the five boxing wizards jump quickly
