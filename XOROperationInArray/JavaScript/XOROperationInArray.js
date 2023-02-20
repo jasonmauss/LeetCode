b6241bce44e23d5770f0ecf6e03a631192fd9cb9
Jason Mauss <jason.mauss@gmail.com>
2023-02-19 22:52:22 -0700

    initial commit

---
+++ XOROperationInArray/JavaScript/XOROperationInArray.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/xor-operation-in-an-array/
+const xorOperation = (n, start) => {
+    return 0;
+};
+// some test cases
+console.log(xorOperation(5, 0)); // 8
+console.log(xorOperation(4, 3)); // 8
