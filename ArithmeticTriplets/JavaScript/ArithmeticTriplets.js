18065bada84b1b37f6131d5a5bb4ce0600f24176
Jason Mauss <jason.mauss@gmail.com>
2023-02-22 22:28:31 -0700

    Adding Solutions for TypeScript and JavaScript

--- ArithmeticTriplets/JavaScript/ArithmeticTriplets.js
+++ ArithmeticTriplets/JavaScript/ArithmeticTriplets.js
@@ -1,6 +1,14 @@
 // Solution for: https://leetcode.com/problems/number-of-arithmetic-triplets/
 const arithmeticTriplets = (nums, diff) => {
-    return 0;
+    const numberSet = new Set();
+    let tripletCount = 0;
+    for (let num of nums) {
+        if (numberSet.has(num - 2 * diff) && numberSet.has(num - diff)) {
+            tripletCount++;
+        }
+        numberSet.add(num);
+    }
+    return tripletCount;
 };
 // some test cases
 console.log(arithmeticTriplets([0, 1, 4, 6, 7, 10], 3)); // 2
