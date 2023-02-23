1efb9e769ee64b344f05a7dbdef8544af52d61f0
Jason Mauss <jason.mauss@gmail.com>
2023-02-22 22:34:36 -0700

    initial commit

---
+++ CheckIfPanagram/JavaScript/CheckIfPanagram.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+const checkIfPangram = (sentence) => {
+    return false;
+};
+// some test cases
+console.log(checkIfPangram('thequickbrownfoxjumpsoverthelazydog')); // true
+console.log(checkIfPangram('leetcode')); // false
