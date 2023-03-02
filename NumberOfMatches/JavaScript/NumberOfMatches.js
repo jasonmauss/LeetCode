b01319018491f9e62606dc1490d281ee75b5c7dc
Jason Mauss <jason.mauss@gmail.com>
2023-03-01 20:43:19 -0700

    initial commit

---
+++ NumberOfMatches/JavaScript/NumberOfMatches.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/count-of-matches-in-tournament/
+const numberOfMatches = (n) => {
+    return 0;
+};
+// some test cases
+console.log(numberOfMatches(7)); // 6
+console.log(numberOfMatches(14)); // 13
---
+++ NumberOfMatches/TypeScript/NumberOfMatches.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/count-of-matches-in-tournament/
+const numberOfMatches = (n: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(numberOfMatches(7)); // 6
+console.log(numberOfMatches(14)); // 13
