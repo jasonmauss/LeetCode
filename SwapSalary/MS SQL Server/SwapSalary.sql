e34e2b609150aa604737aba48bda91ef41197408
Jason Mauss <jason.mauss@gmail.com>
2023-03-04 12:47:12 -0700

    Update SwapSalary.sql

--- SwapSalary/MS SQL Server/SwapSalary.sql
+++ SwapSalary/MS SQL Server/SwapSalary.sql
@@ -18,6 +18,6 @@ INSERT INTO Salary (id, name, sex, salary) VALUES (4, 'D', 'f', 500);
 
 -- SQL Solution:
 
-UPDATE Salary
-SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END
+ UPDATE	Salary 
+	SET sex = CASE WHEN sex = 'm' THEN 'f' ELSE 'm' END
 
