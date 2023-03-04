0773f459e477ae0c6906306a5a2557705952c11d
Jason Mauss <jason.mauss@gmail.com>
2023-03-04 12:55:08 -0700

    initial commit

---
+++ MaxParenthesesDepth/JavaScript/MaxParenthesesDepth.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+const maxDepth = (s) => {
+    return 0;
+};
+// some test cases
+console.log(maxDepth('(1+(2*3)+((8)/4))+1')); // 3
+console.log(maxDepth('(1)+((2))+(((3)))')); // 3
