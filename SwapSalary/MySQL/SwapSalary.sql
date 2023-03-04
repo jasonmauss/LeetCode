1699a6ec8c84fa6398632ee432d037483d36140d
Jason Mauss <jason.mauss@gmail.com>
2023-03-03 22:52:18 -0700

    initial commit

---
+++ SwapSalary/MySQL/SwapSalary.sql
@@ -0,0 +1,14 @@
+-- Solution for: https://leetcode.com/problems/swap-salary/
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS Salary (id INT, name VARCHAR(100), sex CHAR(1), salary INT);
+
+TRUNCATE TABLE Salary;
+
+INSERT INTO Salary (id, name, sex, salary) VALUES ('1', 'A', 'm', '2500');
+INSERT INTO Salary (id, name, sex, salary) VALUES ('2', 'B', 'f', '1500');
+INSERT INTO Salary (id, name, sex, salary) VALUES ('3', 'C', 'm', '5500');
+INSERT INTO Salary (id, name, sex, salary) VALUES ('4', 'D', 'f', '500');
+
+-- SQL Solution:
+	
