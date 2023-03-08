092aca0ed8033c725064e29f9fc044c7863c07dc
Jason Mauss <jason.mauss@gmail.com>
2023-03-07 22:49:16 -0700

    Adding Solutions for TypeScript and JavaScript

--- MaxParenthesesDepth/JavaScript/MaxParenthesesDepth.js
+++ MaxParenthesesDepth/JavaScript/MaxParenthesesDepth.js
@@ -1,6 +1,17 @@
 // Solution for: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 const maxDepth = (s) => {
-    return 0;
+    let depth = 0;
+    let maxDepth = 0;
+    for (let char of s) {
+        if (char === '(') {
+            depth++;
+            maxDepth = Math.max(depth, maxDepth);
+        }
+        if (char === ')') {
+            depth--;
+        }
+    }
+    return maxDepth;
 };
 // some test cases
 console.log(maxDepth('(1+(2*3)+((8)/4))+1')); // 3
--- MaxParenthesesDepth/TypeScript/MaxParenthesesDepth.ts
+++ MaxParenthesesDepth/TypeScript/MaxParenthesesDepth.ts
@@ -1,7 +1,21 @@
 // Solution for: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 const maxDepth = (s: string): number => {
 
-    return 0;
+    let depth = 0;
+    let maxDepth = 0;
+
+    for(let char of s) {
+        if(char === '(') {
+            depth++;
+            maxDepth = Math.max(depth, maxDepth);
+        }
+
+        if(char === ')') {
+            depth--;
+        }
+    }
+
+    return maxDepth;
 
 };
 
