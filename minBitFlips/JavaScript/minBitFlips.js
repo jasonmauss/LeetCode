0f5f391d094ea252a4c9cb0647a07ca844472f3c
Jason Mauss <jason.mauss@gmail.com>
2023-03-09 21:05:59 -0700

    initial commit

---
+++ minBitFlips/JavaScript/minBitFlips.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+const minBitFlips = (start, goal) => {
+    return 0;
+};
+// some test cases
+console.log(minBitFlips(10, 7)); // 3
+console.log(minBitFlips(3, 4)); // 3
---
+++ minBitFlips/TypeScript/minBitFlips.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+const minBitFlips = (start: number, goal: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(minBitFlips(10, 7)); // 3
+console.log(minBitFlips(3, 4)); // 3
