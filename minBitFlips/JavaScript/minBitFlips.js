f26a9cb3b09861993322e593eee72b1bbd28ecad
Jason Mauss <jason.mauss@gmail.com>
2023-03-10 23:18:34 -0700

    Adding Solutions for TypeScript and JavaScript

--- minBitFlips/JavaScript/minBitFlips.js
+++ minBitFlips/JavaScript/minBitFlips.js
@@ -1,6 +1,23 @@
 // Solution for: https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
 const minBitFlips = (start, goal) => {
-    return 0;
+    // track how many flips - this is what will be returned
+    let minFlips = 0;
+    // convert both numbers into a binary string
+    let binaryStart = start.toString(2);
+    let binaryGoal = goal.toString(2);
+    // find the length of the longest of both binary strings
+    let maxLength = Math.max(binaryGoal.length, binaryStart.length);
+    // use that longest length to pad the strings if necessary
+    binaryStart = binaryStart.padStart(maxLength, '0');
+    binaryGoal = binaryGoal.padStart(maxLength, '0');
+    // loop through both strings and add 1 to the flip count
+    // if the characters at the same position do not match
+    for (let i = 0; i < maxLength; i++) {
+        if (binaryGoal.charAt(i) !== binaryStart.charAt(i)) {
+            minFlips++;
+        }
+    }
+    return minFlips;
 };
 // some test cases
 console.log(minBitFlips(10, 7)); // 3
