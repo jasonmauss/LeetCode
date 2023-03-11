d2c41129da908a971dfef630ac2388f31e908397
Jason Mauss <jason.mauss@gmail.com>
2023-03-10 23:24:17 -0700

    initial commit

---
+++ CountConsistentStrings/JavaScript/CountConsistentStrings.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+const countConsistentStrings = (allowed, words) => {
+    return 0;
+};
+// some test cases
+console.log(countConsistentStrings('ab', ["ad", "bd", "aaab", "baa", "badab"])); // 2
+console.log(countConsistentStrings('abc', ["a", "b", "c", "ab", "ac", "bc", "abc"])); // 7
+console.log(countConsistentStrings('cad', ["cc", "acd", "b", "ba", "bac", "bad", "ac", "d"])); // 4
---
+++ CountConsistentStrings/TypeScript/CountConsistentStrings.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+const countConsistentStrings = (allowed: string, words: string[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(countConsistentStrings('ab', ["ad","bd","aaab","baa","badab"])); // 2
+console.log(countConsistentStrings('abc', ["a","b","c","ab","ac","bc","abc"])); // 7
+console.log(countConsistentStrings('cad', ["cc","acd","b","ba","bac","bad","ac","d"])); // 4
