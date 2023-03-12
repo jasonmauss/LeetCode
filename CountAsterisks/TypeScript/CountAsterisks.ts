ac174f3ae7ad1be7aad313606b9a0c383b3a0b1f
Jason Mauss <jason.mauss@gmail.com>
2023-03-11 23:33:59 -0700

    initial commit

---
+++ CountAsterisks/TypeScript/CountAsterisks.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/count-asterisks/
+const countAsterisks = (s: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(countAsterisks("l|*e*et|c**o|*de|")); // 2
+console.log(countAsterisks("iamprogrammer")); // 0
+console.log(countAsterisks("yo|uar|e**|b|e***au|tifu|l")); // 5
