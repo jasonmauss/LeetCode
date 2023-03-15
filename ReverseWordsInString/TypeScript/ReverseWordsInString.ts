ec743f8c6bdb2aa6f220233232c11c404d3031f7
Jason Mauss <jason.mauss@gmail.com>
2023-03-14 23:13:00 -0700

    initial commit

---
+++ ReverseWordsInString/TypeScript/ReverseWordsInString.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+const reverseWords = (s: string): string => {
+
+    return '';
+
+};
+
+
+
+
+
+// some test cases
+console.log(reverseWords("Let's take LeetCode contest")); // "s'teL ekat edoCteeL tsetnoc"
+console.log(reverseWords("God Ding")); // "doG gniD"
