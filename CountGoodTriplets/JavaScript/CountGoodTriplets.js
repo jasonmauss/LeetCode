ae23ced2b60798ef23f7ea1e4d20bced2312c878
Jason Mauss <jason.mauss@gmail.com>
2023-03-31 22:00:28 -0700

    Adding Solutions for TypeScript and JavaScript

--- CountGoodTriplets/JavaScript/CountGoodTriplets.js
+++ CountGoodTriplets/JavaScript/CountGoodTriplets.js
@@ -1,6 +1,19 @@
 // Solution for: https://leetcode.com/problems/count-good-triplets/
 const countGoodTriplets = (arr, a, b, c) => {
-    return 0;
+    let returnValue = 0;
+    let length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            for (let k = j + 1; k < length; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a &&
+                    Math.abs(arr[j] - arr[k]) <= b &&
+                    Math.abs(arr[i] - arr[k]) <= c) {
+                    returnValue++;
+                }
+            }
+        }
+    }
+    return returnValue;
 };
 // some test cases
 console.log(countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3)); // 4
