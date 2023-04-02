b58762e557f7e137140f43b8be56cdb7c5b30e0d
Jason Mauss <jason.mauss@gmail.com>
2023-04-01 23:28:16 -0700

    initial commit

---
+++ UniqueSubjectsTaught/MySQL/UniqueSubjectsTaught.sql
@@ -0,0 +1,16 @@
+-- Solution for: https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS Teacher(teacher_id INT, subject_id INT, dept_id INT);
+
+TRUNCATE TABLE Teacher;
+
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('1', '2', '3');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('1', '2', '4');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('1', '3', '3');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('2', '1', '1');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('2', '2', '1');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('2', '3', '1');
+INSERT INTO Teacher (teacher_id, subject_id, dept_id) VALUES ('2', '4', '1');
+
+-- SQL Solution:
