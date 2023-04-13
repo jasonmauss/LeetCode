3054503c7b77c7049c97f9d0b095a765780499a0
Jason Mauss <jason.mauss@gmail.com>
2023-04-12 23:18:31 -0700

    Adding Solutions for TypeScript and JavaScript

--- FilterElementsFromArray/JavaScript/FilterElementsFromArray.js
+++ FilterElementsFromArray/JavaScript/FilterElementsFromArray.js
@@ -1,8 +1,14 @@
 // Solution for: https://leetcode.com/problems/filter-elements-from-array/description/
 const filter = (arr, fn) => {
-    return [0];
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
 // some test cases
-console.log([0, 10, 20, 30], function greaterThan10(n) { return n > 10; }); // [20, 30]
-console.log([1, 2, 3], function firstIndex(n, i) { return i === 0; }); // [1]
-console.log([-2, -1, 0, 1, 2], function plusOne(n) { return n + 1; }); // [-2, 0, 1, 2]
+console.log(filter([0, 10, 20, 30], function greaterThan10(n) { return n > 10; })); // [20, 30]
+console.log(filter([1, 2, 3], function firstIndex(n, i) { return i === 0; })); // [1]
+console.log(filter([-2, -1, 0, 1, 2], function plusOne(n) { return n + 1; })); // [-2, 0, 1, 2]
