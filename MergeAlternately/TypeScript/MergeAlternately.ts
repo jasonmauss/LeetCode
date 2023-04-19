196066b95ae01052f1c618a5f041c7970e864869
Jason Mauss <jason.mauss@gmail.com>
2023-04-18 21:24:14 -0700

    initial commit

---
+++ MergeAlternately/TypeScript/MergeAlternately.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/merge-strings-alternately/
+const mergeAlternately = (word1: string, word2: string): string => {
+
+    return '';
+
+};
+
+
+
+
+// some test cases
+console.log(mergeAlternately("abc", "pqr")); // apbqcr
+console.log(mergeAlternately("ab", "pqrs")); // apbqrs
+console.log(mergeAlternately("abcd", "pq")); // apbqcd
