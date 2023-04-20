64c78239e9bd0d9923eda404643effdf82a951a9
Jason Mauss <jason.mauss@gmail.com>
2023-04-19 22:14:24 -0700

    Adding Solutions for TypeScript and JavaScript

--- MergeAlternately/JavaScript/MergeAlternately.js
+++ MergeAlternately/JavaScript/MergeAlternately.js
@@ -1,6 +1,16 @@
 // Solution for: https://leetcode.com/problems/merge-strings-alternately/
 const mergeAlternately = (word1, word2) => {
-    return '';
+    const lengthOfLongestWord = Math.max(word1.length, word2.length);
+    let mergedWord = '';
+    for (let charIndex = 0; charIndex < lengthOfLongestWord; charIndex++) {
+        if (word1.length > charIndex) {
+            mergedWord += word1[charIndex];
+        }
+        if (word2.length > charIndex) {
+            mergedWord += word2[charIndex];
+        }
+    }
+    return mergedWord;
 };
 // some test cases
 console.log(mergeAlternately("abc", "pqr")); // apbqcr
