cbd41a18c6fcbccbaeaa1296b5bd2b7835386d69
Jason Mauss <jason.mauss@gmail.com>
2023-04-19 22:20:44 -0700

    initial commit

---
+++ RemoveOuterParentheses/JavaScript/RemoveOuterParentheses.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/remove-outermost-parentheses/
+const removeOuterParentheses = (s) => {
+    return '';
+};
+// some test cases
+console.log(removeOuterParentheses('(()())(())')); // "()()()"
+console.log(removeOuterParentheses('(()())(())(()(()))')); // "()()()()(())"
+console.log(removeOuterParentheses('()()')); // ""
---
+++ RemoveOuterParentheses/TypeScript/RemoveOuterParentheses.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/remove-outermost-parentheses/
+const removeOuterParentheses = (s: string): string => {
+
+    return '';
+
+};
+
+
+
+
+
+// some test cases
+console.log(removeOuterParentheses('(()())(())')); // "()()()"
+console.log(removeOuterParentheses('(()())(())(()(()))')); // "()()()()(())"
+console.log(removeOuterParentheses('()()')); // ""
