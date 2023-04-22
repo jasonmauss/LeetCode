6a3217614278cc02d63d0cd204c718a3138b6cab
Jason Mauss <jason.mauss@gmail.com>
2023-04-21 23:17:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- GenerateFibonacci/JavaScript/GenerateFibonacci.js
+++ GenerateFibonacci/JavaScript/GenerateFibonacci.js
@@ -1,11 +1,24 @@
 // Solution for: https://leetcode.com/problems/generate-fibonacci-sequence/
 function* fibGenerator() {
+    yield 0;
+    yield 1;
+    let prevValue = 0;
+    let nextValue = 1;
+    while (true) {
+        const returnValue = prevValue + nextValue;
+        prevValue = nextValue;
+        nextValue = returnValue;
+        yield returnValue;
+    }
 }
 ;
-/**
- * const gen = fibGenerator();
- * gen.next().value; // 0
- * gen.next().value; // 1
- */
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
+console.log(gen.next().value); // 5
+console.log(gen.next().value); // 8
 // some test cases
-console.log(); // 
+//console.log(); // 
--- GenerateFibonacci/TypeScript/GenerateFibonacci.ts
+++ GenerateFibonacci/TypeScript/GenerateFibonacci.ts
@@ -1,15 +1,31 @@
 // Solution for: https://leetcode.com/problems/generate-fibonacci-sequence/
 function* fibGenerator(): Generator<number, any, number> {
 
+    yield 0;
+    yield 1;
+
+    let prevValue:number = 0;
+    let nextValue:number = 1;
+
+    while(true) {
+        const returnValue:number = prevValue + nextValue;
+        prevValue = nextValue;
+        nextValue = returnValue;
+        yield returnValue;
+    }
 };
 
-/**
- * const gen = fibGenerator();
- * gen.next().value; // 0
- * gen.next().value; // 1
- */
+
+const gen = fibGenerator();
+console.log(gen.next().value); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next().value); // 3
+console.log(gen.next().value); // 5
+console.log(gen.next().value); // 8
 
 
 
 // some test cases
-console.log(); // 
+//console.log(); // 
