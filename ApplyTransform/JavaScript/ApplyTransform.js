945965f6ea8315ec1cb84d9b963a19a70d33d450
Jason Mauss <jason.mauss@gmail.com>
2023-04-23 22:21:10 -0700

    Adding Solutions for TypeScript and JavaScript

--- ApplyTransform/JavaScript/ApplyTransform.js
+++ ApplyTransform/JavaScript/ApplyTransform.js
@@ -1,7 +1,12 @@
 // Solution for: https://leetcode.com/problems/apply-transform-over-each-element-in-array/
-function map(arr, fn) {
-    return [0];
-}
-;
+const map = (arr, fn) => {
+    const returnValues = [];
+    for (let i = 0; i < arr.length; i++) {
+        returnValues.push(fn(arr[i], i));
+    }
+    return returnValues;
+};
 // some test cases
-console.log(); // 
+console.log(map([1, 2, 3], function plusone(n) { return n + 1; })); // [2,3,4]
+console.log(map([1, 2, 3], function plusI(n, i) { return n + i; })); // [1,3,5]
+console.log(map([10, 20, 30], function constant() { return 42; })); // [42,42,42]
