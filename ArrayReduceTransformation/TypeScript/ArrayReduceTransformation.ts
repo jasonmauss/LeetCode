aac050e79b1c02ebda4674d6398c065f9640e61e
Jason Mauss <jason.mauss@gmail.com>
2023-04-25 22:24:11 -0700

    initial commit

---
+++ ArrayReduceTransformation/TypeScript/ArrayReduceTransformation.ts
@@ -0,0 +1,17 @@
+// Solution for: https://leetcode.com/problems/array-reduce-transformation/
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+
+    return 0;
+    
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(); // 
