b44327b8e80ae4d155e7520dc827399b2f6b970c
Jason Mauss <jason.mauss@gmail.com>
2023-04-26 22:39:07 -0700

    initial commit

---
+++ SumMultiples/TypeScript/SumMultiples.ts
@@ -0,0 +1,18 @@
+// Solution for: https://leetcode.com/problems/sum-multiples/
+const sumOfMultiples = (n: number): number => {
+
+    return 0;
+    
+};
+
+
+
+
+
+
+// some test cases
+console.log(sumOfMultiples(7)); // 21
+
+console.log(sumOfMultiples(10)); // 40
+
+console.log(sumOfMultiples(9)); // 30
