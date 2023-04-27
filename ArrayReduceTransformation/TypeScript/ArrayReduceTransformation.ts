84822ee4082c162964a9c4fc7df82af5dc6d6f73
Jason Mauss <jason.mauss@gmail.com>
2023-04-26 22:16:41 -0700

    Adding Solutions for TypeScript and JavaScript

--- ArrayReduceTransformation/TypeScript/ArrayReduceTransformation.ts
+++ ArrayReduceTransformation/TypeScript/ArrayReduceTransformation.ts
@@ -3,15 +3,32 @@ type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
 
-    return 0;
-    
+    let accumulator = init;
+
+    for(const num of nums) {
+        accumulator = fn(accumulator, num);
+    }
+
+    return accumulator;
+
 };
 
 
 
 
 
-
-
 // some test cases
-console.log(); // 
+let nums = [1,2,3,4];
+let fn = function sum(accum, curr) { return accum + curr; }
+let init = 0;
+console.log(reduce(nums, fn, init)); // 10
+
+nums = [1,2,3,4];
+fn = function sum(accum, curr) { return accum + curr * curr; }
+init = 100;
+console.log(reduce(nums, fn, init)); // 130
+
+nums = [];
+fn = function sum(accum, curr) { return 0; }
+init = 25;
+console.log(reduce(nums, fn, init)); // 25
