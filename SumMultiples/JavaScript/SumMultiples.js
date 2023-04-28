979b253bb251a9300728f863ab90f36581ddcd7f
Jason Mauss <jason.mauss@gmail.com>
2023-04-27 23:34:32 -0700

    Adding Solutions for TypeScript and JavaScript

--- SumMultiples/JavaScript/SumMultiples.js
+++ SumMultiples/JavaScript/SumMultiples.js
@@ -1,29 +1,17 @@
 // Solution for: https://leetcode.com/problems/sum-multiples/
-const sumOfMultiples = (n: number): number => {
-
-    if(n < 3) return 0;
-
+const sumOfMultiples = (n) => {
+    if (n < 3)
+        return 0;
     let rangeNumbers = [3, 5, 7];
     let multiplesSum = 0;
-
-    for(let i = 3; i <= n; i++) {
+    for (let i = 3; i <= n; i++) {
         if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
             multiplesSum += i;
         }
     }
-
     return multiplesSum;
-    
 };
-
-
-
-
-
-
 // some test cases
 console.log(sumOfMultiples(7)); // 21
-
 console.log(sumOfMultiples(10)); // 40
-
 console.log(sumOfMultiples(9)); // 30
