34a727cf60b8b53de4f8d488c6474c7edbabe2c0
Jason Mauss <jason.mauss@gmail.com>
2023-04-27 23:42:01 -0700

    initial commit

---
+++ FindDelayedArrivalTime/JavaScript/FindDelayedArrivalTime.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/calculate-delayed-arrival-time/
+const findDelayedArrivalTime = (arrivalTime, delayedTime) => {
+    return 0;
+};
+// some test cases
+console.log(findDelayedArrivalTime(15, 5)); // 20
+console.log(findDelayedArrivalTime(13, 11)); // 0
