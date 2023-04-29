ad2eff814e0bb1aea9ca109e0349cd64f7f156e9
Jason Mauss <jason.mauss@gmail.com>
2023-04-29 02:10:43 -0700

    initial commit

---
+++ MatrixDiaganolSum/JavaScript/MatrixDiaganolSum.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/matrix-diagonal-sum/
+const diagonalSum = (mat) => {
+    return 0;
+};
+// some test cases
+console.log(diagonalSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]])); // 25
+console.log(diagonalSum([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])); // 8
+console.log(diagonalSum([[5]])); // 5
