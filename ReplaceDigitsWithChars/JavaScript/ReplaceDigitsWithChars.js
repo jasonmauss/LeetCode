1d472cb588cd13e919204030b7435b4a4251ddca
Jason Mauss <jason.mauss@gmail.com>
2023-04-30 21:15:28 -0700

    initial commit

---
+++ ReplaceDigitsWithChars/JavaScript/ReplaceDigitsWithChars.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/replace-all-digits-with-characters/
+const replaceDigits = (s) => {
+    return '';
+};
+// some test cases
+console.log(replaceDigits('a1c1e1')); // abcdef
+console.log(replaceDigits('a1b2c3d4e')); // abbdcfdhe
---
+++ ReplaceDigitsWithChars/TypeScript/ReplaceDigitsWithChars.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/replace-all-digits-with-characters/
+const replaceDigits = (s: string): string => {
+
+    return '';
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(replaceDigits('a1c1e1')); // abcdef
+console.log(replaceDigits('a1b2c3d4e')); // abbdcfdhe
