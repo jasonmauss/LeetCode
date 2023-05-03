aefd5ddc74775284fddbc91df3e256640b50b69d
Jason Mauss <jason.mauss@gmail.com>
2023-05-02 22:26:55 -0700

    initial commit

---
+++ SubsetXORSum/TypeScript/SubsetXORSum.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+const subsetXORSum = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(subsetXORSum([1,3])); // 6
+console.log(subsetXORSum([5,1,6])); // 28
+console.log(subsetXORSum([3,4,5,6,7,8])); // 480
