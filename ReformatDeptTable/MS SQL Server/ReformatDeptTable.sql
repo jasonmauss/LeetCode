23f423cd5c811e9020acf56b7907e04724171a51
Jason Mauss <jason.mauss@gmail.com>
2023-05-02 22:22:02 -0700

    Adding Solutions for MySQL and MS SQL Server

--- ReformatDeptTable/MS SQL Server/ReformatDeptTable.sql
+++ ReformatDeptTable/MS SQL Server/ReformatDeptTable.sql
@@ -4,7 +4,7 @@ USE leetcodedb;
 GO
 
 IF OBJECT_ID(N'dbo.Department', N'U') IS NULL BEGIN
-	CREATE TABLE Department (id int, revenue int, month varchar(5));
+	CREATE TABLE Department (id INT, revenue INT, month VARCHAR(5));
 END
 GO
 
@@ -20,5 +20,43 @@ INSERT INTO Department (id, revenue, month) VALUES (1,6000, 'Mar')
 
 
 -- SQL Solution:
-	
+	SELECT	d.id,
+			SUM(
+			CASE WHEN month = 'Jan' THEN revenue ELSE NULL END
+			) AS Jan_Revenue,
+			SUM(
+			CASE WHEN month = 'Feb' THEN revenue ELSE NULL END
+			) AS Feb_Revenue,
+			SUM(
+			CASE WHEN month = 'Mar' THEN revenue ELSE NULL END
+			) AS Mar_Revenue,
+			SUM(
+			CASE WHEN month = 'Apr' THEN revenue ELSE NULL END
+			) AS Apr_Revenue,
+			SUM(
+			CASE WHEN month = 'May' THEN revenue ELSE NULL END
+			) AS May_Revenue,
+			SUM(
+			CASE WHEN month = 'Jun' THEN revenue ELSE NULL END
+			) AS Jun_Revenue,
+			SUM(
+			CASE WHEN month = 'Jul' THEN revenue ELSE NULL END
+			) AS Jul_Revenue,
+			SUM(
+			CASE WHEN month = 'Aug' THEN revenue ELSE NULL END
+			) AS aug_Revenue,
+			SUM(
+			CASE WHEN month = 'Sep' THEN revenue ELSE NULL END
+			) AS Sep_Revenue,
+			SUM(
+			CASE WHEN month = 'Oct' THEN revenue ELSE NULL END
+			) AS Oct_Revenue,
+			SUM(
+			CASE WHEN month = 'Nov' THEN revenue ELSE NULL END
+			) AS Nov_Revenue,
+			SUM(
+			CASE WHEN month = 'Dec' THEN revenue ELSE NULL END
+			) AS Dec_Revenue
+	  FROM	Department d
+  GROUP BY	d.id
 	
