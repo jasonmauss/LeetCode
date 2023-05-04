1dff7f76bbc2871c8621e392efb20f7442062f7b
Jason Mauss <jason.mauss@gmail.com>
2023-05-03 23:49:58 -0700

    initial commit

---
+++ PivotInteger/JavaScript/PivotInteger.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/find-the-pivot-integer/
+const pivotInteger = (n) => {
+    return 0;
+};
+// some test cases
+console.log(pivotInteger(8)); // 6
+console.log(pivotInteger(1)); // 1
+console.log(pivotInteger(4)); // -1
