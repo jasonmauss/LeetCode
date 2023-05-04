292690b5fd22a9b11682c2dd18d301ae25a23204
Jason Mauss <jason.mauss@gmail.com>
2023-05-03 23:39:22 -0700

    Adding Solutions for TypeScript and JavaScript

--- SubsetXORSum/JavaScript/SubsetXORSum.js
+++ SubsetXORSum/JavaScript/SubsetXORSum.js
@@ -1,29 +1,16 @@
 // Solution for: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
-const subsetXORSum = (nums: number[]): number => {
-
-    let subsetXoRs: number[] = [];
-
-    const backtrack = (currentXoR: number, id: number): void => {
-
+const subsetXORSum = (nums) => {
+    let subsetXoRs = [];
+    const backtrack = (currentXoR, id) => {
         subsetXoRs.push(currentXoR);
-        
         for (let i = id; i < nums.length; i++) {
             backtrack(currentXoR ^ nums[i], i + 1);
         }
-    }
-
+    };
     backtrack(0, 0);
-
     return subsetXoRs.reduce((acc, curr) => acc + curr, 0);
-
 };
-
-
-
-
-
-
 // some test cases
-console.log(subsetXORSum([1,3])); // 6
-console.log(subsetXORSum([5,1,6])); // 28
-console.log(subsetXORSum([3,4,5,6,7,8])); // 480
+console.log(subsetXORSum([1, 3])); // 6
+console.log(subsetXORSum([5, 1, 6])); // 28
+console.log(subsetXORSum([3, 4, 5, 6, 7, 8])); // 480
