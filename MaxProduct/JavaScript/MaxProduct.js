f237e3057cfd5b71a9a7c610d4de956306076dac
Jason Mauss <jason.mauss@gmail.com>
2023-05-06 20:51:04 -0700

    Adding Solutions for TypeScript and JavaScript

--- MaxProduct/JavaScript/MaxProduct.js
+++ MaxProduct/JavaScript/MaxProduct.js
@@ -1,6 +1,20 @@
 // Solution for: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
 const maxProduct = (nums) => {
-    return 0;
+    if (nums.length === 2) {
+        return (nums[0] - 1) * (nums[1] - 1);
+    }
+    let firstLargest = 0;
+    let secondLargest = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = nums[i];
+        }
+        else if (nums[i] > secondLargest) {
+            secondLargest = nums[i];
+        }
+    }
+    return (firstLargest - 1) * (secondLargest - 1);
 };
 // some test cases
 console.log(maxProduct([3, 4, 5, 2])); // 12
