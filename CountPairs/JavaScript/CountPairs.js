d8949cd090576fdbf6a0d1712a8bb583323b35ea
Jason Mauss <jason.mauss@gmail.com>
2023-05-08 23:20:19 -0700

    Adding Solutions for TypeScript and JavaScript

--- CountPairs/JavaScript/CountPairs.js
+++ CountPairs/JavaScript/CountPairs.js
@@ -1,6 +1,14 @@
 // Solution for: https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/
 const countPairs = (nums, k) => {
-    return 0;
+    let pairCount = 0;
+    for (let i = 0; i < nums.length; i++)
+        for (let j = i; j < nums.length; j++) {
+            if (j === i)
+                continue;
+            if (nums[i] === nums[j] && (i * j) % k === 0)
+                pairCount++;
+        }
+    return pairCount;
 };
 // some test cases
 console.log(countPairs([3, 1, 2, 2, 2, 1, 3], 2)); // 4
