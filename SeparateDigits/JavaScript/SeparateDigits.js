1f156341ddffe73a9dffd40794c8a5b11cc94902
Jason Mauss <jason.mauss@gmail.com>
2023-05-09 22:23:48 -0700

    Adding Solutions for TypeScript and JavaScript

--- SeparateDigits/JavaScript/SeparateDigits.js
+++ SeparateDigits/JavaScript/SeparateDigits.js
@@ -1,6 +1,13 @@
 // Solution for: https://leetcode.com/problems/separate-the-digits-in-an-array/
 const separateDigits = (nums) => {
-    return [0];
+    const outputNums = [];
+    for (let num of nums) {
+        let tempStringArray = num.toString().split('');
+        for (let chars of tempStringArray) {
+            outputNums.push(parseInt(chars));
+        }
+    }
+    return outputNums;
 };
 // some test cases
 console.log(separateDigits([13, 25, 83, 77])); // [1,3,2,5,8,3,7,7]
