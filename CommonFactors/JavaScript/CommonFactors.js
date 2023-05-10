067785937df2ff31d18d6ec89f8ab2807211c2dc
Jason Mauss <jason.mauss@gmail.com>
2023-05-09 22:27:02 -0700

    initial commit

---
+++ CommonFactors/JavaScript/CommonFactors.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/number-of-common-factors/
+const commonFactors = (a, b) => {
+    return 0;
+};
+// some test cases
+console.log(commonFactors(12, 6)); // 4
+console.log(commonFactors(25, 30)); // 2
