512606df7e1c5393cbcd32ae9e4ac5924cb6d882
Jason Mauss <jason.mauss@gmail.com>
2023-05-09 22:37:07 -0700

    initial commit

---
+++ MinTimeToVisitAllPoints/JavaScript/MinTimeToVisitAllPoints.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/minimum-time-visiting-all-points/
+const minTimeToVisitAllPoints = (points) => {
+    return 0;
+};
+// some test cases
+console.log(minTimeToVisitAllPoints([[1, 1], [3, 4], [-1, 0]])); // 7
+console.log(minTimeToVisitAllPoints([[3, 2], [-2, 2]])); // 5
