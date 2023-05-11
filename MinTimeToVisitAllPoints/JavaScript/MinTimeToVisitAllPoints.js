7eac890d25bc940165f5652b96e85e30e446f29c
Jason Mauss <jason.mauss@gmail.com>
2023-05-10 20:35:35 -0700

    Adding Solutions for TypeScript and JavaScript

--- MinTimeToVisitAllPoints/JavaScript/MinTimeToVisitAllPoints.js
+++ MinTimeToVisitAllPoints/JavaScript/MinTimeToVisitAllPoints.js
@@ -1,6 +1,12 @@
 // Solution for: https://leetcode.com/problems/minimum-time-visiting-all-points/
 const minTimeToVisitAllPoints = (points) => {
-    return 0;
+    let minTime = 0;
+    for (let positionCounter = 1; positionCounter < points.length; positionCounter++) {
+        const xAxisTime = Math.abs(points[positionCounter][0] - points[positionCounter - 1][0]);
+        const yAxisTime = Math.abs(points[positionCounter][1] - points[positionCounter - 1][1]);
+        minTime += Math.max(xAxisTime, yAxisTime);
+    }
+    return minTime;
 };
 // some test cases
 console.log(minTimeToVisitAllPoints([[1, 1], [3, 4], [-1, 0]])); // 7
--- MinTimeToVisitAllPoints/TypeScript/MinTimeToVisitAllPoints.ts
+++ MinTimeToVisitAllPoints/TypeScript/MinTimeToVisitAllPoints.ts
@@ -1,7 +1,17 @@
 // Solution for: https://leetcode.com/problems/minimum-time-visiting-all-points/
 const minTimeToVisitAllPoints = (points: number[][]): number => {
 
-    return 0;
+    let minTime = 0;
+
+    for(let positionCounter = 1; positionCounter < points.length; positionCounter++) {
+        
+        const xAxisTime = Math.abs(points[positionCounter][0] - points[positionCounter - 1][0]);
+        const yAxisTime = Math.abs(points[positionCounter][1] - points[positionCounter - 1][1]);
+
+        minTime += Math.max(xAxisTime, yAxisTime);
+    }
+
+    return minTime;
 
 };
 
