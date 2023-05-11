a5ebb1b5358e9c81162ba3af11642f3331ce8b82
Jason Mauss <jason.mauss@gmail.com>
2023-05-10 20:45:51 -0700

    initial commit

---
+++ LargestAltitude/TypeScript/LargestAltitude.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/find-the-highest-altitude/
+const largestAltitude = (gain: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(largestAltitude([-5,1,5,0,-7])); // 1
+console.log(largestAltitude([-4,-3,-2,-1,4,3,2])); // 0
