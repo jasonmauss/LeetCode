bcb3c5841e8ebc82ab63d37fefe8e45cdb501046
Jason Mauss <jason.mauss@gmail.com>
2023-05-11 16:23:08 -0700

    initial commit

---
+++ CountGoodRectangles/TypeScript/CountGoodRectangles.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+const countGoodRectangles = (rectangles: number[][]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(countGoodRectangles([[5,8],[3,9],[5,12],[16,5]])); // 3
+console.log(countGoodRectangles([[2,3],[3,7],[4,3],[3,7]])); // 3
