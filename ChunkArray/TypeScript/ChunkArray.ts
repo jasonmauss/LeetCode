1ec2c22a9122275056161399667c4810f5b861e7
Jason Mauss <jason.mauss@gmail.com>
2023-05-12 22:29:51 -0700

    initial commit

---
+++ ChunkArray/TypeScript/ChunkArray.ts
@@ -0,0 +1,18 @@
+// Solution for: https://leetcode.com/problems/chunk-array/
+const chunk = (arr: any[], size: number): any[][] => {
+
+    return [];
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(chunk([1,2,3,4,5], 1)); // [[1],[2],[3],[4],[5]]
+console.log(chunk([1,9,6,3,2], 3)); // [[1,9,6],[3,2]]
+console.log(chunk([8,5,3,2,6], 6)); // [[8,5,3,2,6]]
+console.log(chunk([], 1)); // []
