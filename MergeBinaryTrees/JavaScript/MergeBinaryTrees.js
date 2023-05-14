c74f580eb25b593bec4918d6dd6897be724213e5
Jason Mauss <jason.mauss@gmail.com>
2023-05-14 00:34:59 -0700

    initial commit

---
+++ MergeBinaryTrees/JavaScript/MergeBinaryTrees.js
@@ -0,0 +1,19 @@
+// Solution for: https://leetcode.com/problems/merge-two-binary-trees/
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    val;
+    left;
+    right;
+    constructor(val, left, right) {
+        this.val = (val === undefined ? 0 : val);
+        this.left = (left === undefined ? null : left);
+        this.right = (right === undefined ? null : right);
+    }
+}
+const mergeTrees = (root1, root2) => {
+    return null;
+};
+// some test cases
+console.log(); // 
---
+++ MergeBinaryTrees/TypeScript/MergeBinaryTrees.ts
@@ -0,0 +1,30 @@
+// Solution for: https://leetcode.com/problems/merge-two-binary-trees/
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+const mergeTrees = (root1: TreeNode | null, root2: TreeNode | null): TreeNode | null => {
+
+    return null;
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(); // 
