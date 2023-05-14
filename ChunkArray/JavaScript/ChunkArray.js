de667ab5db75210222fcc991a14568d8f140d479
Jason Mauss <jason.mauss@gmail.com>
2023-05-13 20:14:40 -0700

    Adding Solutions for TypeScript and JavaScript

--- ChunkArray/JavaScript/ChunkArray.js
+++ ChunkArray/JavaScript/ChunkArray.js
@@ -1,6 +1,18 @@
 // Solution for: https://leetcode.com/problems/chunk-array/
 const chunk = (arr, size) => {
-    return [];
+    if (arr.length === 0)
+        return [];
+    const chunks = [];
+    for (let i = 0; i < arr.length; i += size) {
+        let tempChunk = [];
+        for (let j = i; j < i + size; j++) {
+            if (j < arr.length) {
+                tempChunk.push(arr[j]);
+            }
+        }
+        chunks.push(tempChunk);
+    }
+    return chunks;
 };
 // some test cases
 console.log(chunk([1, 2, 3, 4, 5], 1)); // [[1],[2],[3],[4],[5]]
