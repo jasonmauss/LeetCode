7cb6653cc3568b93321bdc75dc067a021ef5f4f5
Jason Mauss <jason.mauss@gmail.com>
2023-05-15 23:00:57 -0700

    Adding Solutions for TypeScript and JavaScript

--- MergeBinaryTrees/JavaScript/MergeBinaryTrees.js
+++ MergeBinaryTrees/JavaScript/MergeBinaryTrees.js
@@ -13,7 +13,22 @@ class TreeNode {
     }
 }
 const mergeTrees = (root1, root2) => {
-    return null;
+    // if either node is null, just return 
+    // the other since it's the only value
+    if (root1 === null)
+        return root2;
+    if (root2 === null)
+        return root1;
+    // if neither are null, return the sum of
+    // both values
+    root1.val += root2.val;
+    // now also deal with the descendant nodes
+    root1.left = mergeTrees(root1.left, root2.left);
+    root1.right = mergeTrees(root1.right, root2.right);
+    // return root1 since that's what we added root2 to.
+    return root1;
 };
+let t1 = new TreeNode(1, new TreeNode(3, new TreeNode(5, null, null), null), new TreeNode(2, null, null));
+let t2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4, null, null)), new TreeNode(3, null, new TreeNode(7, null, null)));
 // some test cases
-console.log(); // 
+console.log(mergeTrees(t1, t2)); // 
