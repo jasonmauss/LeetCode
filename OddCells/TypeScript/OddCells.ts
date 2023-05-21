36bda2b9ad7df370c0fd16ac71ab77beb8b68d10
Jason Mauss <jason.mauss@gmail.com>
2023-05-20 23:42:44 -0700

    initial commit

---
+++ OddCells/TypeScript/OddCells.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+const oddCells = (m: number, n: number, indices: number[][]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(oddCells(2, 3, [[0,1],[1,1]])); // 6
+console.log(oddCells(2, 2, [[1,1],[0,0]])); // 0
