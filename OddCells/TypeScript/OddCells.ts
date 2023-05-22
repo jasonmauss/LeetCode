09540c3e7dcb378173632e7cb4ea6b18e1d6e67d
Jason Mauss <jason.mauss@gmail.com>
2023-05-21 23:12:57 -0700

    Adding Solutions for TypeScript and JavaScript

--- OddCells/TypeScript/OddCells.ts
+++ OddCells/TypeScript/OddCells.ts
@@ -1,7 +1,34 @@
 // Solution for: https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
 const oddCells = (m: number, n: number, indices: number[][]): number => {
 
-    return 0;
+    const matrices:number[][] = [];
+    let oddCellCount:number = 0;
+
+    for(let i = 0; i < m; i++) {
+        const tempRow:number[] = Array(n).fill(0);
+        matrices.push(tempRow);
+    }
+
+    for(let i = 0; i < indices.length; i++) {
+        const row:number = indices[i][0];
+        const column:number = indices[i][1];
+
+        for(let j = 0; j < n; j++) {
+            matrices[row][j]  += 1;
+        }
+
+        for(let k = 0; k < m; k++) {
+            matrices[k][column] += 1;
+        }
+    }
+
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if(matrices[i][j] % 2 !== 0) oddCellCount++;
+        }
+    }
+
+    return oddCellCount;
 
 };
 
