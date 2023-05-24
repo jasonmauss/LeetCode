dbf463976df462e8b30c1ee8eb0f4dcfbf51a6ee
Jason Mauss <jason.mauss@gmail.com>
2023-05-23 21:43:26 -0700

    Adding Solutions for TypeScript and JavaScript

--- EvaluateBooleanTree/JavaScript/EvaluateBooleanTree.js
+++ EvaluateBooleanTree/JavaScript/EvaluateBooleanTree.js
@@ -13,8 +13,51 @@ class TreeNode {
     }
 }
 const evaluateTree = (root) => {
-    return false;
+    // if the node is a leaf node and doesn't have a
+    // left or right node, just return the value of the node
+    if (!root.left && !root.right) {
+        return Boolean(root.val);
+    }
+    let boolLeft;
+    let boolRight;
+    if (root.left.val < 2) {
+        boolLeft = Boolean(root.left.val);
+    }
+    else {
+        boolLeft = evaluateTree(root.left);
+    }
+    if (root.right.val < 2) {
+        boolRight = Boolean(root.right.val);
+    }
+    else {
+        boolRight = evaluateTree(root.right);
+    }
+    if (root.val === 2) { // OR
+        if (boolLeft || boolRight) {
+            root.val = 1;
+            return true;
+        }
+        else {
+            root.val = 0;
+            return false;
+        }
+    }
+    if (root.val === 3) { // AND
+        if (boolLeft && boolRight) {
+            root.val = 1;
+            return true;
+        }
+        else {
+            root.val = 0;
+            return false;
+        }
+    }
+    return evaluateTree(root);
 };
 // some test cases
-let rootNode = new TreeNode();
-console.log(evaluateTree(rootNode)); //
+let rootNode = new TreeNode(2, new TreeNode(1, null, null), new TreeNode(3, new TreeNode(0, null, null), new TreeNode(1, null, null)));
+console.log(evaluateTree(rootNode)); // true
+let rootNode2 = new TreeNode(0, null, null);
+console.log(evaluateTree(rootNode2)); // false
+let rootNode3 = new TreeNode(3, new TreeNode(3, new TreeNode(0, null, null), new TreeNode(0, null, null)), new TreeNode(2, new TreeNode(3, new TreeNode(1, null, null), new TreeNode(3, new TreeNode(2, new TreeNode(1, null, null), new TreeNode(1, null, null)), new TreeNode(1, null, null))), new TreeNode(2, new TreeNode(1, null, null), new TreeNode(1, null, null))));
+console.log(evaluateTree(rootNode3));
