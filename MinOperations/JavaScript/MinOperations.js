fe573de4e13a65f75bef63b5b924ed1861c1d337
Jason Mauss <jason.mauss@gmail.com>
2023-05-23 21:47:22 -0700

    initial commit

---
+++ MinOperations/JavaScript/MinOperations.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
+const minOperations = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(minOperations([1, 1, 1])); // 3
+console.log(minOperations([1, 5, 2, 4, 1])); // 14
+console.log(minOperations([8])); // 0
