2c39b27d3dd9be7341f9de7007c80289562ef37b
Jason Mauss <jason.mauss@gmail.com>
2023-05-24 23:01:52 -0700

    Adding Solutions for TypeScript and JavaScript

--- MinOperations/JavaScript/MinOperations.js
+++ MinOperations/JavaScript/MinOperations.js
@@ -1,6 +1,16 @@
 // Solution for: https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
 const minOperations = (nums) => {
-    return 0;
+    let totalOperations = 0;
+    let previousNumber = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] <= previousNumber) {
+            let operationsRequired = (previousNumber - nums[i]) + 1;
+            nums[i] = previousNumber + 1;
+            totalOperations += operationsRequired;
+        }
+        previousNumber = nums[i];
+    }
+    return totalOperations;
 };
 // some test cases
 console.log(minOperations([1, 1, 1])); // 3
