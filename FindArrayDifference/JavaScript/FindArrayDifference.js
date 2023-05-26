4b35757d1923089c2701c01086b7928b34c43628
Jason Mauss <jason.mauss@gmail.com>
2023-05-25 23:08:46 -0700

    Adding Solutions for TypeScript and JavaScript

--- FindArrayDifference/JavaScript/FindArrayDifference.js
+++ FindArrayDifference/JavaScript/FindArrayDifference.js
@@ -1,6 +1,21 @@
 // Solution for: https://leetcode.com/problems/find-the-difference-of-two-arrays/
 const findDifference = (nums1, nums2) => {
-    return [];
+    const differences = [];
+    const answer0 = [];
+    const answer1 = [];
+    for (let num of nums1) {
+        if (!nums2.includes(num) && !answer0.includes(num)) {
+            answer0.push(num);
+        }
+    }
+    for (let num of nums2) {
+        if (!nums1.includes(num) && !answer1.includes(num)) {
+            answer1.push(num);
+        }
+    }
+    differences.push(answer0);
+    differences.push(answer1);
+    return differences;
 };
 // some test cases
 console.log(findDifference([1, 2, 3], [2, 4, 6])); // [[1,3],[4,6]]
--- FindArrayDifference/TypeScript/FindArrayDifference.ts
+++ FindArrayDifference/TypeScript/FindArrayDifference.ts
@@ -1,7 +1,25 @@
 // Solution for: https://leetcode.com/problems/find-the-difference-of-two-arrays/
 const findDifference = (nums1: number[], nums2: number[]): number[][] => {
 
-    return [];
+    const differences = [];
+    const answer0 = [];
+    const answer1 = [];
+
+    for(let num of nums1) {
+        if(!nums2.includes(num) && !answer0.includes(num)) {
+            answer0.push(num);
+        }
+    }
+
+    for(let num of nums2) {
+        if(!nums1.includes(num) && !answer1.includes(num)) {
+            answer1.push(num);
+        }
+    }
+
+    differences.push(answer0);
+    differences.push(answer1);
+    return differences;
 
 };
 
