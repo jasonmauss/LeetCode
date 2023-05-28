551fb5cb8d791310e37d903e4a5006db3f36a6e3
Jason Mauss <jason.mauss@gmail.com>
2023-05-27 22:51:48 -0700

    initial commit

---
+++ RemoveTrailingZeroes/TypeScript/RemoveTrailingZeroes.ts
@@ -0,0 +1,18 @@
+// Solution for: https://leetcode.com/problems/remove-trailing-zeros-from-a-string/
+const removeTrailingZeros = (num: string): string => {
+    
+    return '';
+
+};
+
+
+
+
+
+
+
+
+
+// some test cases
+console.log(removeTrailingZeros('51230100')); // 512301
+console.log(removeTrailingZeros('123')); // 123
