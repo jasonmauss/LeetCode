9949517be86c062f5fd370ac5d93433e8e356d7e
Jason Mauss <jason.mauss@gmail.com>
2023-05-27 22:46:24 -0700

    Adding Solutions for TypeScript and JavaScript

--- DIStringMatch/JavaScript/DIStringMatch.js
+++ DIStringMatch/JavaScript/DIStringMatch.js
@@ -1,6 +1,25 @@
 // Solution for: https://leetcode.com/problems/di-string-match/
 const diStringMatch = (s) => {
-    return [0];
+    const result = [];
+    let lastValue = 0;
+    let firstValue = 0;
+    for (let i = 0; i <= s.length; i++) {
+        switch (s[i]) {
+            case "D":
+                result.push(s.length - lastValue++);
+                break;
+            case "I":
+                result.push(firstValue++);
+                break;
+            default:
+                if (s[i - 1] === "D")
+                    result.push(s.length - lastValue);
+                if (s[i - 1] === "I")
+                    result.push(firstValue);
+                break;
+        }
+    }
+    return result;
 };
 // some test cases
 console.log(diStringMatch("IDID")); // [0,4,1,3,2]
