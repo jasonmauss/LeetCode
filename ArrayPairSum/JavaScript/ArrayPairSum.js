7be511e276856e5e918db3cbd2d6a76cee6962b5
Jason Mauss <jason.mauss@gmail.com>
2023-05-27 23:17:11 -0700

    initial commit

---
+++ ArrayPairSum/JavaScript/ArrayPairSum.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/array-partition/
+const arrayPairSum = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(arrayPairSum([1, 4, 3, 2])); // 4
+console.log(arrayPairSum([6, 2, 6, 5, 1, 2])); // 9
