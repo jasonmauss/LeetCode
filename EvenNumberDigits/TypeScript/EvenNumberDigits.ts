4222bcc182ad78e227c6c28d8914e80d83184bde
Jason Mauss <jason.mauss@gmail.com>
2023-06-01 22:39:24 -0700

    initial commit

---
+++ EvenNumberDigits/TypeScript/EvenNumberDigits.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+const findNumbers = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(findNumbers([12,345,2,6,7896])); // 2
+console.log(findNumbers([555,901,482,1771])); // 1
