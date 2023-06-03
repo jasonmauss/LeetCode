5ebdec9f0a0c82e87b16e09a56308e4c3d774237
Jason Mauss <jason.mauss@gmail.com>
2023-06-02 21:45:41 -0700

    initial commit

---
+++ SumBaseDigits/JavaScript/SumBaseDigits.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/sum-of-digits-in-base-k/
+const sumBase = (n, k) => {
+    return 0;
+};
+// some test cases
+console.log(sumBase(34, 6)); // 9
+console.log(sumBase(10, 10)); // 1
---
+++ SumBaseDigits/TypeScript/SumBaseDigits.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/sum-of-digits-in-base-k/
+const sumBase = (n: number, k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(sumBase(34, 6)); // 9
+console.log(sumBase(10, 10)); // 1
