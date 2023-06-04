a45a75104b7fabb605bda4d6bcf7322f3bb44aa8
Jason Mauss <jason.mauss@gmail.com>
2023-06-03 23:27:58 -0700

    Adding Solutions for TypeScript and JavaScript

--- SumBaseDigits/JavaScript/SumBaseDigits.js
+++ SumBaseDigits/JavaScript/SumBaseDigits.js
@@ -1,6 +1,13 @@
 // Solution for: https://leetcode.com/problems/sum-of-digits-in-base-k/
 const sumBase = (n, k) => {
-    return 0;
+    let sumOfDigits = 0;
+    let digits = n.toString(k).split('');
+    let numbers = [];
+    for (let digit of digits) {
+        numbers.push(parseInt(digit));
+    }
+    sumOfDigits = numbers.reduce((a, b) => a + b, 0);
+    return sumOfDigits;
 };
 // some test cases
 console.log(sumBase(34, 6)); // 9
