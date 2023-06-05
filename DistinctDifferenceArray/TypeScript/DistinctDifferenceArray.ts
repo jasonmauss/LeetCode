df405daa60bf38d0d2bee32adeb2143b4478e0eb
Jason Mauss <jason.mauss@gmail.com>
2023-06-04 18:46:18 -0700

    initial commit

---
+++ DistinctDifferenceArray/TypeScript/DistinctDifferenceArray.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/find-the-distinct-difference-array/
+const distinctDifferenceArray = (nums: number[]): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(distinctDifferenceArray([1,2,3,4,5])); // [-3,-1,1,3,5]
+console.log(distinctDifferenceArray([3,2,3,4,2])); // [-2,-1,0,2,3]
