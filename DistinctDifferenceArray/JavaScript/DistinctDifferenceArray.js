280d784c1c043c880c36fcb6dd59a10c440d0816
Jason Mauss <jason.mauss@gmail.com>
2023-06-05 00:00:36 -0700

    Adding Solutions for TypeScript and JavaScript

--- DistinctDifferenceArray/JavaScript/DistinctDifferenceArray.js
+++ DistinctDifferenceArray/JavaScript/DistinctDifferenceArray.js
@@ -1,6 +1,12 @@
 // Solution for: https://leetcode.com/problems/find-the-distinct-difference-array/
 const distinctDifferenceArray = (nums) => {
-    return [0];
+    const differences = [];
+    for (let i = 0; i < nums.length; i++) {
+        let distinctPrefixValues = new Set(nums.slice(0, i + 1)).size;
+        let distinctSuffixValues = new Set(nums.slice(i + 1)).size;
+        differences.push(distinctPrefixValues - distinctSuffixValues);
+    }
+    return differences;
 };
 // some test cases
 console.log(distinctDifferenceArray([1, 2, 3, 4, 5])); // [-3,-1,1,3,5]
