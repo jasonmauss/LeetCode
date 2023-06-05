7f773ab86e7c20667bcf1ca1a21846fdc2d1ffe9
Jason Mauss <jason.mauss@gmail.com>
2023-06-05 00:07:48 -0700

    initial commit

---
+++ ReverseString/JavaScript/ReverseString.js
@@ -0,0 +1,10 @@
+// Solution for:https://leetcode.com/problems/reverse-string/solutions/1511930/ts-solution/
+/**
+ Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s) => {
+    return;
+};
+// some test cases
+console.log(reverseString(["h", "e", "l", "l", "o"])); // ["o","l","l","e","h"]
+console.log(reverseString(["H", "a", "n", "n", "a", "h"])); // ["h","a","n","n","a","H"]
