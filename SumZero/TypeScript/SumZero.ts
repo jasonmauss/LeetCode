08bce61b0699d27c04e1a0a579353ddd305c18c7
Jason Mauss <jason.mauss@gmail.com>
2023-06-05 23:06:33 -0700

    initial commit

---
+++ SumZero/TypeScript/SumZero.ts
@@ -0,0 +1,19 @@
+// Solution for: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+const sumZero = (n: number): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+
+
+
+
+// some test cases
+console.log(sumZero(5)); //  [-7,-1,1,3,4] or [-5,-1,1,2,3] or [-3,-1,2,-2,4]
+console.log(sumZero(3)); //  [-1,0,1]
+console.log(sumZero(1)); //  [0]
