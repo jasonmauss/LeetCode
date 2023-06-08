148e4cc26f9d9695c3cf1a4c6f22efd9654f2f52
Jason Mauss <jason.mauss@gmail.com>
2023-06-07 22:58:35 -0700

    initial commit

---
+++ PrimaryDepartment/MS SQL Server/PrimaryDepartment.sql
@@ -0,0 +1,27 @@
+-- Solution for: https://leetcode.com/problems/primary-department-for-each-employee/
+
+USE leetcodedb;
+GO
+
+IF OBJECT_ID(N'dbo.Employee', N'U') IS NULL BEGIN
+	CREATE TABLE Employee (employee_id INT, department_id INT, primary_flag CHAR(1));
+END
+GO
+
+
+TRUNCATE TABLE Employee;
+
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (1, 1,'N');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (2, 1,'Y');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (2, 2,'N');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (3, 3,'N');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (4, 2,'N');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (4, 3,'Y');
+INSERT INTO Employee (employee_id, department_id, primary_flag) VALUES (4, 4,'N');
+
+
+-- SQL Solution:
+
+	 
+
+	
