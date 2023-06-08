0db9c81ea43c07ea1dfdf7037d8e8b805df05c68
Jason Mauss <jason.mauss@gmail.com>
2023-06-07 22:48:32 -0700

    Adding Solutions for TypeScript and JavaScript

--- AreOccurrencesEqual/TypeScript/AreOccurrencesEqual.ts
+++ AreOccurrencesEqual/TypeScript/AreOccurrencesEqual.ts
@@ -1,7 +1,26 @@
 // Solution for: https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
 const areOccurrencesEqual = (s: string): boolean => {
 
-    return false;
+    const occurences = {};
+
+    for(let i = 0; i < s.length; i++) {
+        occurences[s[i]] = occurences[s[i]] || {};
+        occurences[s[i]] = Number(occurences[s[i]]) + 1 || Number(1);
+    }
+
+    let occurenceCount;
+
+    for(const property in occurences) {
+        if(!occurenceCount) {
+            occurenceCount = occurences[property];
+        } else {
+            if(occurenceCount != occurences[property]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 
 };
 
