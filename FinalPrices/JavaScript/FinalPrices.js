f654d09310dddb741e969383cf2f35ef08b3f54c
Jason Mauss <jason.mauss@gmail.com>
2023-06-09 23:53:57 -0700

    initial commit

---
+++ FinalPrices/JavaScript/FinalPrices.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+const finalPrices = (prices) => {
+    return [0];
+};
+// some test cases
+console.log(finalPrices([8, 4, 6, 2, 3])); //[4,2,4,2,3]
+console.log(finalPrices([1, 2, 3, 4, 5])); //[1,2,3,4,5]
+console.log(finalPrices([10, 1, 1, 6])); //[9,0,1,6]
