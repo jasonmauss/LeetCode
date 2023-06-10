b968c5ef214e7ec82876bf715965ff678cd3bda7
Jason Mauss <jason.mauss@gmail.com>
2023-06-10 16:56:20 -0700

    initial commit

---
+++ RepeatedNTimes/JavaScript/RepeatedNTimes.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+const repeatedNTimes = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(repeatedNTimes([1, 2, 3, 3])); // 3
+console.log(repeatedNTimes([2, 1, 2, 5, 3, 2])); // 2
+console.log(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])); // 5
