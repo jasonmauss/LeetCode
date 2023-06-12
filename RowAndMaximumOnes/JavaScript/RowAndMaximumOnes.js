57d35deccebe10511675db9f71aa21f78f16727e
Jason Mauss <jason.mauss@gmail.com>
2023-06-11 17:06:00 -0700

    initial commit

---
+++ RowAndMaximumOnes/JavaScript/RowAndMaximumOnes.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/row-with-maximum-ones/
+const rowAndMaximumOnes = (mat) => {
+    return [0];
+};
+// some test cases
+console.log(rowAndMaximumOnes([[0, 1], [1, 0]])); // [0,1]
+console.log(rowAndMaximumOnes([[0, 0, 0], [0, 1, 1]])); // [1,2]
+console.log(rowAndMaximumOnes([[0, 0], [1, 1], [0, 0]])); // [1,2]
---
+++ RowAndMaximumOnes/TypeScript/RowAndMaximumOnes.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/row-with-maximum-ones/
+const rowAndMaximumOnes = (mat: number[][]): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+// some test cases
+console.log(rowAndMaximumOnes([[0,1],[1,0]])); // [0,1]
+console.log(rowAndMaximumOnes([[0,0,0],[0,1,1]])); // [1,2]
+console.log(rowAndMaximumOnes([[0,0],[1,1],[0,0]])); // [1,2]
