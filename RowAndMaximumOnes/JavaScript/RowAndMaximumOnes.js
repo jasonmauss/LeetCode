c6193075d8c97b10effff3503ebdc8a05915de92
Jason Mauss <jason.mauss@gmail.com>
2023-06-11 21:28:59 -0700

    Adding Solutions for TypeScript and JavaScript

--- RowAndMaximumOnes/JavaScript/RowAndMaximumOnes.js
+++ RowAndMaximumOnes/JavaScript/RowAndMaximumOnes.js
@@ -1,6 +1,15 @@
 // Solution for: https://leetcode.com/problems/row-with-maximum-ones/
 const rowAndMaximumOnes = (mat) => {
-    return [0];
+    let maxRow = 0;
+    let maxCount = 0;
+    for (let i = 0; i < mat.length; i++) {
+        let tempCount = mat[i].filter(x => x === 1).length;
+        if (tempCount > maxCount) {
+            maxRow = i;
+            maxCount = tempCount;
+        }
+    }
+    return [maxRow, maxCount];
 };
 // some test cases
 console.log(rowAndMaximumOnes([[0, 1], [1, 0]])); // [0,1]
