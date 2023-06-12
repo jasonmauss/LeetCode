e430ea8905979aa275e997f5b9a2dc5afb12eb87
Jason Mauss <jason.mauss@gmail.com>
2023-06-11 17:02:18 -0700

    Adding Solutions for TypeScript and JavaScript

--- VowelStrings/JavaScript/VowelStrings.js
+++ VowelStrings/JavaScript/VowelStrings.js
@@ -1,7 +1,16 @@
 // Solution for: https://leetcode.com/problems/count-the-number-of-vowel-strings-in-range/
 const vowelStrings = (words, left, right) => {
-    return 0;
+    let validVowelStringsCount = 0;
+    const vowels = "aeiou";
+    const rangeStrings = words.slice(left, right + 1);
+    for (const word of rangeStrings) {
+        if (vowels.includes(word.charAt(0)) &&
+            vowels.includes(word.charAt(word.length - 1)))
+            validVowelStringsCount++;
+    }
+    return validVowelStringsCount;
 };
 // some test cases
 console.log(vowelStrings(["are", "amy", "u"], 0, 2)); // 2
 console.log(vowelStrings(["hey", "aeo", "mu", "ooo", "artro"], 1, 4)); // 3
+console.log(vowelStrings(["vo", "j", "i", "s", "i"], 0, 3)); // 1
