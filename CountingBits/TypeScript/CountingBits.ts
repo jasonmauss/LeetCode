6b2853a764cd39a5fff22cc7f91cbac70829d104
Jason Mauss <jason.mauss@gmail.com>
2023-06-13 21:32:57 -0700

    initial commit

---
+++ CountingBits/TypeScript/CountingBits.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/counting-bits/
+const countBits = (n: number): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+
+
+// some test cases
+console.log(countBits(2)); // [0, 1, 1]
+console.log(countBits(5)); // [0, 1, 1, 2, 1, 2]
