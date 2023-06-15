c4335eeec96f3d920f13dd554a9174b7cca67db9
Jason Mauss <jason.mauss@gmail.com>
2023-06-14 23:45:19 -0700

    initial commit

---
+++ EqualFrenquency/TypeScript/EqualFrenquency.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/remove-letter-to-equalize-frequency/description/
+const equalFrequency = (word: string): boolean => {
+
+    return false;
+
+};
+
+
+
+
+
+// some test cases
+console.log(equalFrequency("abcc")); // true
+console.log(equalFrequency("abcc")); // false
