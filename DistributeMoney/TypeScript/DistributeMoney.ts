391cf106c1e1b57eadef69800128e236e123114c
Jason Mauss <jason.mauss@gmail.com>
2023-06-16 00:24:53 -0700

    initial commit

---
+++ DistributeMoney/TypeScript/DistributeMoney.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/distribute-money-to-maximum-children/
+const distMoney = (money: number, children: number): number => {
+
+    return 0;
+    
+};
+
+
+
+
+
+// some test cases
+console.log(distMoney(20, 3)); // 1
+console.log(distMoney(16, 2)); // 2
