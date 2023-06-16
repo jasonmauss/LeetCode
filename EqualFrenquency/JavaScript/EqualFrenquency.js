471578bd77f532dc11e069c335764fae17a24072
Jason Mauss <jason.mauss@gmail.com>
2023-06-15 23:09:48 -0700

    Adding Solutions for TypeScript and JavaScript

--- EqualFrenquency/JavaScript/EqualFrenquency.js
+++ EqualFrenquency/JavaScript/EqualFrenquency.js
@@ -1,7 +1,25 @@
 // Solution for: https://leetcode.com/problems/remove-letter-to-equalize-frequency/description/
 const equalFrequency = (word) => {
+    const frequencyCounts = {};
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        frequencyCounts[letter] = frequencyCounts[letter] || 0;
+        frequencyCounts[letter] = frequencyCounts[letter] + 1;
+    }
+    let frequenciesContainOne = false;
+    const frequencyValues = [];
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+        frequencyCounts[letter] -= 1;
+        if (frequencyCounts[letter] === 0)
+            delete frequencyCounts[letter];
+        if (new Set(Object.values(frequencyCounts)).size === 1)
+            return true;
+        frequencyCounts[letter] ? frequencyCounts[letter] += 1 : frequencyCounts[letter] = 1;
+    }
     return false;
 };
 // some test cases
 console.log(equalFrequency("abcc")); // true
 console.log(equalFrequency("aazz")); // false
+console.log(equalFrequency("bac")); // true
