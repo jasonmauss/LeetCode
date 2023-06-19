959fb781e6370cf30e14b063b59046b5ade1d635
Jason Mauss <jason.mauss@gmail.com>
2023-06-18 23:23:15 -0700

    Adding Solutions for TypeScript and JavaScript

--- DistributeMoney/TypeScript/DistributeMoney.ts
+++ DistributeMoney/TypeScript/DistributeMoney.ts
@@ -1,7 +1,22 @@
 // Solution for: https://leetcode.com/problems/distribute-money-to-maximum-children/
 const distMoney = (money: number, children: number): number => {
 
-    return 0;
+    if (money < children)
+            return -1;
+    
+        const drawback = children * 8 - money;
+    
+        if (!drawback)
+            return Number(children);
+    
+        if (drawback === 4)
+            return children - 2;
+    
+        if (drawback < 0)
+            return children - 1;
+    
+        return Number(children - Math.ceil(drawback / 7));
+        
     
 };
 
