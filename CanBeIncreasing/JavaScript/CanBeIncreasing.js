9cd1f49814390ece96339c938f9b8a637b44571b
Jason Mauss <jason.mauss@gmail.com>
2023-06-19 22:18:55 -0700

    initial commit

---
+++ CanBeIncreasing/JavaScript/CanBeIncreasing.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+const canBeIncreasing = (nums) => {
+    return false;
+};
+// some test cases
+console.log(canBeIncreasing([1, 2, 10, 5, 7])); //  true
+console.log(canBeIncreasing([2, 3, 1, 2])); //  false
+console.log(canBeIncreasing([1, 1, 1])); //  false
