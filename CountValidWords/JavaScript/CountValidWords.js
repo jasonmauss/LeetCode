ff4a9719fcd0b0ece37655c31add1d4b073dcdcf
Jason Mauss <jason.mauss@gmail.com>
2023-06-20 22:36:52 -0700

    initial commit

---
+++ CountValidWords/JavaScript/CountValidWords.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+const countValidWords = (sentence) => {
+    return 0;
+};
+// some test cases
+console.log(countValidWords("cat and  dog")); // 3
+console.log(countValidWords("!this  1-s b8d!")); // 0
+console.log(countValidWords("alice and  bob are playing stone-game10")); // 5
---
+++ CountValidWords/TypeScript/CountValidWords.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+const countValidWords = (sentence: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(countValidWords("cat and  dog")); // 3
+console.log(countValidWords("!this  1-s b8d!")); // 0
+console.log(countValidWords("alice and  bob are playing stone-game10")); // 5
