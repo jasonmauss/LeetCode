40d9b628f5301336820f9141ba11a89e6941929c
Jason Mauss <jason.mauss@gmail.com>
2023-06-23 22:14:31 -0700

    initial commit

---
+++ DiagonalPrime/JavaScript/DiagonalPrime.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/prime-in-diagonal/description/
+const diagonalPrime = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(diagonalPrime([[1, 2, 3], [5, 6, 7], [9, 10, 11]])); // 11
+console.log(diagonalPrime([[1, 2, 3], [5, 17, 7], [9, 11, 10]])); // 17
