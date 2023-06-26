66929f3d2dc18b6a0caede1c29222ded973a49aa
Jason Mauss <jason.mauss@gmail.com>
2023-06-25 23:06:59 -0700

    initial commit

---
+++ ValidMountainArray/TypeScript/ValidMountainArray.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/valid-mountain-array/
+const validMountainArray = (arr: number[]): boolean => {
+
+    return false;
+
+};
+
+
+
+
+
+// some test cases
+console.log(validMountainArray([2,1])); //  false
+console.log(validMountainArray([3,5,5])); //  false
+console.log(validMountainArray([0,3,2,1])); //  true
