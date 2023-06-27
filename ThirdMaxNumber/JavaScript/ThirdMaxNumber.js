6dd25a069e64928afe333168d5dc2f3b20ee3536
Jason Mauss <jason.mauss@gmail.com>
2023-06-26 22:59:39 -0700

    initial commit

---
+++ ThirdMaxNumber/JavaScript/ThirdMaxNumber.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/third-maximum-number/
+const thirdMax = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(thirdMax([3, 2, 1])); // 1
+console.log(thirdMax([1, 2])); // 2
+console.log(thirdMax([2, 2, 3, 1])); // 1
