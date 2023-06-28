112fcbed16a20651f5f0e94d84aed3d565e8b8ce
Jason Mauss <jason.mauss@gmail.com>
2023-06-27 22:47:03 -0700

    Adding Solutions for TypeScript and JavaScript

--- ThirdMaxNumber/TypeScript/ThirdMaxNumber.ts
+++ ThirdMaxNumber/TypeScript/ThirdMaxNumber.ts
@@ -1,8 +1,16 @@
 // Solution for: https://leetcode.com/problems/third-maximum-number/
 const thirdMax = (nums: number[]): number => {
 
-    return 0;
+    nums.sort((a,b) => b - a);
 
+    const numSet = new Set(nums);
+
+    const distinctValues = [...numSet];
+
+    if(distinctValues.length < 3) return Math.max(...distinctValues);
+
+    return distinctValues[2];
+    
 };
 
 
@@ -14,3 +22,4 @@ const thirdMax = (nums: number[]): number => {
 console.log(thirdMax([3,2,1])); // 1
 console.log(thirdMax([1,2])); // 2
 console.log(thirdMax([2,2,3,1])); // 1
+console.log(thirdMax([1,2,2,5,3,5])); // 2
