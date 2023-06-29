b410e32a1f5a9eb7564ebc1858d867c1350a9038
Jason Mauss <jason.mauss@gmail.com>
2023-06-28 21:17:28 -0700

    initial commit

---
+++ NumDifferentIntegers/JavaScript/NumDifferentIntegers.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/number-of-different-integers-in-a-string/
+const numDifferentIntegers = (word) => {
+    return 0;
+};
+// some test cases
+console.log(numDifferentIntegers('a123bc34d8ef34')); // 3
+console.log(numDifferentIntegers('leet1234code234')); // 2
+console.log(numDifferentIntegers('a1b01c001')); // 1
