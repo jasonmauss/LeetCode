064b7b5aa48cf6363eb2cbe4b061aa860780760a
Jason Mauss <jason.mauss@gmail.com>
2023-06-28 21:02:46 -0700

    initial commit

---
+++ CategorizeBox/JavaScript/CategorizeBox.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/categorize-box-according-to-criteria/
+const categorizeBox = (length, width, height, mass) => {
+    return '';
+};
+// some test cases
+console.log(categorizeBox(1000, 35, 700, 300)); // "Heavy"
+console.log(categorizeBox(200, 50, 800, 50)); // "Neither"
