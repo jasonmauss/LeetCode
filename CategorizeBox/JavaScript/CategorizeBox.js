0cb4bfc46f7e21ff5b9305851e6e6b88ba83d17c
Jason Mauss <jason.mauss@gmail.com>
2023-06-28 21:07:49 -0700

    Adding Solutions for TypeScript and JavaScript

--- CategorizeBox/JavaScript/CategorizeBox.js
+++ CategorizeBox/JavaScript/CategorizeBox.js
@@ -1,6 +1,20 @@
 // Solution for: https://leetcode.com/problems/categorize-box-according-to-criteria/
 const categorizeBox = (length, width, height, mass) => {
-    return '';
+    var isHeavy = false;
+    var isBulky = false;
+    const TenToFourth = Math.pow(10, 4);
+    const TenToNinth = Math.pow(10, 9);
+    if (length >= TenToFourth || width >= TenToFourth || height >= TenToFourth || length * width * height >= TenToNinth)
+        isBulky = true;
+    if (mass >= 100)
+        isHeavy = true;
+    if (isBulky && isHeavy)
+        return 'Both';
+    if (isBulky && !isHeavy)
+        return 'Bulky';
+    if (!isBulky && isHeavy)
+        return 'Heavy';
+    return 'Neither';
 };
 // some test cases
 console.log(categorizeBox(1000, 35, 700, 300)); // "Heavy"
