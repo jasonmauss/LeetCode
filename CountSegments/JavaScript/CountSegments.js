89cab7b7854d02e0e6019a72e6d67e11dbc42b8b
Jason Mauss <jason.mauss@gmail.com>
2023-06-29 21:56:21 -0700

    initial commit

---
+++ CountSegments/JavaScript/CountSegments.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/number-of-segments-in-a-string/
+const countSegments = (s) => {
+    return 0;
+};
+// some test cases
+console.log(countSegments("Hello, my name is John")); // 5
+console.log(countSegments("Hello")); // 1
---
+++ CountSegments/TypeScript/CountSegments.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/number-of-segments-in-a-string/
+const countSegments = (s: string): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(countSegments("Hello, my name is John")); // 5
+console.log(countSegments("Hello")); // 1
