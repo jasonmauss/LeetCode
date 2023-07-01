4fc45f28c59095a216ae00f622e258dc5fc90330
Jason Mauss <jason.mauss@gmail.com>
2023-06-30 21:26:39 -0700

    Adding Solutions for TypeScript and JavaScript

--- IsBoomerang/TypeScript/IsBoomerang.ts
+++ IsBoomerang/TypeScript/IsBoomerang.ts
@@ -1,7 +1,18 @@
 // Solution for: https://leetcode.com/problems/valid-boomerang/solutions/287019/java-0ms-solution/
 const isBoomerang = (points: number[][]): boolean => {
 
-    return false;
+    let x1 = points[0][0];
+    let x2 = points[1][0];
+    let x3 = points[2][0];
+
+    let y1 = points[0][1];
+    let y2 = points[1][1];
+    let y3 = points[2][1];
+
+    if ((((x1 - x2) * (y2 - y3)) - ((x2 - x3) * (y1 - y2))) == 0)
+        return false;
+    else
+        return true;
 
 };
 
