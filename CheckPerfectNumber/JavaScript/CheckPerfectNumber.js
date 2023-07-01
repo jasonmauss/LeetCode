132f4d3f7d87324a67f38763f7a321e80e7036ec
Jason Mauss <jason.mauss@gmail.com>
2023-06-30 21:30:42 -0700

    initial commit

---
+++ CheckPerfectNumber/JavaScript/CheckPerfectNumber.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/perfect-number/
+const checkPerfectNumber = (num) => {
+    return false;
+};
+// some test cases
+console.log(checkPerfectNumber(28)); // true
+console.log(checkPerfectNumber(7)); // false
