9078dec70f3e9ab949f2b71d20fdf68e463c196b
Jason Mauss <jason.mauss@gmail.com>
2023-06-30 21:23:13 -0700

    initial commit

---
+++ IsBoomerang/JavaScript/IsBoomerang.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/valid-boomerang/solutions/287019/java-0ms-solution/
+const isBoomerang = (points) => {
+    return false;
+};
+// some test cases
+console.log(isBoomerang([[1, 1], [2, 3], [3, 2]])); // true
+console.log(isBoomerang([[1, 1], [2, 2], [3, 3]])); // false
---
+++ IsBoomerang/TypeScript/IsBoomerang.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/valid-boomerang/solutions/287019/java-0ms-solution/
+const isBoomerang = (points: number[][]): boolean => {
+
+    return false;
+
+};
+
+
+
+
+
+// some test cases
+console.log(isBoomerang([[1,1],[2,3],[3,2]])); // true
+console.log(isBoomerang([[1,1],[2,2],[3,3]])); // false
