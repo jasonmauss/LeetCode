854c17ecf59526f4b8caa18214efcfe7d4f7eb32
Jason Mauss <jason.mauss@gmail.com>
2023-06-30 21:15:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- CountSegments/JavaScript/CountSegments.js
+++ CountSegments/JavaScript/CountSegments.js
@@ -1,6 +1,6 @@
 // Solution for: https://leetcode.com/problems/number-of-segments-in-a-string/
 const countSegments = (s) => {
-    return 0;
+    return s.split(/\s+/).filter(x => x.trim().length > 1).length;
 };
 // some test cases
 console.log(countSegments("Hello, my name is John")); // 5
