46a5c0be08c4bb615aa2e1a808ecec7eec7df8b5
Jason Mauss <jason.mauss@gmail.com>
2023-07-02 15:11:10 -0700

    Adding Solutions for TypeScript and JavaScript

--- CaptureForts/JavaScript/CaptureForts.js
+++ CaptureForts/JavaScript/CaptureForts.js
@@ -1,6 +1,29 @@
 // Solution for: https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
 const captureForts = (forts) => {
-    return 0;
+    let maxForts = 0;
+    const checkForts = (fortArray) => {
+        let maxForts = 0;
+        // move forward in the forts and capture the max sequence of
+        // zero's that exist between a 1 and -1.
+        for (let i = 0; i < forts.length; i++) {
+            if (forts[i] === 1) {
+                let placeholder = i;
+                let fortsFound = 0;
+                i++;
+                while (forts[i] === 0) {
+                    fortsFound++;
+                    i++;
+                }
+                if (forts[i] === -1) {
+                    maxForts = Math.max(maxForts, fortsFound);
+                }
+                // go back to our placeholder
+                i = placeholder;
+            }
+        }
+        return maxForts;
+    };
+    return Math.max(...[checkForts(forts), checkForts(forts.reverse())]);
 };
 // some test cases
 console.log(captureForts([1, 0, 0, -1, 0, 0, 0, 0, 1])); // 4
--- CaptureForts/TypeScript/CaptureForts.ts
+++ CaptureForts/TypeScript/CaptureForts.ts
@@ -1,8 +1,36 @@
 // Solution for: https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
 const captureForts = (forts: number[]): number => {
 
-    return 0;
+    let maxForts = 0;
 
+    const checkForts = (fortArray: number[]): number => {
+        
+        let maxForts = 0;
+
+        // move forward in the forts and capture the max sequence of
+        // zero's that exist between a 1 and -1.
+        for(let i = 0; i < forts.length; i++) {
+            if(forts[i] === 1) {
+                let placeholder = i;
+                let fortsFound = 0;
+                i++;
+                while(forts[i] === 0) {
+                    fortsFound++;
+                    i++;
+                }
+                if(forts[i] === -1) {
+                    maxForts = Math.max(maxForts, fortsFound);
+                }
+                // go back to our placeholder
+                i = placeholder;
+            }
+        }
+
+        return maxForts;
+    };
+
+    return Math.max(...[checkForts(forts), checkForts(forts.reverse())])
+    
 };
 
 
