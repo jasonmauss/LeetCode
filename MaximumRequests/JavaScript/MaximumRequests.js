6a93281ed5b32146093b4c1176e46c2e5055620d
Jason Mauss <jason.mauss@gmail.com>
2023-07-02 18:36:01 -0700

    initial commit

---
+++ MaximumRequests/JavaScript/MaximumRequests.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+const maximumRequests = (n, requests) => {
+    return 0;
+};
+// some test cases
+console.log(maximumRequests(5, [[0, 1], [1, 0], [0, 1], [1, 2], [2, 0], [3, 4]])); // 5
+console.log(maximumRequests(3, [[0, 0], [1, 2], [2, 1]])); // 3
+console.log(maximumRequests(4, [[0, 3], [3, 1], [1, 2], [2, 0]])); // 4
