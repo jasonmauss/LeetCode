1c2182db9263e60aaea50de82c03923f31a46d63
Jason Mauss <jason.mauss@gmail.com>
2023-07-04 12:56:27 -0700

    initial commit

---
+++ JumpGameII/JavaScript/JumpGameII.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/jump-game-ii/description/
+const jump = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(jump([2, 3, 1, 1, 4])); // 2
+console.log(jump([2, 3, 0, 1, 4])); // 2
