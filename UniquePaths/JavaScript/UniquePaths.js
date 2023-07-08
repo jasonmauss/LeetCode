67d75efd1d85af1f2badf377ebf75b4454bf5dc6
Jason Mauss <jason.mauss@gmail.com>
2023-07-07 22:13:04 -0700

    initial commit

---
+++ UniquePaths/JavaScript/UniquePaths.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/unique-paths/
+const uniquePaths = (m, n) => {
+    return 0;
+};
+// some test cases
+console.log(uniquePaths(3, 7)); // 28
+console.log(uniquePaths(3, 2)); // 3
---
+++ UniquePaths/TypeScript/UniquePaths.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/unique-paths/
+const uniquePaths = (m: number, n: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(uniquePaths(3, 7)); // 28
+console.log(uniquePaths(3, 2)); // 3
