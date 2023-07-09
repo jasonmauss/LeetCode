4ea2e95cd8eb9d65dd47e1645f65e537b255c2a2
Jason Mauss <jason.mauss@gmail.com>
2023-07-08 22:51:41 -0700

    initial commit

---
+++ ShortestPathAllKeys/JavaScript/ShortestPathAllKeys.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/shortest-path-to-get-all-keys/
+const shortestPathAllKeys = (grid) => {
+    return 0;
+};
+// some test cases
+console.log(shortestPathAllKeys(["@.a..", "###.#", "b.A.B"])); // 8
+console.log(shortestPathAllKeys(["@..aA", "..B#.", "....b"])); // 6
+console.log(shortestPathAllKeys(["@Aa"])); // -1
---
+++ ShortestPathAllKeys/TypeScript/ShortestPathAllKeys.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/shortest-path-to-get-all-keys/
+const shortestPathAllKeys = (grid: string[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(shortestPathAllKeys(["@.a..","###.#","b.A.B"])); // 8
+console.log(shortestPathAllKeys(["@..aA","..B#.","....b"])); // 6
+console.log(shortestPathAllKeys(["@Aa"])); // -1
