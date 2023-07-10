648462403b17ff149c8754ecc1398d6001458809
Jason Mauss <jason.mauss@gmail.com>
2023-07-09 22:37:11 -0700

    initial commit

---
+++ NumberIsFascinating/JavaScript/NumberIsFascinating.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+const isFascinating = (n) => {
+    return false;
+};
+// some test cases
+console.log(isFascinating(192)); // true
+console.log(isFascinating(100)); // false
