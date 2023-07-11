89c47e94822f58a43daabcec507e822d356f153b
Jason Mauss <jason.mauss@gmail.com>
2023-07-10 17:39:47 -0700

    initial commit

---
+++ CanFinish/JavaScript/CanFinish.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/course-schedule/
+const canFinish = (numCourses, prerequisites) => {
+    return false;
+};
+// some test cases
+console.log(canFinish(2, [[1, 0]])); // true
+console.log(canFinish(2, [[1, 0], [0, 1]])); // false
