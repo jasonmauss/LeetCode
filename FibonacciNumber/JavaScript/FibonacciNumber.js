bd79b15b027c5a6b34b77711c7eea46e04bc3349
Jason Mauss <jason.mauss@gmail.com>
2023-07-16 21:55:09 -0700

    Adding Solutions for TypeScript and JavaScipt

--- FibonacciNumber/JavaScript/FibonacciNumber.js
+++ FibonacciNumber/JavaScript/FibonacciNumber.js
@@ -1,8 +1,15 @@
 // Solution for: https://leetcode.com/problems/fibonacci-number/
-function fib(n) {
-    return 0;
-}
-;
+const fib = (n) => {
+    if (n < 2)
+        return n;
+    let fibNumber1 = 0, fibNumber2 = 1, tempFibNumber = 0;
+    for (let i = 2; i <= n; i++) {
+        tempFibNumber = fibNumber1;
+        fibNumber1 = fibNumber2;
+        fibNumber2 = tempFibNumber + fibNumber1;
+    }
+    return fibNumber2;
+};
 // some test cases
 console.log(fib(2)); // 1
 console.log(fib(3)); // 2
