323d5bc57efae36e62ea16f286b4db4f484ae4d6
Jason Mauss <jason.mauss@gmail.com>
2023-07-17 22:06:19 -0700

    initial commit

---
+++ NumberOfGoodSubarraySplits/JavaScript/NumberOfGoodSubarraySplits.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/ways-to-split-array-into-good-subarrays/
+const numberOfGoodSubarraySplits = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(numberOfGoodSubarraySplits([0, 1, 0, 0, 1])); // 3
+console.log(numberOfGoodSubarraySplits([0, 1, 0])); // 1
---
+++ NumberOfGoodSubarraySplits/TypeScript/NumberOfGoodSubarraySplits.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/ways-to-split-array-into-good-subarrays/
+const numberOfGoodSubarraySplits = (nums: number[]): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(numberOfGoodSubarraySplits([0,1,0,0,1])); // 3
+console.log(numberOfGoodSubarraySplits([0,1,0])); // 1
