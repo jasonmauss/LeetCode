75e1b80e3f94502853d9b30c1817d3403d0ca220
Jason Mauss <jason.mauss@gmail.com>
2023-07-19 20:56:07 -0700

    initial commit

---
+++ FindKthLargestElement/JavaScript/FindKthLargestElement.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/kth-largest-element-in-an-array/
+const findKthLargest = (nums, k) => {
+    return 0;
+};
+// some test cases
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2)); // 5
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)); // 4
---
+++ FindKthLargestElement/TypeScript/FindKthLargestElement.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/kth-largest-element-in-an-array/
+const findKthLargest = (nums: number[], k: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(findKthLargest([3,2,1,5,6,4], 2)); // 5
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4)); // 4
