9fa2b21c57cf971f363e4c4c3917b0f7e7e70667
Jason Mauss <jason.mauss@gmail.com>
2023-07-24 22:10:03 -0700

    initial commit

---
+++ MaxNonDecreasingLength/JavaScript/MaxNonDecreasingLength.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/longest-non-decreasing-subarray-from-two-arrays/
+const maxNonDecreasingLength = (nums1, nums2) => {
+    return 0;
+};
+// some test cases
+console.log(maxNonDecreasingLength([2, 3, 1], [1, 2, 1])); // 2
+console.log(maxNonDecreasingLength([1, 3, 2, 1], [2, 2, 3, 4])); // 4
+console.log(maxNonDecreasingLength([1, 1], [2, 2])); // 2
