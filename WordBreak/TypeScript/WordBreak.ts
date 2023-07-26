b30f4d544651559d4918936e715393cb6368268d
Jason Mauss <jason.mauss@gmail.com>
2023-07-26 00:02:32 -0700

    initial commit

---
+++ WordBreak/TypeScript/WordBreak.ts
@@ -0,0 +1,16 @@
+// Solution for: https://leetcode.com/problems/word-break/
+const wordBreak = (s: string, wordDict: string[]): boolean => {
+
+    return false;
+
+};
+
+
+
+
+
+
+// some test cases
+console.log(wordBreak("leetcode", ["leet","code"])); // true
+console.log(wordBreak("applepenapple", ["apple","pen"])); // true
+console.log(wordBreak("catsandog", ["cats","dog","sand","and","cat"])); // false
