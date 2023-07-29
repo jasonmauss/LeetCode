c0e7dd8a648bf8130bcb6eaa41cd252276ec20ff
Jason Mauss <jason.mauss@gmail.com>
2023-07-29 12:31:56 -0700

    Adding Solutions for TypeScript and JavaScript

--- CheckArrayIsGood/JavaScript/CheckArrayIsGood.js
+++ CheckArrayIsGood/JavaScript/CheckArrayIsGood.js
@@ -1,6 +1,14 @@
 // Solution for: https://leetcode.com/problems/check-if-array-is-good/
 const isGood = (nums) => {
-    return false;
+    nums.sort((a, b) => a - b);
+    const length = nums.length;
+    for (let i = 0; i < length - 1; i++) {
+        if (nums[i] !== i + 1)
+            return false;
+    }
+    if (nums[length - 1] !== nums[length - 2])
+        return false;
+    return true;
 };
 // some test cases
 console.log(isGood([2, 1, 3])); // false
