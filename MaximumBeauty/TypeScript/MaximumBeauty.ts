70b4a0cab27293a01caafb95076d6922bf32fc82
Jason Mauss <jason.mauss@gmail.com>
2023-07-29 13:02:52 -0700

    initial commit

---
+++ MaximumBeauty/TypeScript/MaximumBeauty.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation
+function maximumBeauty(nums: number[], k: number): number {
+
+    return 0;
+
+};
+
+
+
+
+
+// some test cases
+console.log(maximumBeauty([4,6,1,2], 2)); // 3
+console.log(maximumBeauty([1,1,1,1], 10)); // 4
