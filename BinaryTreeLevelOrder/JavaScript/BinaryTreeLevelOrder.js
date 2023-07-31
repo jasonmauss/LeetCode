931cb9b50f42f97eac3325ccc42b5bc6434de61b
Jason Mauss <jason.mauss@gmail.com>
2023-07-30 20:46:36 -0700

    Adding Solutions for TypeScript and JavaScript

--- BinaryTreeLevelOrder/JavaScript/BinaryTreeLevelOrder.js
+++ BinaryTreeLevelOrder/JavaScript/BinaryTreeLevelOrder.js
@@ -13,12 +13,25 @@ class TreeNode {
     }
 }
 const levelOrder = (root) => {
-    return [[]];
+    const returnValues = [];
+    const getNodeValuesAtLevel = (node, level) => {
+        if (!node)
+            return;
+        if (!returnValues[level])
+            returnValues.push([]);
+        returnValues[level].push(node.val);
+        if (node.left)
+            getNodeValuesAtLevel(node.left, level + 1);
+        if (node.right)
+            getNodeValuesAtLevel(node.right, level + 1);
+    };
+    getNodeValuesAtLevel(root, 0);
+    return returnValues;
 };
 // some test cases
 let rootNode1 = new TreeNode(3, new TreeNode(9, null, null), new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null)));
 console.log(levelOrder(rootNode1)); // [[3],[9,20],[15,7]]
 let rootNode2 = new TreeNode(1, null, null);
 console.log(levelOrder(rootNode2)); // [[1]]
-let rootNode3 = new TreeNode();
+let rootNode3 = null;
 console.log(levelOrder(rootNode3)); // []
--- BinaryTreeLevelOrder/TypeScript/BinaryTreeLevelOrder.ts
+++ BinaryTreeLevelOrder/TypeScript/BinaryTreeLevelOrder.ts
@@ -17,7 +17,24 @@ class TreeNode {
 
 const levelOrder = (root: TreeNode | null): number[][] => {
 
-    return [[]];
+    const returnValues:number[][] = [];
+
+    const getNodeValuesAtLevel = (node:TreeNode | null, level:number) : number[] => {
+
+        if(!node) return;
+
+        if(!returnValues[level]) returnValues.push([]);
+
+        returnValues[level].push(node.val);
+
+
+        if(node.left) getNodeValuesAtLevel(node.left, level + 1);
+        if(node.right) getNodeValuesAtLevel(node.right, level + 1);
+    }
+
+    getNodeValuesAtLevel(root, 0);
+
+    return returnValues;
 
 };
 
@@ -36,5 +53,5 @@ console.log(levelOrder(rootNode1)); // [[3],[9,20],[15,7]]
 let rootNode2:TreeNode = new TreeNode(1, null, null);
 console.log(levelOrder(rootNode2)); // [[1]]
 
-let rootNode3:TreeNode = new TreeNode();
+let rootNode3:TreeNode = null;
 console.log(levelOrder(rootNode3)); // []
