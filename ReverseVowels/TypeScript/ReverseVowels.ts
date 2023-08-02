815be31b1f35394b09d7c10e3f1d15f1c2f0e29a
Jason Mauss <jason.mauss@gmail.com>
2023-08-01 23:13:30 -0700

    Adding Solutions for TypeScript and JavaScript

--- ReverseVowels/TypeScript/ReverseVowels.ts
+++ ReverseVowels/TypeScript/ReverseVowels.ts
@@ -1,8 +1,26 @@
 // Solution for: https://leetcode.com/problems/reverse-vowels-of-a-string/
 const reverseVowels = (s: string): string => {
 
-    return '';
+    let vowelChars:string[] = [];
+    let nonVowelChars:string[] = [];
+    let vowels = ['a','e','i','o','u'];
 
+    for(let i = 0; i < s.length; i++) {
+        if(vowels.includes(s[i].toLowerCase())) {
+            vowelChars.push(s[i]);
+            nonVowelChars.push('*');
+        } else {
+            nonVowelChars.push(s[i]);
+        }
+    }
+
+    for(let i = 0; i < nonVowelChars.length; i++) {
+        if(nonVowelChars[i] === '*') {
+            nonVowelChars[i] = vowelChars.pop();
+        }
+    }
+
+    return nonVowelChars.join('');
 };
 
 
