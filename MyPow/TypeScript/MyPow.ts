e392e338b6795c8d536f045e2d680c05f9a37fe0
Jason Mauss <jason.mauss@gmail.com>
2023-08-03 00:02:03 -0700

    initial commit

---
+++ MyPow/TypeScript/MyPow.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/powx-n/
+const myPow = (x: number, n: number): number => {
+
+    return 0;
+
+};
+
+
+// some test cases
+console.log(myPow(2.00000, 10)); // 1024.00000
+console.log(myPow(2.10000, 3)); // 9.26100
+console.log(myPow(2.00000, -2)); // 0.25000
