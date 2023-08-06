7e0447bae5d8547a94ab3556a9a210770b67ce31
Jason Mauss <jason.mauss@gmail.com>
2023-08-05 23:56:50 -0700

    Adding Solutions for TypeScript and JavaScript

--- ValidAnagram/JavaScript/ValidAnagram.js
+++ ValidAnagram/JavaScript/ValidAnagram.js
@@ -1,6 +1,11 @@
 // Solution for: https://leetcode.com/problems/valid-anagram/
 const isAnagram = (s, t) => {
-    return false;
+    if (s.length !== t.length)
+        return false;
+    for (let char of s) {
+        t = t.replace(char, '');
+    }
+    return t.trim().length === 0;
 };
 // some test cases
 console.log(isAnagram("anagram", "nagaram")); // true
