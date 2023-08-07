3f7d43f54019ec40637628b32aca9c5afbc8b0fc
Jason Mauss <jason.mauss@gmail.com>
2023-08-06 23:43:37 -0700

    initial commit

---
+++ IsPalindrome/JavaScript/IsPalindrome.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/valid-palindrome/
+const isPalindrome = (s) => {
+    return false;
+};
+// some test cases
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
+console.log(isPalindrome(" ")); // true
