4d46411df9b0378b8c516d00ffb405c5d3c29860
Jason Mauss <jason.mauss@gmail.com>
2023-08-07 21:30:04 -0700

    initial commit

---
+++ AsteroidCollision/JavaScript/AsteroidCollision.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/asteroid-collision/
+const asteroidCollision = (asteroids) => {
+    return [0];
+};
+// some test cases
+console.log(asteroidCollision([5, 10, -5])); // [5,10]
+console.log(asteroidCollision([8, -8])); // []
+console.log(asteroidCollision([10, 2, -5])); // [10]
---
+++ AsteroidCollision/TypeScript/AsteroidCollision.ts
@@ -0,0 +1,15 @@
+// Solution for: https://leetcode.com/problems/asteroid-collision/
+const asteroidCollision = (asteroids: number[]): number[] => {
+
+    return [0];
+
+};
+
+
+
+
+
+// some test cases
+console.log(asteroidCollision([5,10,-5])); // [5,10]
+console.log(asteroidCollision([8, -8])); // []
+console.log(asteroidCollision([10,2,-5])); // [10]
