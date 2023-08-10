0c8b8feb669ea46494e0cbd0ee6e45b0a907f492
Jason Mauss <jason.mauss@gmail.com>
2023-08-09 23:10:58 -0700

    Adding Solutions for TypeScript and JavaScript

--- ClimbingStairs/JavaScript/ClimbingStairs.js
+++ ClimbingStairs/JavaScript/ClimbingStairs.js
@@ -1,7 +1,15 @@
 // Solution for: https://leetcode.com/problems/climbing-stairs/
 const climbStairs = (n) => {
-    return 0;
+    // seed with 5 values
+    const stairClimbingSequence = [1, 2, 3, 5, 8, 13];
+    while (stairClimbingSequence.length < n) {
+        stairClimbingSequence.push(stairClimbingSequence[stairClimbingSequence.length - 1] +
+            stairClimbingSequence[stairClimbingSequence.length - 2]);
+    }
+    return stairClimbingSequence[n - 1];
 };
 // some test cases
 console.log(climbStairs(2)); // 2
 console.log(climbStairs(3)); // 3
+console.log(climbStairs(5)); // 8
+console.log(climbStairs(7)); // 21
