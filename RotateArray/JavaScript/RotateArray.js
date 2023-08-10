5205d15185c58234323a2e879ceab3bad53f4ed1
Jason Mauss <jason.mauss@gmail.com>
2023-08-09 23:16:07 -0700

    initial commit

---
+++ RotateArray/JavaScript/RotateArray.js
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/rotate-array/
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+const rotate = (nums, k) => {
+};
+// some test cases
+const nums = [1, 2, 3, 4, 5, 6, 7];
+rotate(nums, 3);
+console.log(nums); // [5,6,7,1,2,3,4]
+const nums2 = [-1, -100, 3, 99];
+rotate(nums2, 2);
+console.log(nums2); // [3,99,-1,-100]
---
+++ RotateArray/TypeScript/RotateArray.ts
@@ -0,0 +1,21 @@
+// Solution for: https://leetcode.com/problems/rotate-array/
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+const rotate = (nums: number[], k: number): void => {
+
+
+};
+
+
+
+// some test cases
+const nums = [1,2,3,4,5,6,7];
+rotate(nums, 3);
+console.log(nums); // [5,6,7,1,2,3,4]
+
+const nums2 = [-1,-100,3,99];
+rotate(nums2, 2);
+console.log(nums2); // [3,99,-1,-100]
+
+
