3e22017e22fc079a54c971db4561e79e8a51a370
Jason Mauss <jason.mauss@gmail.com>
2023-08-12 17:20:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- SearchRotatedSortedArray/JavaScript/SearchRotatedSortedArray.js
+++ SearchRotatedSortedArray/JavaScript/SearchRotatedSortedArray.js
@@ -1,6 +1,6 @@
 // Solution for: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 const search = (nums, target) => {
-    return false;
+    return nums.filter((x) => x === target).length > 0;
 };
 // some test cases
 console.log(search([2, 5, 6, 0, 0, 1, 2], 0)); // true
