693ad34dfa2018b60f3cf44d04b9e06060493560
Jason Mauss <jason.mauss@gmail.com>
2023-08-16 23:28:12 -0700

    Adding Solution for TypeScript and JavaScript

--- LinkedListCycle/TypeScript/LinkedListCycle.ts
+++ LinkedListCycle/TypeScript/LinkedListCycle.ts
@@ -15,6 +15,19 @@ class ListNode {
 
 function hasCycle(head: ListNode | null): boolean {
     
+    if (head === null || head.next === null) return false;
+
+    let slow: ListNode | null = head;
+    let fast: ListNode | null = head;
+
+    while (fast && fast.next) {
+
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) return true;
+    }
+
     return false;
 
 };
@@ -26,11 +39,13 @@ let node3:ListNode = new ListNode(0);
 let node2:ListNode = new ListNode(2, node3);
 let node4:ListNode = new ListNode(-4, node2);
 let rootNode1:ListNode = new ListNode(3, node2);
+node3.next = node4;
 
 console.log(hasCycle(rootNode1)); // true
 
 node2 = new ListNode(2)
-let node1 = new ListNode(1, node2);
+let node1 = new ListNode(1);
+node1.next = node2;
 node2.next = node1;
 
 console.log(hasCycle(node1)); // true
