5436778fb730311c7388ba90e9b3d7ad6665333e
Jason Mauss <jason.mauss@gmail.com>
2023-08-18 23:42:23 -0700

    initial commit

---
+++ MinSpeedOnTime/JavaScript/MinSpeedOnTime.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+const minSpeedOnTime = (dist, hour) => {
+    return 0;
+};
+// some test cases
+console.log(minSpeedOnTime([1, 3, 2], 6)); // 1
+console.log(minSpeedOnTime([1, 3, 2], 2.7)); // 3
+console.log(minSpeedOnTime([1, 3, 2], 1.9)); // -1
---
+++ MinSpeedOnTime/TypeScript/MinSpeedOnTime.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+const minSpeedOnTime = (dist: number[], hour: number): number => {
+
+    return 0;
+
+};
+
+
+
+
+// some test cases
+console.log(minSpeedOnTime([1,3,2], 6)); // 1
+console.log(minSpeedOnTime([1,3,2], 2.7)); // 3
+console.log(minSpeedOnTime([1,3,2], 1.9)); // -1
