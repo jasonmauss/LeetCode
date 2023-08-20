3e24c5c4acb396f8a93667f884b23b0683223be1
Jason Mauss <jason.mauss@gmail.com>
2023-08-19 22:26:45 -0700

    initial commit

---
+++ MoveZeroes/JavaScript/MoveZeroes.js
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/move-zeroes/
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = (nums) => {
+};
+// some test cases
+const nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+console.log(nums); // [1,3,12,0,0]
+const nums1 = [0];
+moveZeroes(nums1);
+console.log(nums1); // [0]
