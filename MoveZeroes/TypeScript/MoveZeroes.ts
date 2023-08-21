55cd74e711be6dfd37315deebbf6c249dec28805
Jason Mauss <jason.mauss@gmail.com>
2023-08-20 19:38:47 -0700

    Adding Solutions for TypeScript and JavaScript

--- MoveZeroes/TypeScript/MoveZeroes.ts
+++ MoveZeroes/TypeScript/MoveZeroes.ts
@@ -4,7 +4,22 @@
  */
  const moveZeroes = (nums: number[]): void => {
 
+    // There's no work to be done if there's only one element
+    if(nums.length === 1) return;
 
+    // this will allow us to know when we can stop iterating through the array
+    let zeroesFound = 0;
+
+    // if we encounter a zero, remove it with splice, and append it to the end
+    // of the array using push. Also decrement the iterator by 1 so that we don't
+    // end up skipping the checking of one of the array elements after removing a zero.
+    for(let i = 0; i < nums.length - zeroesFound; i++) {
+        if(nums[i] === 0) {
+            nums.push(...nums.splice(i, 1));
+            zeroesFound++;
+            i--;
+        }
+    }
 
  };
 
@@ -19,3 +34,7 @@
  const nums1 = [0];
  moveZeroes(nums1);
  console.log(nums1); // [0]
+
+ const nums2 = [0,0,1];
+ moveZeroes(nums2);
+ console.log(nums2); // [1,0,0]
