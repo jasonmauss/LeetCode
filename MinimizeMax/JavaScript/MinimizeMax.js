a48945956f04060d192c0bb1eafde74d68e7927a
Jason Mauss <jason.mauss@gmail.com>
2023-08-22 21:37:14 -0700

    initial commit

---
+++ MinimizeMax/JavaScript/MinimizeMax.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/
+const minimizeMax = (nums, p) => {
+    return 0;
+};
+// some test cases
+console.log(minimizeMax([10, 1, 2, 7, 1, 3], 2)); // 1
+console.log(minimizeMax([4, 2, 1, 2], 1)); // 0
---
+++ MinimizeMax/TypeScript/MinimizeMax.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/
+const minimizeMax = (nums: number[], p: number): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(minimizeMax([10,1,2,7,1,3], 2)); // 1
+console.log(minimizeMax([4,2,1,2], 1)); // 0
