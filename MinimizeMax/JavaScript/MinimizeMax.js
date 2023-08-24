a8e7909fd7b5086f91de3919f8434d54ea42c4e8
Jason Mauss <jason.mauss@gmail.com>
2023-08-23 19:49:57 -0700

    Adding Solutions for TypeScript and JavaScript

--- MinimizeMax/JavaScript/MinimizeMax.js
+++ MinimizeMax/JavaScript/MinimizeMax.js
@@ -1,6 +1,35 @@
 // Solution for: https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/
 const minimizeMax = (nums, p) => {
-    return 0;
+    const numsLength = nums.length;
+    if (numsLength < 2)
+        return 0;
+    nums.sort((a, b) => a - b);
+    const getPairsCount = (pairLimit) => {
+        let i = 0;
+        let count = 0;
+        while (i < numsLength - 1) {
+            if (nums[i + 1] - nums[i] <= pairLimit) {
+                count++;
+                i += 2;
+            }
+            else {
+                i++;
+            }
+        }
+        return count;
+    };
+    let left = 0;
+    let right = nums[numsLength - 1] - nums[0];
+    while (left < right) {
+        const middle = (left + right) >>> 1;
+        if (getPairsCount(middle) >= p) {
+            right = middle;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    return left;
 };
 // some test cases
 console.log(minimizeMax([10, 1, 2, 7, 1, 3], 2)); // 1
