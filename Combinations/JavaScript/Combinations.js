8a4f403ad729b4169252c7ab071b9e36b9c259d1
Jason Mauss <jason.mauss@gmail.com>
2023-08-26 01:54:45 -0700

    initial commit

---
+++ Combinations/JavaScript/Combinations.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/combinations
+const combine = (n, k) => {
+    return [];
+};
+// some test cases
+console.log(combine(4, 2)); // [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+console.log(combine(1, 1)); // [[1]]
---
+++ Combinations/TypeScript/Combinations.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/combinations
+const combine = (n: number, k: number): number[][] => {
+
+    return [];
+
+};
+
+
+
+// some test cases
+console.log(combine(4, 2)); // [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+console.log(combine(1, 1)); // [[1]]
