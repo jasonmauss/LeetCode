976a512279d450b7e893c4723167b74e97ae5c65
Jason Mauss <jason.mauss@gmail.com>
2023-08-25 22:23:35 -0700

    initial commit

---
+++ ReverseLinkedList/TypeScript/ReverseLinkedList.ts
@@ -0,0 +1,42 @@
+// Solution for: https://leetcode.com/problems/reverse-linked-list/
+/**
+ * Definition for singly-linked list.
+ */
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+
+    return null;
+
+};
+
+
+
+const rootNode:ListNode = new ListNode(1,
+    new ListNode(2,
+        new ListNode(3,
+            new ListNode(4,
+                new ListNode(5, null)
+            )
+        )
+    )
+);
+
+// some test cases
+console.log(reverseList(rootNode)); // [5,4,3,2,1]
+
+
+
+const rootNode2:ListNode = new ListNode(1,
+    new ListNode(2, null)
+);
+
+console.log(reverseList(rootNode2)); // [2, 1]
