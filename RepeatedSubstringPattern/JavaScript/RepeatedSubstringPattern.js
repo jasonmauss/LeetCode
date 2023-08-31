42a11ce70eabcb6df14fdf0d28467d32f0324b03
Jason Mauss <jason.mauss@gmail.com>
2023-08-30 23:50:42 -0700

    initial commit

---
+++ RepeatedSubstringPattern/JavaScript/RepeatedSubstringPattern.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/repeated-substring-pattern/
+const repeatedSubstringPattern = (s) => {
+    return false;
+};
+// some test cases
+console.log(repeatedSubstringPattern("abab")); // true
+console.log(repeatedSubstringPattern("aba")); // false
+console.log(repeatedSubstringPattern("abcabcabcabc")); // true
