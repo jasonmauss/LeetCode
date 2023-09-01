c818ff42564a1fbc019ff843f1e8606939341af0
Jason Mauss <jason.mauss@gmail.com>
2023-08-31 23:56:11 -0700

    Adding Solutions for TypeScript and JavaScript

--- InterleavingString/JavaScript/InterleavingString.js
+++ InterleavingString/JavaScript/InterleavingString.js
@@ -1,6 +1,21 @@
 // Solution for: https://leetcode.com/problems/interleaving-string/
 const isInterleave = (s1, s2, s3) => {
-    return false;
+    if (s1.length + s2.length !== s3.length)
+        return false;
+    const dp = new Array(s2.length + 1).fill(false);
+    for (let i = 0; i <= s1.length; i++) {
+        for (let j = 0; j <= s2.length; j++) {
+            if (i === 0 && j === 0)
+                dp[j] = true;
+            else if (i === 0)
+                dp[j] = dp[j - 1] && s2[j - 1] === s3[i + j - 1];
+            else if (j === 0)
+                dp[j] = dp[j] && s1[i - 1] === s3[i + j - 1];
+            else
+                dp[j] = (dp[j] && s1[i - 1] === s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] === s3[i + j - 1]);
+        }
+    }
+    return dp[s2.length];
 };
 // some test cases
 console.log(isInterleave("aabcc", "dbbca", "aadbbcbcac")); // true
--- InterleavingString/TypeScript/InterleavingString.ts
+++ InterleavingString/TypeScript/InterleavingString.ts
@@ -1,7 +1,20 @@
 // Solution for: https://leetcode.com/problems/interleaving-string/
 const isInterleave = (s1: string, s2: string, s3: string): boolean => {
 
-    return false;
+    if(s1.length + s2.length !== s3.length) return false;
+
+    const dp = new Array(s2.length + 1).fill(false);
+
+    for (let i = 0; i <= s1.length; i++) {
+        for (let j = 0; j <= s2.length; j++) {
+            if (i === 0 && j === 0) dp[j] = true;
+            else if (i === 0) dp[j] = dp[j - 1] && s2[j - 1] === s3[i + j - 1];
+            else if (j === 0) dp[j] = dp[j] && s1[i - 1] === s3[i + j - 1];
+            else dp[j] = (dp[j] && s1[i - 1] === s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] === s3[i + j - 1]);
+        }
+    }
+
+    return dp[s2.length];
 
 };
 
