0acad214d0be7f7d213185991a8140ed51f60f50
Jason Mauss <jason.mauss@gmail.com>
2023-09-02 14:02:55 -0700

    initial commit

---
+++ IsSubsequence/JavaScript/IsSubsequence.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/is-subsequence/
+const isSubsequence = (s, t) => {
+    return false;
+};
+// some test cases
+console.log(isSubsequence('abc', 'ahbgdc')); // true
+console.log(isSubsequence('axc', 'ahbgdc')); // false
---
+++ IsSubsequence/TypeScript/IsSubsequence.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/is-subsequence/
+const isSubsequence = (s: string, t: string): boolean => {
+
+    return false;
+
+};
+
+
+
+// some test cases
+console.log(isSubsequence('abc', 'ahbgdc')); // true
+console.log(isSubsequence('axc', 'ahbgdc')); // false
