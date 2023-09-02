b103f181760014a11572deb325db7fa70babe01b
Jason Mauss <jason.mauss@gmail.com>
2023-09-02 13:54:17 -0700

    Adding Solutions for TypeScript and JavaScript

--- AddBinary/JavaScript/AddBinary.js
+++ AddBinary/JavaScript/AddBinary.js
@@ -1,6 +1,6 @@
 // Solution for: https://leetcode.com/problems/add-binary/
 const addBinary = (a, b) => {
-    return '';
+    return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(2);
 };
 // some test cases
 console.log(addBinary("11", "1")); // "100"
