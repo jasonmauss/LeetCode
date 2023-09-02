62b6fdc83b545a08355c133a33ac8bde576cc244
Jason Mauss <jason.mauss@gmail.com>
2023-09-01 22:42:19 -0700

    initial commit

---
+++ AddBinary/JavaScript/AddBinary.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/add-binary/
+const addBinary = (a, b) => {
+    return '';
+};
+// some test cases
+console.log(addBinary("11", "1")); // "100"
+console.log(addBinary("1010", "1011")); // "10101"
