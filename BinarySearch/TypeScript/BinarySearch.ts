e975c22995ea8bc6cd66ec5c8925061b22b1d3c8
Jason Mauss <jason.mauss@gmail.com>
2023-09-03 00:08:12 -0700

    initial commit

---
+++ BinarySearch/TypeScript/BinarySearch.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/binary-search/
+const search = (nums: number[], target: number): number => {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(search([-1,0,3,5,9,12], 9)); // 4
+console.log(search([-1,0,3,5,9,12], 2)); // -1
