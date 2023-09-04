c9fb672025c753de3abbadfcd55e18d15a188496
Jason Mauss <jason.mauss@gmail.com>
2023-09-03 23:27:27 -0700

    initial commit

---
+++ MissingNumber/JavaScript/MissingNumber.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/missing-number/
+const missingNumber = (nums) => {
+    return 0;
+};
+// some test cases
+console.log(missingNumber([3, 0, 1])); // 2
+console.log(missingNumber([0, 1])); // 2
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
