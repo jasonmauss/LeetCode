c4d4b55703fc697200a6bd4d8313008fa5f0665d
Jason Mauss <jason.mauss@gmail.com>
2023-09-03 23:17:14 -0700

    Adding Solutions for TypeScript and JavaScript

--- BinarySearch/JavaScript/BinarySearch.js
+++ BinarySearch/JavaScript/BinarySearch.js
@@ -1,6 +1,19 @@
 // Solution for: https://leetcode.com/problems/binary-search/
 const search = (nums, target) => {
-    return 0;
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        const middle = left + Math.floor((right - left) / 2);
+        if (nums[middle] === target) {
+            return middle;
+        }
+        else if (nums[middle] < target) {
+            left = middle + 1;
+        }
+        else if (nums[middle] > target) {
+            right = middle - 1;
+        }
+    }
+    return -1;
 };
 // some test cases
 console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
