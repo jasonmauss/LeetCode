ee11e7131a75bef06c1272894d8cf52401544b18
Jason Mauss <jason.mauss@gmail.com>
2023-09-06 23:42:35 -0700

    initial commit

---
+++ IsAcronym/TypeScript/IsAcronym.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/
+const isAcronym = (words: string[], s: string): boolean => {
+
+    return false;
+
+};
+
+
+
+// some test cases
+console.log(isAcronym(["alice","bob","charlie"], "abc")); // true
+console.log(isAcronym(["an","apple"], "a")); // false
+console.log(isAcronym(["never","gonna","give","up","on","you"], "ngguoy")); // true
