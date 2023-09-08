35acbefe709e6833c70c4a98484aeb58b8b8f7a4
Jason Mauss <jason.mauss@gmail.com>
2023-09-07 19:33:44 -0700

    initial commit

---
+++ FurthestFromOrigin/JavaScript/FurthestFromOrigin.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/furthest-point-from-origin/
+const furthestDistanceFromOrigin = (moves) => {
+    return 0;
+};
+// some test cases
+console.log(furthestDistanceFromOrigin("L_RL__R")); // 3
+console.log(furthestDistanceFromOrigin("_R__LL_")); // 5
+console.log(furthestDistanceFromOrigin("_______")); // 7
