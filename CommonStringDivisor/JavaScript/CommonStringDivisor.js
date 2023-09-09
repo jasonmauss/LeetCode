ceda985ef4607b56fcd867360b4cda445a123d77
Jason Mauss <jason.mauss@gmail.com>
2023-09-08 22:59:13 -0700

    initial commit

---
+++ CommonStringDivisor/JavaScript/CommonStringDivisor.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/greatest-common-divisor-of-strings/
+const gcdOfStrings = (str1, str2) => {
+    return '';
+};
+// some test cases
+console.log(gcdOfStrings("ABCABC", "ABC")); // "ABC"
+console.log(gcdOfStrings("ABABAB", "ABAB")); // "AB"
+console.log(gcdOfStrings("LEET", "CODE")); // ""
---
+++ CommonStringDivisor/TypeScript/CommonStringDivisor.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/greatest-common-divisor-of-strings/
+const gcdOfStrings = (str1: string, str2: string): string => {
+
+    return '';
+
+};
+
+
+
+
+// some test cases
+console.log(gcdOfStrings("ABCABC", "ABC")); // "ABC"
+console.log(gcdOfStrings("ABABAB", "ABAB")); // "AB"
+console.log(gcdOfStrings("LEET", "CODE")); // ""
