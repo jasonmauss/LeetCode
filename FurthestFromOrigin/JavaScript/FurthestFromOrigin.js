0addd4c468fbd381bc1fa62ae2b113fdaadf92c2
Jason Mauss <jason.mauss@gmail.com>
2023-09-08 22:16:29 -0700

    Adding Solutions for TypeScript and JavaScript

--- FurthestFromOrigin/JavaScript/FurthestFromOrigin.js
+++ FurthestFromOrigin/JavaScript/FurthestFromOrigin.js
@@ -1,6 +1,19 @@
 // Solution for: https://leetcode.com/problems/furthest-point-from-origin/
 const furthestDistanceFromOrigin = (moves) => {
-    return 0;
+    let left = 0, right = 0, totalMoves = moves.length;
+    for (let i = 0; i < totalMoves; i++) {
+        if (moves[i] === "L") {
+            left++;
+            continue;
+        }
+        if (moves[i] === "R") {
+            right++;
+            continue;
+        }
+    }
+    totalMoves = totalMoves - (right + left);
+    let subtotal = Math.abs(right - left);
+    return totalMoves + subtotal;
 };
 // some test cases
 console.log(furthestDistanceFromOrigin("L_RL__R")); // 3
