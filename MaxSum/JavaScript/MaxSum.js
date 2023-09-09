8734508fecb30146a8de8b8fdd220cb56be53874
Jason Mauss <jason.mauss@gmail.com>
2023-09-08 22:53:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- MaxSum/JavaScript/MaxSum.js
+++ MaxSum/JavaScript/MaxSum.js
@@ -1,6 +1,16 @@
 // Solution for: https://leetcode.com/problems/max-pair-sum-in-an-array/
 const maxSum = (nums) => {
-    return 0;
+    let maxPairSumValue = -1;
+    for (let i = 0; i < nums.length; i++) {
+        const maxDigit = Math.max(...nums[i].toString().split('').map(Number));
+        for (let j = i + 1; j < nums.length; j++) {
+            const maxOtherDigit = Math.max(...nums[j].toString().split('').map(Number));
+            if (maxOtherDigit === maxDigit) {
+                maxPairSumValue = Math.max(maxPairSumValue, (nums[i] + nums[j]));
+            }
+        }
+    }
+    return maxPairSumValue;
 };
 // some test cases
 console.log(maxSum([51, 71, 17, 24, 42])); // 88
--- MaxSum/TypeScript/MaxSum.ts
+++ MaxSum/TypeScript/MaxSum.ts
@@ -1,7 +1,20 @@
 // Solution for: https://leetcode.com/problems/max-pair-sum-in-an-array/
 const maxSum = (nums: number[]): number => {
 
-    return 0;
+    let maxPairSumValue = -1;
+
+    for(let i = 0; i < nums.length; i++) {
+        const maxDigit = Math.max(...nums[i].toString().split('').map(Number));
+        for(let j = i + 1; j < nums.length; j++) {
+            const maxOtherDigit = Math.max(...nums[j].toString().split('').map(Number));
+            if(maxOtherDigit === maxDigit) {
+                maxPairSumValue = Math.max(maxPairSumValue, (nums[i] + nums[j]));
+            }
+        }
+    }
+
+
+    return maxPairSumValue;
 
 };
 
