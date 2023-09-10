89cbb75ca5dd2411188a55d23368fa79e1fd2659
Jason Mauss <jason.mauss@gmail.com>
2023-09-09 23:54:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- CommonStringDivisor/TypeScript/CommonStringDivisor.ts
+++ CommonStringDivisor/TypeScript/CommonStringDivisor.ts
@@ -1,7 +1,29 @@
 // Solution for: https://leetcode.com/problems/greatest-common-divisor-of-strings/
 const gcdOfStrings = (str1: string, str2: string): string => {
 
-    return '';
+    const length = str2.length;
+    let greatestCommonDivisor = '';
+
+    for (let i = 0; i < length; i++) {
+        const gcdFromBeginning = str2.substring(0, (length - (i + 1)));
+        const gcdFromEnd = str2.substring(length - 1 - i);
+
+        if (str1.replaceAll(gcdFromBeginning, '') == '' && 
+            str2.replaceAll(gcdFromBeginning, '') == '') {
+            if (greatestCommonDivisor.length < gcdFromBeginning.length) {
+                greatestCommonDivisor = gcdFromBeginning;
+            }
+        } 
+
+        if (str1.replaceAll(gcdFromEnd, '') === '' && 
+            str2.replaceAll(gcdFromEnd, '') === '') {
+            if (greatestCommonDivisor.length < gcdFromEnd.length) {
+                greatestCommonDivisor = gcdFromEnd;
+            }
+        }
+    }
+
+    return greatestCommonDivisor;
 
 };
 
