fa0f6fe5339b0a33cb89e31b8d983df38cc59425
Jason Mauss <jason.mauss@gmail.com>
2023-09-10 22:49:49 -0700

    initial commit

---
+++ CountPairsLessThanTarget/JavaScript/CountPairsLessThanTarget.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+const countPairs = (nums, target) => {
+    return 0;
+};
+// some test cases
+console.log(countPairs([-1, 1, 2, 3, 1], 2)); // 3
+console.log(countPairs([-6, 2, 5, -2, -7, -1, 3], -2)); // 10
