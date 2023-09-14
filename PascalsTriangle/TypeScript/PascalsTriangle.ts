bdd76f4ad783759019e2cecc580584c8584274ca
Jason Mauss <jason.mauss@gmail.com>
2023-09-13 22:08:56 -0700

    Adding Solutions for TypeScript and JavaScript

--- PascalsTriangle/TypeScript/PascalsTriangle.ts
+++ PascalsTriangle/TypeScript/PascalsTriangle.ts
@@ -1,7 +1,29 @@
 // Solution for: https://leetcode.com/problems/pascals-triangle/
 const generate = (numRows: number): number[][] => {
 
-    return [];
+    const triangleRows = [];
+    let parentRow = undefined;
+
+    for(let i = 0; i < numRows; i++) {
+        const row = [];
+        row.push(1);
+
+        if(parentRow && parentRow.length > 1) {
+            for(let j = 0; j < parentRow.length - 1; j++) {
+                row.push(parentRow[j] + parentRow[j + 1]);
+            }
+        }
+
+        if(i > 0) {
+            row.push(1);
+        }
+
+        triangleRows.push(row);
+        parentRow = row;
+    }
+
+
+    return triangleRows;
 
 };
 
