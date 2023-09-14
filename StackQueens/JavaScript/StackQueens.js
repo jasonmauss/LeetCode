3ddc6e344908e90276c2ae05c2053344bf331112
Jason Mauss <jason.mauss@gmail.com>
2023-09-13 22:13:32 -0700

    initial commit

---
+++ StackQueens/JavaScript/StackQueens.js
@@ -0,0 +1,25 @@
+// Solution for: https://leetcode.com/problems/implement-stack-using-queues/
+class MyStack {
+    constructor() {
+    }
+    push(x) {
+    }
+    pop() {
+        return 0;
+    }
+    top() {
+        return 0;
+    }
+    empty() {
+        return false;
+    }
+}
+// some test cases
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
