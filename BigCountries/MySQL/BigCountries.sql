6dffd2dad9d1f04776a17a4c3d5a1ab8cf8adca3
Jason Mauss <jason.mauss@gmail.com>
2023-09-14 17:55:06 -0700

    initial commit

---
+++ BigCountries/MySQL/BigCountries.sql
@@ -0,0 +1,13 @@
+-- Solution for: https://leetcode.com/problems/big-countries/ on MySQL dbms platform
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS World (name VARCHAR(255), continent VARCHAR(255), area INT, population INT, gdp BIGINT);
+TRUNCATE TABLE World;
+
+INSERT INTO World (name, continent, area, population, gdp) values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000');
+INSERT INTO World (name, continent, area, population, gdp) values ('Albania', 'Europe', '28748', '2831741', '12960000000');
+INSERT INTO World (name, continent, area, population, gdp) values ('Algeria', 'Africa', '2381741', '37100000', '188681000000');
+INSERT INTO World (name, continent, area, population, gdp) values ('Andorra', 'Europe', '468', '78115', '3712000000');
+INSERT INTO World (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000');
+
+-- The actual query / solution
