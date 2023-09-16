0d695f03bfccfbd86cd39f46a909130fa5f1eaa3
Jason Mauss <jason.mauss@gmail.com>
2023-09-15 21:54:51 -0700

    Adding Solutions for MySQL and MS SQL Server

--- BigCountries/MS SQL Server/BigCountries.sql
+++ BigCountries/MS SQL Server/BigCountries.sql
@@ -14,4 +14,9 @@ INSERT INTO World (name, continent, area, population, gdp) values ('Andorra', 'E
 INSERT INTO World (name, continent, area, population, gdp) values ('Angola', 'Africa', 1246700, 20609294, 100990000000);
 
 -- The actual query / solution
-	
+	SELECT	w.name,
+			w.population,
+			w.area
+	  FROM	World w
+	 WHERE	(w.area >= 3000000)
+	   OR	(w.population >= 25000000)
