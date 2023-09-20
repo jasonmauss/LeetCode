93bca7c7951acbe6b387d7be4368bbfa6152ec71
Jason Mauss <jason.mauss@gmail.com>
2023-09-19 23:59:52 -0700

    initial commit

---
+++ ExcelColumnToNumber/JavaScript/ExcelColumnToNumber.js
@@ -0,0 +1,9 @@
+// Solution for: https://leetcode.com/problems/excel-sheet-column-number/
+function titleToNumber(columnTitle) {
+    return 0;
+}
+;
+// some test cases
+console.log(titleToNumber("A")); // 1
+console.log(titleToNumber("AB")); // 28
+console.log(titleToNumber("ZY")); // 701
---
+++ ExcelColumnToNumber/TypeScript/ExcelColumnToNumber.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/excel-sheet-column-number/
+function titleToNumber(columnTitle: string): number {
+
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(titleToNumber("A")); // 1
+console.log(titleToNumber("AB")); // 28
+console.log(titleToNumber("ZY")); // 701
