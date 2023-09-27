1faec5c0aa0444397c5e476c2efb0a5234b02375
Jason Mauss <jason.mauss@gmail.com>
2023-09-26 22:55:42 -0700

    Adding Solutions for MySQL and MS SQL Server

--- NthHighestSalary/MS SQL Server/NthHighestSalary.sql
+++ NthHighestSalary/MS SQL Server/NthHighestSalary.sql
@@ -12,12 +12,26 @@ INSERT INTO Employee (id, salary) VALUES (2, 200)
 INSERT INTO Employee (id, salary) VALUES (3, 300)
 
 -- The actual query / solution
-GO
+GO;
 
 CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
 BEGIN
     RETURN (
-        /* Write your T-SQL query statement below. */
 
-    );
+		SELECT sq.salary FROM 
+		(
+			SELECT	e.salary,
+					ROW_NUMBER() OVER (ORDER BY e.salary DESC) AS number
+			  FROM	(SELECT DISTINCT salary FROM Employee) e
+		) AS sq
+		WHERE sq.number = @N
+
+    )
 END
+
+USE [leetcodedb]
+GO
+
+SELECT [dbo].[getNthHighestSalary] (1)
+GO
+
