7c00fde8ffb461ea745c1c737758419696bc78f2
Jason Mauss <jason.mauss@gmail.com>
2023-09-30 00:28:50 -0600

    initial commit

---
+++ AddDigits/JavaScript/AddDigits.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/add-digits/
+var addDigits = function (num) {
+    return 0;
+};
+// some test cases
+console.log(addDigits(38)); // 2
+console.log(addDigits(0)); // 0
