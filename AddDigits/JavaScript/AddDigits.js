2c921189942ca49dd8982fe58d6ac75bc74dceee
Jason Mauss <jason.mauss@gmail.com>
2023-10-01 00:29:38 -0600

    Adding Solutions for TypeScript and JavaScript

--- AddDigits/JavaScript/AddDigits.js
+++ AddDigits/JavaScript/AddDigits.js
@@ -1,6 +1,14 @@
 // Solution for: https://leetcode.com/problems/add-digits/
 var addDigits = function (num) {
-    return 0;
+    var digitString = num.toString();
+    while (digitString.length > 1) {
+        var sum = 0;
+        for (var i = 0; i < digitString.length; i++) {
+            sum += Number(digitString[i]);
+        }
+        digitString = sum.toString();
+    }
+    return Number(digitString);
 };
 // some test cases
 console.log(addDigits(38)); // 2
