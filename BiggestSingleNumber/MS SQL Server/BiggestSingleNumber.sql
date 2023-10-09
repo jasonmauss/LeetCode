30efe2981080171d2a5901a2ff84c992f21e2cb2
Jason Mauss <jason.mauss@gmail.com>
2023-10-08 23:02:58 -0700

    initial commit

---
+++ BiggestSingleNumber/MS SQL Server/BiggestSingleNumber.sql
@@ -0,0 +1,21 @@
+-- Solution for: https://leetcode.com/problems/biggest-single-number/ on mssql dbms platform
+
+-- Create the table and insert the data
+IF OBJECT_ID(N'dbo.MyNumbers', N'U') IS NULL BEGIN
+	CREATE TABLE MyNumbers (num INT);
+END
+
+TRUNCATE TABLE MyNumbers;
+
+INSERT INTO MyNumbers (num) VALUES (8);
+INSERT INTO MyNumbers (num) VALUES (8);
+INSERT INTO MyNumbers (num) VALUES (3);
+INSERT INTO MyNumbers (num) VALUES (3);
+INSERT INTO MyNumbers (num) VALUES (1);
+INSERT INTO MyNumbers (num) VALUES (4);
+INSERT INTO MyNumbers (num) VALUES (5);
+INSERT INTO MyNumbers (num) VALUES (6);
+
+
+-- The actual query / solution
+	
