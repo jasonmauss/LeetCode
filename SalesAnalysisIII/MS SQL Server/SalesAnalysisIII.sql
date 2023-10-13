2864ea6bd5a2e91fbcd3b92db05cd98298c77c8f
Jason Mauss <jason.mauss@gmail.com>
2023-10-12 20:35:57 -0700

    Adding Solutions for MySQL and MS SQL Server

--- SalesAnalysisIII/MS SQL Server/SalesAnalysisIII.sql
+++ SalesAnalysisIII/MS SQL Server/SalesAnalysisIII.sql
@@ -24,4 +24,13 @@ INSERT INTO Sales (seller_id, product_id, buyer_id, sale_date, quantity, price) 
 
 
 -- The actual query / solution
-	
+	SELECT	product_id,
+			product_name
+	  FROM  Product
+	 WHERE  product_id IN
+			(SELECT	product_id
+			   FROM Sales
+		   GROUP BY product_id
+			 HAVING	MAX(sale_date) <= '2019-03-31'
+				AND MIN(sale_date) >= '2019-01-01'
+			)
