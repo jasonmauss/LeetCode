0d27320a036f10b282b9dd20517b35c1c377e44e
Jason Mauss <jason.mauss@gmail.com>
2023-10-13 23:49:08 -0700

    Adding Solutions for MySQL and MS SQL Server

--- UserActivityI/MS SQL Server/UserActivityI.sql
+++ UserActivityI/MS SQL Server/UserActivityI.sql
@@ -22,4 +22,8 @@ INSERT INTO Activity (user_id, session_id, activity_date, activity_type) VALUES 
 
 
 -- The actual query / solution
-
+	SELECT	a.activity_date AS 'day',
+			COUNT(DISTINCT user_id) AS active_users
+	  FROM	Activity a
+	 WHERE	a.activity_date BETWEEN DATEADD(DAY, -29, '2019-07-27') AND '2019-07-27'
+  GROUP BY	a.activity_date
--- UserActivityI/MySQL/UserActivityI.sql
+++ UserActivityI/MySQL/UserActivityI.sql
@@ -19,4 +19,8 @@ INSERT INTO Activity (user_id, session_id, activity_date, activity_type) VALUES 
 
 
 -- The actual query / solution
-
+ SELECT		a.activity_date AS 'day',
+			COUNT(DISTINCT user_id) AS active_users
+	  FROM	Activity a
+	 WHERE	a.activity_date BETWEEN DATE_ADD('2019-07-27', INTERVAL -29 DAY) AND '2019-07-27'
+  GROUP BY	a.activity_date
