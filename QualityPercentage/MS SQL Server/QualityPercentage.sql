173c67a3b481d3ede8b8a17e93c9edd86a69aec3
Jason Mauss <jason.mauss@gmail.com>
2023-10-15 17:45:09 -0700

    Adding Solutions for MySQL and MS SQL Server

--- QualityPercentage/MS SQL Server/QualityPercentage.sql
+++ QualityPercentage/MS SQL Server/QualityPercentage.sql
@@ -17,4 +17,12 @@ INSERT INTO Queries (query_name, result, position, rating) VALUES ('Cat', 'Sphyn
 
 
 -- The actual query / solution
-	
+	SELECT	q.query_name,
+			CAST(SUM(q.rating * 1.00 / q.position * 1.00) / COUNT(query_name) AS decimal(5,2)) AS quality,
+			CAST( (SUM(CASE WHEN q.rating < 3 THEN 1.00 ELSE 0 END)
+					/
+				   COUNT(query_name)
+				   ) * 100
+				AS DECIMAL(5,2)) AS poor_query_percentage
+	  FROM	Queries q
+  GROUP BY	q.query_name
