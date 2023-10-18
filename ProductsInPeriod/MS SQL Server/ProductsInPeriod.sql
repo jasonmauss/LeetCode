5bb0deba32518bfa829b20d126bd62fb7dc1ecfb
Jason Mauss <jason.mauss@gmail.com>
2023-10-17 22:21:04 -0700

    initial commit

---
+++ ProductsInPeriod/MS SQL Server/ProductsInPeriod.sql
@@ -0,0 +1,39 @@
+-- Solution for: https://leetcode.com/problems/students-and-examinations/ on mssql dbms platform
+
+-- Create the table and insert the data
+IF OBJECT_ID(N'dbo.Products', N'U') IS NULL BEGIN
+	CREATE TABLE Products (product_id int, product_name varchar(40), product_category varchar(40));
+END
+
+TRUNCATE TABLE Products;
+
+IF OBJECT_ID(N'dbo.Orders ', N'U') IS NULL BEGIN
+	CREATE TABLE Orders  (product_id int, order_date date, unit int);
+END
+
+TRUNCATE TABLE Orders ;
+
+
+INSERT INTO Products (product_id, product_name, product_category) VALUES (1, 'Leetcode Solutions', 'Book')
+INSERT INTO Products (product_id, product_name, product_category) VALUES (2, 'Jewels of Stringology', 'Book')
+INSERT INTO Products (product_id, product_name, product_category) VALUES (3, 'HP', 'Laptop')
+INSERT INTO Products (product_id, product_name, product_category) VALUES (4, 'Lenovo', 'Laptop')
+INSERT INTO Products (product_id, product_name, product_category) VALUES (5, 'Leetcode Kit', 'T-shirt')
+
+INSERT INTO Orders (product_id, order_date, unit) VALUES (1, '2020-02-05', 60)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (1, '2020-02-10', 70)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (2, '2020-01-18', 30)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (2, '2020-02-11', 80)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (3, '2020-02-17', 2)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (3, '2020-02-24', 3)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (4, '2020-03-01', 20)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (4, '2020-03-04', 30)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (4, '2020-03-04', 60)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (5, '2020-02-25', 50)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (5, '2020-02-27', 50)
+INSERT INTO Orders (product_id, order_date, unit) VALUES (5, '2020-03-01', 50)
+
+
+
+-- The actual query / solution
+	
