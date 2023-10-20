adc1ec953fe994edbfe8ab4e27ed96ab719928cd
Jason Mauss <jason.mauss@gmail.com>
2023-10-19 22:06:31 -0700

    initial commit

---
+++ PatientsCondition/MySQL/PatientsCondition.sql
@@ -0,0 +1,16 @@
+-- Solution for: https://leetcode.com/problems/patients-with-a-condition/ on MySQL dbms platform
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS Patients (patient_id INT, patient_name VARCHAR(30), conditions VARCHAR(100));
+
+TRUNCATE TABLE Patients;
+
+INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (1, 'Daniel', 'YFEV COUGH');
+INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (2, 'Alice', '');
+INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (3, 'Bob', 'DIAB100 MYOP');
+INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (4, 'George', 'ACNE DIAB100');
+INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (5, 'Alain', 'DIAB201');
+
+
+-- The actual query / solution
+	
