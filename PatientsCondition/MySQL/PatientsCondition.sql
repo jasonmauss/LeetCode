205f86bf2cea5be31f3a8db79bdf9004b5019f24
Jason Mauss <jason.mauss@gmail.com>
2023-10-20 16:59:35 -0700

    Adding Solutions for MySQL and MS SQL Server

--- PatientsCondition/MySQL/PatientsCondition.sql
+++ PatientsCondition/MySQL/PatientsCondition.sql
@@ -13,4 +13,8 @@ INSERT INTO Patients (patient_id, patient_name, conditions) VALUES (5, 'Alain', 
 
 
 -- The actual query / solution
-	
+	SELECT	patient_id,
+			patient_name,
+			conditions
+	  FROM	Patients p
+	 WHERE	(p.conditions LIKE '%DIAB1%')
