405fc9428ef994f8b0622eae1c4148512fe70b69
Jason Mauss <jason.mauss@gmail.com>
2023-10-27 23:02:12 -0700

    Adding Solutions for MySQL and MS SQL Server

--- EmployeeReportsCount/MS SQL Server/EmployeeReportsCount.sql
+++ EmployeeReportsCount/MS SQL Server/EmployeeReportsCount.sql
@@ -8,11 +8,22 @@ END
 TRUNCATE TABLE Employees;
 
 INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (9, 'Hercy', NULL, 43)
-INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (6, 'Alice', '9', 41)
-INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (4, 'Bob', '9', 36)
+INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (6, 'Alice', 9, 41)
+INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (4, 'Bob', 9, 36)
 INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (2, 'Winston', NULL, 37)
 
 
 -- The actual query / solution
-	
+	SELECT	e1.employee_id,
+			e1.name,
+			e2.reports_count,
+			e2.average_age
+	  FROM	Employees e1
+	  JOIN	(SELECT	reports_to,
+					COUNT(reports_to) AS reports_count,
+					CAST(ROUND(AVG(age * 1.00), 0) AS INT) AS average_age
+			   FROM	Employees e
+		   GROUP BY	reports_to) AS e2 ON e2.reports_to = e1.employee_id
+
+  ORDER BY	e1.employee_id
 	
--- EmployeeReportsCount/MySQL/EmployeeReportsCount.sql
+++ EmployeeReportsCount/MySQL/EmployeeReportsCount.sql
@@ -6,13 +6,22 @@ CREATE TABLE IF NOT EXISTS Employees (employee_id INT, name VARCHAR(20), reports
 TRUNCATE TABLE Employees;
 
 INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (9, 'Hercy', NULL, 43);
-INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (6, 'Alice', '9', 41);
-INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (4, 'Bob', '9', 36);
+INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (6, 'Alice', 9, 41);
+INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (4, 'Bob', 9, 36);
 INSERT INTO Employees (employee_id, name, reports_to, age) VALUES (2, 'Winston', NULL, 37);
 
 
 
 -- The actual query / solution
-	
+	SELECT	e1.employee_id,
+			e1.name,
+			e2.reports_count,
+			e2.average_age
+	  FROM	Employees e1
+	  JOIN	(SELECT	reports_to,
+					COUNT(reports_to) AS reports_count,
+					ROUND(AVG(age * 1.00), 0) AS average_age
+			   FROM	Employees e
+		   GROUP BY	reports_to) AS e2 ON e2.reports_to = e1.employee_id
 
 
