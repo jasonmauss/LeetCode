0d1a986fdf6f3ef87d40fc8abb9c74bd750c72a6
Jason Mauss <jason.mauss@gmail.com>
2023-10-27 23:08:17 -0700

    initial commit

---
+++ SpecialBonus/MySQL/SpecialBonus.sql
@@ -0,0 +1,19 @@
+-- Solution for: https://leetcode.com/problems/calculate-special-bonus/ on MySQL dbms platform
+USE leetcodedb;
+
+CREATE TABLE IF NOT EXISTS Employees (employee_id INT, name VARCHAR(30), salary INT);
+
+TRUNCATE TABLE Employees;
+
+INSERT INTO Employees (employee_id, name, salary) VALUES (2, 'Meir', 3000);
+INSERT INTO Employees (employee_id, name, salary) VALUES (3, 'Michael', 3800);
+INSERT INTO Employees (employee_id, name, salary) VALUES (7, 'Addilyn', 7400);
+INSERT INTO Employees (employee_id, name, salary) VALUES (8, 'Juan', 6100);
+INSERT INTO Employees (employee_id, name, salary) VALUES (9, 'Kannon', 7700);
+
+
+
+-- The actual query / solution
+	
+
+
