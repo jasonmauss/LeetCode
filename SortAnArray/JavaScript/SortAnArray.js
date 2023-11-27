8ce6f78ccfe0d1c780515904888f939f0a247375
Jason Mauss <jason.mauss@gmail.com>
2023-11-26 23:36:47 -0700

    Adding Solutions for TypeScript and JavaScript

--- SortAnArray/JavaScript/SortAnArray.js
+++ SortAnArray/JavaScript/SortAnArray.js
@@ -1,6 +1,38 @@
 // Solution for: https://leetcode.com/problems/sort-an-array/
 var sortArray = function (nums) {
-    return null;
+    var temp = new Array(nums.length);
+    mergeSort(nums, temp, 0, nums.length - 1);
+    return nums;
+};
+var mergeSort = function (nums, temp, left, right) {
+    if (left < right) {
+        var mid = Math.floor((left + right) / 2);
+        mergeSort(nums, temp, left, mid);
+        mergeSort(nums, temp, mid + 1, right);
+        merge(nums, temp, left, mid, right);
+    }
+};
+var merge = function (nums, temp, left, mid, right) {
+    var i = left;
+    var j = mid + 1;
+    var k = left;
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            temp[k++] = nums[i++];
+        }
+        else {
+            temp[k++] = nums[j++];
+        }
+    }
+    while (i <= mid) {
+        temp[k++] = nums[i++];
+    }
+    while (j <= right) {
+        temp[k++] = nums[j++];
+    }
+    for (var m = left; m <= right; m++) {
+        nums[m] = temp[m];
+    }
 };
 // some tests
 console.log(sortArray([5, 2, 3, 1])); // [1, 2, 3, 5]
