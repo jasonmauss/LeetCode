dac9732d4b12740f5e212b44bc2feb3fcd3ad238
Jason Mauss <jason.mauss@gmail.com>
2023-12-03 23:07:16 -0700

    initial commit

---
+++ SortEvenOdd/TypeScript/SortEvenOdd.ts
@@ -0,0 +1,9 @@
+// Solution for: https://leetcode.com/problems/sort-even-and-odd-indices-independently/
+const sortEvenOdd = (nums: number[]): number[] => {
+    return [];
+};
+
+
+// some tests
+console.log(sortEvenOdd([4,1,2,3])); // [2,3,4,1]
+console.log(sortEvenOdd([2,1])); // [2,1]
