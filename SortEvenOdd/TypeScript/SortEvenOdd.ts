a0e42e11b356ef3cc7ed1b842eec269b5fad680a
Jason Mauss <jason.mauss@gmail.com>
2023-12-04 20:40:29 -0700

    Adding Solutions for TypeScript and JavaScript

--- SortEvenOdd/TypeScript/SortEvenOdd.ts
+++ SortEvenOdd/TypeScript/SortEvenOdd.ts
@@ -1,6 +1,30 @@
 // Solution for: https://leetcode.com/problems/sort-even-and-odd-indices-independently/
 const sortEvenOdd = (nums: number[]): number[] => {
-    return [];
+    
+    const evenNums = [];
+    const oddNums = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(i % 2 === 0) {
+            evenNums.push(nums[i]);
+        } else {
+            oddNums.push(nums[i]);
+        }
+    }
+
+    // sort even index nums in ascending order
+    if(evenNums.length > 0) evenNums.sort((a,b) => a - b);
+    // sort odd index nums in descending order
+    if(oddNums.length > 0) oddNums.sort((a,b) => b - a);
+
+    const returnArray = [];
+
+    while(evenNums.length > 0 || oddNums.length > 0) {
+        if(evenNums.length > 0) returnArray.push(evenNums.shift());
+        if(oddNums.length > 0) returnArray.push(oddNums.shift());
+    }
+
+    return returnArray;
 };
 
 
