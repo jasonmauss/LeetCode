a3453738c7c0247305da127c5e6ab8c5763ef25f
Jason Mauss <jason.mauss@gmail.com>
2023-12-10 22:52:09 -0700

    Adding Solutions for TypeScript and JavaScript

--- MinRightShifts/JavaScript/MinRightShifts.js
+++ MinRightShifts/JavaScript/MinRightShifts.js
@@ -1,6 +1,23 @@
 // Solution for: https://leetcode.com/problems/minimum-right-shifts-to-sort-the-array/
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var minimumRightShifts = function (nums) {
-    return 0;
+    var sortedNums = __spreadArray([], nums, true).sort(function (a, b) { return a - b; });
+    if (JSON.stringify(nums) == JSON.stringify(sortedNums))
+        return 0;
+    for (var i = 0; i < nums.length; i++) {
+        nums.unshift(nums.pop());
+        if (JSON.stringify(nums) == JSON.stringify(sortedNums))
+            return i + 1;
+    }
+    return -1;
 };
 // some test cases
 console.log(minimumRightShifts([3, 4, 5, 1, 2])); // 2
