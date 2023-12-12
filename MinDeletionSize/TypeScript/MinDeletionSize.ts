c965663203db7431f7903c9cbdf8f322c7b34d49
Jason Mauss <jason.mauss@gmail.com>
2023-12-11 23:29:38 -0700

    initial commit

---
+++ MinDeletionSize/TypeScript/MinDeletionSize.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/delete-columns-to-make-sorted/
+
+const minDeletionSize = (strs: string[]): number => {
+    
+    return 0;
+
+};
+
+
+// some tests
+console.log(minDeletionSize(["cba","daf","ghi"])); // 1
+console.log(minDeletionSize(["a","b"])); // 0
+console.log(minDeletionSize(["zyx","wvu","tsr"])); // 3
