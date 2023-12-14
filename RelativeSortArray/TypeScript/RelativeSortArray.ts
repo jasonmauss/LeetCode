defa42c73ee4142224709925f0eaaf8b095950ce
Jason Mauss <jason.mauss@gmail.com>
2023-12-13 23:19:44 -0700

    initial commit

---
+++ RelativeSortArray/TypeScript/RelativeSortArray.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/relative-sort-array/
+
+const relativeSortArray = (arr1: number[], arr2: number[]): number[] => {
+    
+    return [];
+
+};
+
+
+// some tests
+console.log(relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6])); // [2,2,2,1,4,3,3,9,6,7,19]
+console.log(relativeSortArray([28,6,22,8,44,17], [22,28,8,6])); // [22,28,8,6,17,44]
