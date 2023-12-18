95225bef14651d5eda0c2e182fe1159ee3243eff
Jason Mauss <jason.mauss@gmail.com>
2023-12-17 22:24:37 -0700

    initial commit

---
+++ CountNegatives/JavaScript/CountNegatives.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+var countNegatives = function (grid) {
+    return 0;
+};
+// some tests
+console.log(countNegatives([[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]])); // 8
+console.log(countNegatives([[3, 2], [1, 0]])); // 0
