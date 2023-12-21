38967be43afb45b448fafac163b2adaed53235c8
Jason Mauss <jason.mauss@gmail.com>
2023-12-20 22:12:49 -0700

    Adding Solutions for TypeScript and JavaScript

--- MinDeletionSizeIII/TypeScript/MinDeletionSizeIII.ts
+++ MinDeletionSizeIII/TypeScript/MinDeletionSizeIII.ts
@@ -1,7 +1,26 @@
 // Solution for: https://leetcode.com/problems/delete-columns-to-make-sorted-iii/
 const minDeletionSize = (strs: string[]): number => {
     
-    return 0;
+    const totalStrs = strs.length;
+    const strSize = strs[0].length;
+    let result:number = strSize - 1;
+    let k:number = 0;
+
+    let dp:number[] = new Array<number>(strSize).fill(1); // using fill so compile with tsc --lib "es2015,dom"
+
+    for(let i = 0; i < strSize; ++i) {
+        for(let j = 0; j < i; ++j) {
+            for(k = 0; k < totalStrs; ++k) {
+                if(strs[k].charAt(j) > strs[k].charAt(i)) break;
+            }
+            if(k === totalStrs && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        result = Math.min(result, strSize - dp[i]);
+    }
+
+    return result;
 
 };
 
