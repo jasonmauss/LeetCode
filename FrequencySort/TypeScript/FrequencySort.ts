b341803cd29a7dd128b9dc115ae8e03c117429cb
Jason Mauss <jason.mauss@gmail.com>
2023-12-21 18:04:29 -0700

    initial commit

---
+++ FrequencySort/TypeScript/FrequencySort.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+const frequencySort = (nums: number[]): number[] => {
+
+    return [];
+    
+};
+
+
+// some tests
+console.log(frequencySort([1,1,2,2,2,3])); // [3,1,1,2,2,2]
+console.log(frequencySort([2,3,1,3,2])); // [1,3,3,2,2]
+console.log(frequencySort([-1,1,-6,4,5,-6,1,4,1])); // [5,-1,4,4,-6,-6,1,1,1]
