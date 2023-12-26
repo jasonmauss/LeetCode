19444c9e90839971e9cddd241a8aa15b249ac779
Jason Mauss <jason.mauss@gmail.com>
2023-12-25 23:36:46 -0700

    initial commit

---
+++ PartitionString/JavaScript/PartitionString.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/optimal-partition-of-string/
+const partitionString = (s) => {
+    return 0;
+};
+// some tests
+console.log(partitionString('abacaba')); // 4
+console.log(partitionString('ssssss')); // 6
---
+++ PartitionString/TypeScript/PartitionString.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/optimal-partition-of-string/
+const partitionString = (s: string): number => {
+    
+    return 0;
+
+};
+
+
+
+
+
+// some tests
+console.log(partitionString('abacaba')); // 4
+console.log(partitionString('ssssss')); // 6
