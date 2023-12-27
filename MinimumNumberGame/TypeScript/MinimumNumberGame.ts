94d109d35eae748c43b01ef8b41e19c87040cfec
Jason Mauss <jason.mauss@gmail.com>
2023-12-27 14:44:48 -0700

    initial commit

---
+++ MinimumNumberGame/TypeScript/MinimumNumberGame.ts
@@ -0,0 +1,10 @@
+// Solution for: https://leetcode.com/problems/minimum-number-game/
+function numberGame(nums: number[]): number[] {
+    return [];
+};
+
+
+
+// some tests
+console.log(numberGame([5,4,2,3])); // [3,2,5,4]
+console.log(numberGame([2,5])); // [5,2]
