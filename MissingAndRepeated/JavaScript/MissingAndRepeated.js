cba5c1ca09bbda11f6af3234f3bd44130ea680a3
Jason Mauss <jason.mauss@gmail.com>
2023-12-30 19:46:27 -0700

    Adding Solutions for TypeScript and JavaScript

--- MissingAndRepeated/JavaScript/MissingAndRepeated.js
+++ MissingAndRepeated/JavaScript/MissingAndRepeated.js
@@ -1,6 +1,18 @@
 // Solution for: https://leetcode.com/problems/find-missing-and-repeated-values/
 const findMissingAndRepeatedValues = (grid) => {
-    return [];
+    let missing = 0;
+    let repeated = 0;
+    let numberTracking = new Array(Math.pow(grid.length, 2)).fill(0);
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            let temp = grid[i][j] - 1;
+            numberTracking[temp]++;
+            if (numberTracking[temp] === 2)
+                repeated = temp + 1;
+        }
+    }
+    missing = numberTracking.findIndex((x) => x === 0) + 1;
+    return [repeated, missing];
 };
 // some tests
 console.log(findMissingAndRepeatedValues([[1, 3], [2, 2]])); // [2,4]
