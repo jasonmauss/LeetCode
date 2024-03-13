8fbe58e004a8a2ecd2a8da62348c5d7c49071775
Jason Mauss <jason.mauss@gmail.com>
2024-03-12 23:31:23 -0700

    initial commit

---
+++ FindIntersectionValues/JavaScript/FindIntersectionValues.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/find-common-elements-between-two-arrays/
+var findIntersectionValues = function (nums1, nums2) {
+    return [0];
+};
+// some tests[
+console.log(findIntersectionValues([4, 3, 2, 3, 1], [2, 2, 5, 2, 3, 6])); // [3,4]
+console.log(findIntersectionValues([3, 4, 2, 3], [1, 5])); // [0,0]
---
+++ FindIntersectionValues/TypeScript/FindIntersectionValues.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/find-common-elements-between-two-arrays/
+const findIntersectionValues = (nums1: number[], nums2: number[]): number[] => {
+    
+    return [0];
+
+};
+
+
+// some tests[
+console.log(findIntersectionValues([4,3,2,3,1], [2,2,5,2,3,6])); // [3,4]
+console.log(findIntersectionValues([3,4,2,3], [1,5])); // [0,0]
