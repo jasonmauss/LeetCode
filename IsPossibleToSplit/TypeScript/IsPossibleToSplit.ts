93d2c5810f816155c950322a783610da425cd24e
Jason Mauss <jason.mauss@gmail.com>
2024-03-14 22:32:11 -0700

    initial commit

---
+++ IsPossibleToSplit/TypeScript/IsPossibleToSplit.ts
@@ -0,0 +1,11 @@
+// Solution for: https://leetcode.com/problems/split-the-array/
+const isPossibleToSplit = (nums: number[]): boolean => {
+    
+    return false;
+
+};
+
+
+// some test cases
+console.log(isPossibleToSplit([1,1,2,2,3,4])); // true
+console.log(isPossibleToSplit([1,1,1,1])); // false
