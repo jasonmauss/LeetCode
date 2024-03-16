f3126485c32a1ca4170395e2797146a8d9e6236a
Jason Mauss <jason.mauss@gmail.com>
2024-03-15 23:31:51 -0700

    Adding Solutions for TypeScript and JavaScript

--- IsPossibleToSplit/TypeScript/IsPossibleToSplit.ts
+++ IsPossibleToSplit/TypeScript/IsPossibleToSplit.ts
@@ -1,7 +1,19 @@
 // Solution for: https://leetcode.com/problems/split-the-array/
 const isPossibleToSplit = (nums: number[]): boolean => {
     
-    return false;
+    const digits = {};
+
+    for(const num of nums) {
+        digits[num] = digits[num] ? digits[num] += 1 : digits[num] = 1;
+    }
+
+    for(const prop in digits) {
+        if(digits[prop] > 2) {
+            return false;
+        }
+    }
+
+    return true;
 
 };
 
@@ -9,3 +21,4 @@ const isPossibleToSplit = (nums: number[]): boolean => {
 // some test cases
 console.log(isPossibleToSplit([1,1,2,2,3,4])); // true
 console.log(isPossibleToSplit([1,1,1,1])); // false
+console.log(isPossibleToSplit([8,9,8,5,9,3,3,1,2,1])); // true
