fac4531b83e191be01775a95e96e211821bdab25
Jason Mauss <jason.mauss@gmail.com>
2024-03-16 15:32:46 -0700

    initial commit

---
+++ MinimizeStringValue/JavaScript/MinimizeStringValue.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/replace-question-marks-in-string-to-minimize-its-value/description/
+const minimizeStringValue = (s) => {
+    return '';
+};
+// some test cases
+console.log(minimizeStringValue('???')); // abc
+console.log(minimizeStringValue('a?a?')); // abac
