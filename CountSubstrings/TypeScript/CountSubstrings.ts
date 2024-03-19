4c0de8f900b474c9d113887e2190e187f53f1959
Jason Mauss <jason.mauss@gmail.com>
2024-03-18 22:39:43 -0700

    initial commit

---
+++ CountSubstrings/TypeScript/CountSubstrings.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/count-substrings-starting-and-ending-with-given-character/description/
+const countSubstrings = (s: string, c: string): number => {
+    
+    return 0;
+    
+};
+
+
+
+
+// some test cases
+console.log(countSubstrings('abada', 'a')); // 6
+console.log(countSubstrings('zzz','z')); // 6
