aef8580270562fa38059d73aeac39931edd7b8ae
Jason Mauss <jason.mauss@gmail.com>
2024-03-25 23:24:55 -0700

    Adding Solutions for MySQL and MS SQL Server

--- MonthlyTransactionsI/MySQL/MonthlyTransactionsI.sql
+++ MonthlyTransactionsI/MySQL/MonthlyTransactionsI.sql
@@ -10,6 +10,13 @@ INSERT INTO Transactions (id, country, state, amount, trans_date) VALUES ('124',
 
 
 -- The actual query / solution
-
+	SELECT	DATE_FORMAT(trans_date, "%Y-%m") AS month,
+			country,
+			COUNT(id) AS trans_count,
+			SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+			SUM(amount) AS trans_total_amount,
+			SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+	  FROM	Transactions tx
+  GROUP BY	DATE_FORMAT(trans_date, "%Y-%m"), country;
 
 
