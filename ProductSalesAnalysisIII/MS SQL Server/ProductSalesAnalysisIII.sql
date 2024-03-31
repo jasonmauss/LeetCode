9e8980ef1a91554285bb1c33adaae7c5fe522e9f
Jason Mauss <jason.mauss@gmail.com>
2024-03-30 20:13:43 -0700

    initial commit

---
+++ ProductSalesAnalysisIII/MS SQL Server/ProductSalesAnalysisIII.sql
@@ -0,0 +1,28 @@
+-- Solution for: https://leetcode.com/problems/product-sales-analysis-iii on mssql dbms platform
+
+USE leetcodedb;
+
+-- Create the table and insert the data
+IF OBJECT_ID(N'dbo.Sales', N'U') IS NULL BEGIN
+	CREATE TABLE Sales (sale_id INT, product_id INT, [year] INT, quantity INT, price INT);
+END
+
+IF OBJECT_ID(N'dbo.Product', N'U') IS NULL BEGIN
+	CREATE TABLE Product (product_id INT, product_name VARCHAR(10));
+END
+
+TRUNCATE TABLE Sales;
+TRUNCATE TABLE Product;
+
+INSERT INTO Sales (sale_id, product_id, [year], quantity, price) VALUES (1, 100, 2008, 10, 5000)
+INSERT INTO Sales (sale_id, product_id, [year], quantity, price) VALUES (2, 100, 2009, 12, 5000)
+INSERT INTO Sales (sale_id, product_id, [year], quantity, price) VALUES (7, 200, 2011, 15, 9000)
+
+INSERT INTO Product (product_id, product_name) VALUES (100, 'Nokia')
+INSERT INTO Product (product_id, product_name) VALUES (200, 'Apple')
+INSERT INTO Product (product_id, product_name) VALUES (300, 'Samsung')
+
+
+-- The actual query / solution
+
+
