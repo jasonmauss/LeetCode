7596a7c6c9e6f4d7ad6e340a96710b77b8e7c47a
Jason Mauss <jason.mauss@gmail.com>
2024-04-17 16:45:42 -0700

    Adding Solutions for MySQL and MS SQL Server

--- FriendRequestsII/MySQL/FriendRequestsII.sql
+++ FriendRequestsII/MySQL/FriendRequestsII.sql
@@ -5,9 +5,26 @@ CREATE TABLE IF NOT EXISTS RequestAccepted (requester_id INT NOT NULL, accepter_
 
 TRUNCATE TABLE RequestAccepted;
 
-INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('1', '2', '2016/06/03');
-INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('1', '3', '2016/06/08');
-INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('2', '3', '2016/06/08');
-INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('3', '4', '2016/06/09');
+INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('1', '2', '2016-06-03');
+INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('1', '3', '2016-06-08');
+INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('2', '3', '2016-06-08');
+INSERT INTO RequestAccepted (requester_id, accepter_id, accept_date) VALUES ('3', '4', '2016-06-09');
 
 -- The actual query / solution
+	SELECT	dataset.id,
+			SUM(dataset.RequestOrAcceptCount) AS num 
+	  FROM (SELECT	ra.requester_id AS id,
+					COUNT(ra.requester_id) AS RequestOrAcceptCount
+			  FROM	RequestAccepted ra
+		  GROUP BY	ra.requester_id
+		
+			 UNION	ALL
+
+			SELECT	ra.accepter_id AS id,
+					COUNT(ra.accepter_id) AS RequestOrAcceptCount
+			  FROM	RequestAccepted ra
+		  GROUP BY	ra.accepter_id) AS dataset
+
+  GROUP BY	dataset.id
+  ORDER BY	SUM(dataset.RequestOrAcceptCount) DESC
+	 LIMIT  1
