cd77e1e6c23537489dd90eb686039f668ecca7b4
Jason Mauss <jason.mauss@gmail.com>
2024-05-05 23:00:33 -0700

    initial commit

---
+++ SearchRange/JavaScript/SearchRange.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+var searchRange = function (nums, target) {
+    return [];
+};
+// some test cases
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8)); // [3,4]
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6)); // [-1,-1]
+console.log(searchRange([], 0)); // [-1,-1]
