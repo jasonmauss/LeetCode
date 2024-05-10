fa0e7ea57656e93cd4a7c6aa1ce35823c635773c
Jason Mauss <jason.mauss@gmail.com>
2024-05-09 23:41:44 -0700

    initial commit

---
+++ MultiplyStrings/TypeScript/MultiplyStrings.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/multiply-strings/
+
+const multiply = (num1: string, num2: string): string => {
+    
+    return '';
+
+};
+
+
+
+// some test cases
+console.log(multiply('2', '3')); // '6'
+console.log(multiply('123', '456')); // '56088'
