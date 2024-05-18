09a3fd1bb29a9728f9df1ffa10b64d97a066b279
Jason Mauss <jason.mauss@gmail.com>
2024-05-18 10:49:46 -0700

    initial commit

---
+++ CombinationSumII/JavaScript/CombinationSumII.js
@@ -0,0 +1,21 @@
+// Solution for: https://leetcode.com/problems/combination-sum-ii/
+var combinationSum2 = function (candidates, target) {
+    return [[0]];
+};
+// some test cases
+console.log(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)); /*
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+
+*/
+console.log(combinationSum2([2, 5, 2, 1, 2], 5)); /*
+[
+[1,2,2],
+[5]
+]
+
+*/
---
+++ CombinationSumII/TypeScript/CombinationSumII.ts
@@ -0,0 +1,29 @@
+// Solution for: https://leetcode.com/problems/combination-sum-ii/
+
+
+const combinationSum2 = (candidates: number[], target: number): number[][] => {
+    
+    return [[0]];
+    
+};
+
+
+
+// some test cases
+console.log(combinationSum2([10,1,2,7,6,1,5], 8)); /* 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+
+*/
+
+console.log(combinationSum2([2,5,2,1,2], 5)); /* 
+[
+[1,2,2],
+[5]
+]
+
+*/
