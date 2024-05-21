94c90177f7d7938be2ab9341df9fe06c7a5f7463
Jason Mauss <jason.mauss@gmail.com>
2024-05-20 22:10:31 -0700

    initial commit

---
+++ SingleNumberII/JavaScript/SingleNumberII.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/single-number-ii/
+var singleNumber = function (nums) {
+    return 0;
+};
+// some test cases
+console.log(singleNumber([2, 2, 3, 2])); // 3
+console.log(singleNumber([0, 1, 0, 1, 0, 1, 99])); // 99
