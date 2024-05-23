1a025fac73942afd8123fefe958622faa36c63c8
Jason Mauss <jason.mauss@gmail.com>
2024-05-22 19:30:05 -0700

    initial commit

---
+++ CourseScheduleII/JavaScript/CourseScheduleII.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/course-schedule-ii/description/
+var findOrder = function (numCourses, prerequisites) {
+    return [];
+};
+// some test cases
+console.log(findOrder(2, [[1, 0]])); // [0,1]
+console.log(findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]])); // [0,2,1,3]
