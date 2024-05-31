c01bbc40a13233deb0d16b9d432a73dd7f91e223
Jason Mauss <jason.mauss@gmail.com>
2024-05-30 22:01:35 -0700

    initial commit

---
+++ LongestPalindrome/JavaScript/LongestPalindrome.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/longest-palindrome/
+var longestPalindrome = function (s) {
+    return 0;
+};
+// some test cases
+console.log(longestPalindrome('abccccdd')); // 7
+console.log(longestPalindrome('a')); // 1
---
+++ LongestPalindrome/TypeScript/LongestPalindrome.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/longest-palindrome/
+
+const longestPalindrome = (s: string): number => {
+    
+    return 0;
+
+};
+
+
+// some test cases
+console.log(longestPalindrome('abccccdd')); // 7
+console.log(longestPalindrome('a')); // 1
