6f3345d0f5396c1b826ff149dc234be5ca8682a9
Jason Mauss <jason.mauss@gmail.com>
2024-06-01 22:45:36 -0700

    initial commit

---
+++ IslandPerimeter/JavaScript/IslandPerimeter.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/island-perimeter/
+var islandPerimeter = function (grid) {
+    return 0;
+};
+// some test cases
+console.log(islandPerimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]])); // 16
+console.log(islandPerimeter([[1]])); // 4
+console.log(islandPerimeter([[1, 0]])); // 4
---
+++ IslandPerimeter/TypeScript/IslandPerimeter.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/island-perimeter/
+
+const islandPerimeter = (grid: number[][]): number => {
+    
+    return 0;
+
+};
+
+
+// some test cases
+console.log(islandPerimeter([[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])); // 16
+console.log(islandPerimeter([[1]])); // 4
+console.log(islandPerimeter([[1,0]])); // 4
