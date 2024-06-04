e2b87e774f613a5dd434f7079e350b92ba122b30
Jason Mauss <jason.mauss@gmail.com>
2024-06-03 22:08:58 -0700

    initial commit

---
+++ NextGreaterElement/JavaScript/NextGreaterElement.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/next-greater-element-i
+var nextGreaterElement = function (nums1, nums2) {
+    return [0];
+};
+// some test cases
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])); // [-1, 3, -1]
+console.log(nextGreaterElement([2, 4], [1, 2, 3, 4])); // [3, -1]
