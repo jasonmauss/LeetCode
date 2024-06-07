03493fe7981affb0fd62fab82f90aa61ce1b01af
Jason Mauss <jason.mauss@gmail.com>
2024-06-06 22:12:26 -0700

    Adding Solutions for TypeScript and JavaScript

--- StudentAttendanceRecordI/TypeScript/StudentAttendanceRecordI.ts
+++ StudentAttendanceRecordI/TypeScript/StudentAttendanceRecordI.ts
@@ -2,7 +2,35 @@
 
 const checkRecord = (s: string): boolean => {
     
-    return false;
+    // the long way
+    // let totalAbscences:number = 0;
+    // let consecutiveDaysLate:number = 0;
+
+    // for(let char of s) {
+    //     if(char === 'A') {
+    //         totalAbscences++;
+    //         if(totalAbscences === 2) {
+    //             return false;
+    //         }
+    //         consecutiveDaysLate = 0;
+    //     }
+
+    //     if(char === 'L') {
+    //         consecutiveDaysLate++;
+    //         if(consecutiveDaysLate === 3) {
+    //             return false;
+    //         }
+    //     }
+
+    //     if(char === 'P') {
+    //         consecutiveDaysLate = 0;
+    //     }
+    // }
+
+    // return totalAbscences < 2 && consecutiveDaysLate < 3;
+
+    // the short way
+    return s.split('').filter((x) => x === 'A').length < 2 && !s.includes('LLL');
 
 };
 
