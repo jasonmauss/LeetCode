f73e0e46f71bb801a05e8b80a4de4645590a753e
Jason Mauss <jason.mauss@gmail.com>
2024-06-07 19:34:45 -0700

    initial commit

---
+++ DistributeCandies/JavaScript/DistributeCandies.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/distribute-candies
+var distributeCandies = function (candyType) {
+    return 0;
+};
+// some test cases
+console.log(distributeCandies([1, 1, 2, 2, 3, 3])); // 3
+console.log(distributeCandies([1, 1, 2, 3])); // 2
+console.log(distributeCandies([6, 6, 6, 6])); // 1
---
+++ DistributeCandies/TypeScript/DistributeCandies.ts
@@ -0,0 +1,14 @@
+// Solution for: https://leetcode.com/problems/distribute-candies
+
+const distributeCandies = (candyType: number[]): number => {
+    
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(distributeCandies([1,1,2,2,3,3])); // 3
+console.log(distributeCandies([1,1,2,3])); // 2
+console.log(distributeCandies([6,6,6,6])); // 1
