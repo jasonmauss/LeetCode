682b2a9ecadbe55c1020a73c892dcb75091bb5af
Jason Mauss <jason.mauss@gmail.com>
2024-06-09 15:14:40 -0700

    initial commit

---
+++ ReverseStringII/TypeScript/ReverseStringII.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/reverse-string-ii/
+
+const reverseStr = (s: string, k: number): string => {
+    
+    return '';
+
+};
+
+
+
+// some test cases
+console.log(reverseStr('abcdefg', 2)); // 'bacdfeg'
+console.log(reverseStr('abcd', 2)); // 'bacd'
