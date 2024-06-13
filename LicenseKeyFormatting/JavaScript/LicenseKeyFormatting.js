e92853ab9a0c8e7db1f9d8357ad54dde64309984
Jason Mauss <jason.mauss@gmail.com>
2024-06-13 15:37:44 -0700

    initial commit

---
+++ LicenseKeyFormatting/JavaScript/LicenseKeyFormatting.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/license-key-formatting/
+var licenseKeyFormatting = function (s, k) {
+    return '';
+};
+// some test cases
+console.log(licenseKeyFormatting('5F3Z-2e-9-w', 4)); // '5F3Z-2E9W'
+console.log(licenseKeyFormatting('2-5g-3-J', 2)); // '2-5G-3J'
---
+++ LicenseKeyFormatting/TypeScript/LicenseKeyFormatting.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/license-key-formatting/
+
+const licenseKeyFormatting = (s: string, k: number): string => {
+    
+    return '';
+
+};
+
+
+// some test cases
+console.log(licenseKeyFormatting('5F3Z-2e-9-w', 4)); // '5F3Z-2E9W'
+console.log(licenseKeyFormatting('2-5g-3-J', 2)); // '2-5G-3J'
