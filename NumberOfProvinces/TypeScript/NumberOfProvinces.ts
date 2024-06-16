12400d88f500e39786da7bbd4f8ec02c6e6074ab
Jason Mauss <jason.mauss@gmail.com>
2024-06-15 22:56:14 -0700

    initial commit

---
+++ NumberOfProvinces/TypeScript/NumberOfProvinces.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/number-of-provinces/
+
+const findCircleNum = (isConnected: number[][]): number => {
+
+    return 0;
+    
+};
+
+
+// some test cases
+console.log(findCircleNum([[1,1,0],[1,1,0],[0,0,1]])); // 2
+console.log(findCircleNum([[1,0,0],[0,1,0],[0,0,1]])); // 3
