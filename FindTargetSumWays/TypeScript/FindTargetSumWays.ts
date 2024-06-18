a3396802b404bfb5fafc21a6e536d6ab2939303e
Jason Mauss <jason.mauss@gmail.com>
2024-06-17 23:43:14 -0700

    initial commit

---
+++ FindTargetSumWays/TypeScript/FindTargetSumWays.ts
@@ -0,0 +1,12 @@
+// Solution for: https://leetcode.com/problems/target-sum/
+
+const findTargetSumWays = (nums: number[], target: number): number => {
+    
+    return 0;
+
+};
+
+
+// some test cases
+console.log(findTargetSumWays([1,1,1,1,1], 3)); // 5
+console.log(findTargetSumWays([1], 1)); // 1
