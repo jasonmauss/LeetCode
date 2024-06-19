534250fb1c31ef470477d73853878e2ce6b24820
Jason Mauss <jason.mauss@gmail.com>
2024-06-18 22:19:32 -0700

    Adding Solutions for TypeScript and JavaScript

--- FindTargetSumWays/JavaScript/FindTargetSumWays.js
+++ FindTargetSumWays/JavaScript/FindTargetSumWays.js
@@ -1,6 +1,20 @@
 // Solution for: https://leetcode.com/problems/target-sum/
 var findTargetSumWays = function (nums, target) {
-    return 0;
+    var sumKeys = new Map();
+    var traverseOptions = function (precedingSum, index) {
+        var memoKey = "".concat(precedingSum, "-").concat(index);
+        if (sumKeys.has(memoKey)) {
+            return sumKeys.get(memoKey);
+        }
+        if (index === nums.length) {
+            return precedingSum === target ? 1 : 0;
+        }
+        var optionsSum = traverseOptions(precedingSum + nums[index], index + 1) +
+            traverseOptions(precedingSum - nums[index], index + 1);
+        sumKeys.set(memoKey, optionsSum);
+        return optionsSum;
+    };
+    return traverseOptions(0, 0);
 };
 // some test cases
 console.log(findTargetSumWays([1, 1, 1, 1, 1], 3)); // 5
--- FindTargetSumWays/TypeScript/FindTargetSumWays.ts
+++ FindTargetSumWays/TypeScript/FindTargetSumWays.ts
@@ -2,7 +2,30 @@
 
 const findTargetSumWays = (nums: number[], target: number): number => {
     
-    return 0;
+    const sumKeys = new Map<string, number>();
+
+	const traverseOptions = (precedingSum: number, index: number) => {
+
+		const memoKey = `${precedingSum}-${index}`;
+
+		if (sumKeys.has(memoKey)) {
+			return sumKeys.get(memoKey);
+		}
+
+		if (index === nums.length) {
+			return precedingSum === target ? 1 : 0;
+		}
+
+		const optionsSum =
+			traverseOptions(precedingSum + nums[index], index + 1) +
+			traverseOptions(precedingSum - nums[index], index + 1);
+
+            sumKeys.set(memoKey, optionsSum);
+
+		return optionsSum;
+	};
+
+	return traverseOptions(0, 0);
 
 };
 
