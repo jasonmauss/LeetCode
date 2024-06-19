90030754ab4f1274b273fb254b2831904d139a59
Jason Mauss <jason.mauss@gmail.com>
2024-06-19 16:24:10 -0700

    initial commit

---
+++ FindAnagrams/JavaScript/FindAnagrams.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+var findAnagrams = function (s, p) {
+    return [0];
+};
+// some test cases
+console.log(findAnagrams('cbaebabacd', 'abc')); // [0,6]
+console.log(findAnagrams('abab', 'ab')); // [0,1,2]
