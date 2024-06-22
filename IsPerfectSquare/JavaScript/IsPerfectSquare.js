c0eb6ec1b320f56fc09fe65dc4f6c1dcdc6b3617
Jason Mauss <jason.mauss@gmail.com>
2024-06-21 22:53:55 -0700

    initial commit

---
+++ IsPerfectSquare/JavaScript/IsPerfectSquare.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/valid-perfect-square/
+function isPerfectSquare(num) {
+    return false;
+}
+;
+// some test cases
+console.log(isPerfectSquare(16)); // true
+console.log(isPerfectSquare(14)); // false
