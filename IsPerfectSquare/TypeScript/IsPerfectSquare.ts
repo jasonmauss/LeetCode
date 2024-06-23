2401c9533c1b0f588a89c1a46a8ccbf93f008ba3
Jason Mauss <jason.mauss@gmail.com>
2024-06-22 22:57:05 -0700

    Adding Solutions for TypeScript and JavaScript

--- IsPerfectSquare/TypeScript/IsPerfectSquare.ts
+++ IsPerfectSquare/TypeScript/IsPerfectSquare.ts
@@ -2,6 +2,21 @@
 
 function isPerfectSquare(num: number): boolean {
     
+    let lesserNum = 1;
+    let higherNum = num;
+
+    while(lesserNum <= higherNum) {
+        const middleNum = Math.floor((lesserNum + higherNum) / 2);
+
+        const squaredResult = middleNum * middleNum;
+
+        if(squaredResult === num) return true;
+
+        else if(squaredResult < num) lesserNum = middleNum + 1;
+
+        else higherNum = middleNum - 1;
+    }
+
     return false;
 
 };
