375a3ebedb204355ff0fdf663dc0fc221669a053
Jason Mauss <jason.mauss@gmail.com>
2024-06-23 23:02:59 -0700

    initial commit

---
+++ HammingWeight/JavaScript/HammingWeight.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/number-of-1-bits/
+var hammingWeight = function (n) {
+    return 0;
+};
+// some test cases
+console.log(hammingWeight(11)); // 3
+console.log(hammingWeight(128)); // 1
+console.log(hammingWeight(2147483645)); // 30
