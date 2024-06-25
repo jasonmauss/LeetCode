3fdb1268b1a2b3ca3eca9ba9926af4584c54c52d
Jason Mauss <jason.mauss@gmail.com>
2024-06-24 21:40:24 -0700

    Adding Solutions for TypeScript and JavaScript

--- HammingWeight/JavaScript/HammingWeight.js
+++ HammingWeight/JavaScript/HammingWeight.js
@@ -1,6 +1,8 @@
 // Solution for: https://leetcode.com/problems/number-of-1-bits/
 var hammingWeight = function (n) {
-    return 0;
+    var regex = new RegExp('1', 'g');
+    var matches = n.toString(2).match(regex);
+    return matches ? matches.length : 0;
 };
 // some test cases
 console.log(hammingWeight(11)); // 3
