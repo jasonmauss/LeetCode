e9e957631f6ea15f97f3f08fd9033aef5aa6759d
Jason Mauss <jason.mauss@gmail.com>
2024-06-29 15:02:49 -0700

    initial commit

---
+++ CanCompleteCircuit/JavaScript/CanCompleteCircuit.js
@@ -0,0 +1,7 @@
+// Solution for: https://leetcode.com/problems/gas-station/
+var canCompleteCircuit = function (gas, cost) {
+    return 0;
+};
+// some test cases
+console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2])); // 3
+console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3])); // -1
---
+++ CanCompleteCircuit/TypeScript/CanCompleteCircuit.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/gas-station/
+
+const canCompleteCircuit = (gas: number[], cost: number[]): number => {
+    
+    return 0;
+
+};
+
+
+
+// some test cases
+console.log(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2])); // 3
+console.log(canCompleteCircuit([2,3,4], [3,4,3])); // -1
