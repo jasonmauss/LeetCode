391329cf0deff513209f07d4a9b43dd432d38067
Jason Mauss <jason.mauss@gmail.com>
2024-07-03 20:49:05 -0700

    initial commit

---
+++ RestoreIpAddresses/JavaScript/RestoreIpAddresses.js
@@ -0,0 +1,8 @@
+// Solution for: https://leetcode.com/problems/restore-ip-addresses/
+var restoreIpAddresses = function (s) {
+    return [];
+};
+// some test cases
+console.log(restoreIpAddresses('25525511135')); // ["255.255.11.135","255.255.111.35"]
+console.log(restoreIpAddresses('101023')); // ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+console.log(restoreIpAddresses('0000')); // ["0.0.0.0"]
---
+++ RestoreIpAddresses/TypeScript/RestoreIpAddresses.ts
@@ -0,0 +1,13 @@
+// Solution for: https://leetcode.com/problems/restore-ip-addresses/
+
+const restoreIpAddresses = (s: string): string[] => {
+    
+    return [];
+
+};
+
+
+// some test cases
+console.log(restoreIpAddresses('25525511135')); // ["255.255.11.135","255.255.111.35"]
+console.log(restoreIpAddresses('101023')); // ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+console.log(restoreIpAddresses('0000')); // ["0.0.0.0"]
