c3c192609e110e80844dc338cbae76231fcba468
Jason Mauss <jason.mauss@gmail.com>
2024-07-04 23:11:57 -0700

    Adding Solutions for TypeScript and JavaScript

--- RestoreIpAddresses/JavaScript/RestoreIpAddresses.js
+++ RestoreIpAddresses/JavaScript/RestoreIpAddresses.js
@@ -1,6 +1,23 @@
 // Solution for: https://leetcode.com/problems/restore-ip-addresses/
+var dfs = function (s, i, current, block, count, result) {
+    if (i > s.length || count > 3 || block.length > 1 && parseInt(block) + "" != block || Number(block) > 255)
+        return;
+    if (i == s.length) {
+        if (block != "" && count == 3) {
+            result.push(current);
+        }
+        else {
+            return;
+        }
+    }
+    dfs(s, i + 1, current + s[i], block + s[i], count, result);
+    if (block != "")
+        dfs(s, i, current + ".", "", count + 1, result);
+};
 var restoreIpAddresses = function (s) {
-    return [];
+    var result = [];
+    dfs(s, 0, "", "", 0, result);
+    return result;
 };
 // some test cases
 console.log(restoreIpAddresses('25525511135')); // ["255.255.11.135","255.255.111.35"]
